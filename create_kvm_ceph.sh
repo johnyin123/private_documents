28e0cc47a93d8f1c3514a580c1a64a2bfad034e4
johnyin123 <johnyin.news@163.com>
2017-11-03 09:26:10 +0800

    modi

--- create_kvm_ceph.sh
+++ create_kvm_ceph.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -u
 
+CEPH_KVM_POOL=kvm_os_pool
+TPL_IMG=CentOS7.4.tpl.raw
+
 function genceph_img() {
 local ceph_pool=$1
 local vm_img=$2
@@ -16,20 +19,21 @@ if [ "${FOUND_IMG}" == "1" ]; then
     echo "${vm_img} create failed!!!"
     return 1
 else
-    rbd copy ${tpl_img} ${ceph_pool}/${vm_img}
+    rbd copy ${tpl_img} ${ceph_pool}/${vm_img} || return 1
     local DEV_RBD=$(rbd map ${ceph_pool}/${vm_img})
-    mount ${DEV_RBD}p2 /mnt
-    sed -i "s/^IPADDR=.*/IPADDR=\"${guest_ipaddr}\"/g"    /mnt/etc/sysconfig/network-scripts/ifcfg-eth0
-    sed -i "s/^NETMASK=.*/NETMASK=\"${guset_netmask}\"/g" /mnt/etc/sysconfig/network-scripts/ifcfg-eth0
-    sed -i "s/^GATEWAY=.*/GATEWAY=\"${guest_gw}\"/g"      /mnt/etc/sysconfig/network-scripts/ifcfg-eth0
-    echo "${guest_hostname}" > /mnt/etc/hostname
-    chattr +i /mnt/etc/hostname
+    mount ${DEV_RBD}p2 /mnt || return 2
+    sed -i "s/^IPADDR=.*/IPADDR=\"${guest_ipaddr}\"/g"    /mnt/etc/sysconfig/network-scripts/ifcfg-eth0 || return 3
+    sed -i "s/^NETMASK=.*/NETMASK=\"${guset_netmask}\"/g" /mnt/etc/sysconfig/network-scripts/ifcfg-eth0 || return 4
+    sed -i "s/^GATEWAY=.*/GATEWAY=\"${guest_gw}\"/g"      /mnt/etc/sysconfig/network-scripts/ifcfg-eth0 || return 5
+    echo "${guest_hostname}" > /mnt/etc/hostname || return 6
+    chattr +i /mnt/etc/hostname || return 7
+    #sed -i "s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"console=ttyS0\"" /etc/default/grub
+    #grub2-mkconfig -o /boot/grub2/grub.cfg
     rm -f /mnt/ssh/ssh_host_*
-    umount /mnt
-    rbd showmapped 
-    rbd unmap ${DEV_RBD}
-    rbd showmapped 
-    echo "copy vmimage, OK"
+    echo "set ip/gw/hostname/sshd_key OK"
+    umount /mnt || return 8
+    rbd unmap ${DEV_RBD} || return 9
+    echo "     disk:OK"
     return 0
 fi
 }
@@ -41,12 +45,14 @@ function genkvm_xml(){
     local vm_img=$4
     local title=$5
     local desc=$6
+    local uuid=$7
 
     local memsize=$((1024*1024*1))
     local vcpus=1
-    cat > ${VMNAME}<<EOFA
+    cat > ${vmname}<<EOFA
 <domain type='kvm'>
-  <name>${VMNAME}</name>
+  <name>${vmname}</name>
+  <uuid>${uuid}</uuid>
   <title>${title}</title>
   <description>${desc}</description>
   <memory unit='KiB'>${memsize}</memory>
@@ -100,50 +106,45 @@ function genkvm_xml(){
 EOFA
 }
 
-CEPH_KVM_POOL=kvm_os_pool
-TPL_IMG=CentOS7.4.tpl.raw
-
 [[ -r "hosts.conf" ]] || {
 	cat >"hosts.conf" <<- EOF
-#IP          hostname(小写-)  netmask	gateway title   desc
-10.0.2.100   kvm1         255.255.255.0 10.0.2.1  熙康测试机器  描述灭有啊
+#IP          hostname-prefix(小写-)  netmask	    gateway   title         desc
+10.0.2.100   kvm                     255.255.255.0  10.0.2.1  熙康测试机器  描述灭有啊
 
 EOF
 	echo "Created hosts.conf using defaults.  Please review it/configure before running again."
 	exit 1
 }
+
 CONF='cat hosts.conf | grep -v -e "^$" -e "^#"'
 IPS=$(eval $CONF | awk '{print $1}')
 
-
 for ip in $IPS
 do
-
     VMNAME=$(eval $CONF | grep ${ip} | awk '{print $2}')
+    UUID=$(cat /proc/sys/kernel/random/uuid)
     NETMASK=$(eval $CONF | grep ${ip} | awk '{print $3}')
     GATEWAY=$(eval $CONF | grep ${ip} | awk '{print $4}')
-    VM_IMG=${VMNAME}.raw
+    VM_IMG=${VMNAME}-${UUID}.raw
 
-    genceph_img ${CEPH_KVM_POOL} ${VM_IMG} ${TPL_IMG} ${VMNAME} ${ip} ${NETMASK} ${GATEWAY}
-    echo "RETURN $?  xxxxxxxxxxxxxxxxxx"
-    if [[ $? == 1  ]]; then
-        echo "xxxxxxxxxxxxxxxx"
+    echo "Create vm:${VMNAME}-${UUID}"
+    echo "     disk:${VM_IMG}"
+    echo "       ip:${ip}"
+    echo "       gw:${GATEWAY}"
+
+    genceph_img ${CEPH_KVM_POOL} ${VM_IMG} ${TPL_IMG} "${VMNAME}-${UUID}" ${ip} ${NETMASK} ${GATEWAY}
+    if [[ $? != 0  ]]; then
+        echo "   failed: $?"
+        continue
     fi
-
     VM_TITLE=$(eval $CONF | grep ${ip} | awk '{print $5}')
     VM_DESC=$(eval $CONF | grep ${ip} | awk '{print $6}')
     ceph_secret_uuid=$(virsh secret-list  | grep libvirt | awk '{ print $1}')
-    genkvm_xml ${VMNAME} ${ceph_secret_uuid} ${CEPH_KVM_POOL} ${VM_IMG} ${VM_TITLE} ${VM_DESC}
-
-    read -p "Create VM ${VMNAME}[y|n]" yn
-    case "${yn}" in
-        y|Y|yes|YES)
-            virsh define ${VMNAME}
-            virsh list --all
-            ;;
-        *)
-            rbd remove ${CEPH_KVM_POOL}/${VM_IMG}
-            ;;
-    esac
-    rm ${VMNAME} -f
+    genkvm_xml "${VMNAME}-${UUID}" ${ceph_secret_uuid} ${CEPH_KVM_POOL} ${VM_IMG} ${VM_TITLE} ${VM_DESC} ${UUID}
+    virsh define ${VMNAME}-${UUID} > /dev/null 2>&1 
+    virsh list --title --all | grep "${VMNAME}-${UUID}" > /dev/null 2>&1  && echo "    staus:OK" || echo "    staus:FAILED"
+    rm ${VMNAME}-${UUID} -f
+    echo "================================================================="
 done
+echo "OK"
+exit 0
