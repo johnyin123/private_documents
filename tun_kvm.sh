9518fbe6cea7df30e932814e668b9e6cc73854b7
johnyin123 <johnyin.news@163.com>
2017-11-16 10:33:09 +0800

    add import rbd via ssh

--- tun_kvm.sh
+++ tun_kvm.sh
@@ -10,3 +10,8 @@ Add below codes after the line "<currentMemory ..."
 <memoryBacking>
   <hugepages/>
 </memoryBacking>
+
+
+#import LVM image via ssh to remote host
+# Using format 2 rbd images 
+# dd if=/dev/e0.0/snap-webhotel | pv | ssh root@remote-server 'rbd --image-format 2 import - sata/webserver'
