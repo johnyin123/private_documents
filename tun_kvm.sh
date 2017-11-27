aceeb36062918570cf54b9e948f8223a08937015
johnyin123 <johnyin.news@163.com>
2017-11-27 08:17:33 +0800

    tun_kvm

--- tun_kvm.sh
+++ tun_kvm.sh
@@ -20,3 +20,33 @@ Add below codes after the line "<currentMemory ..."
 # rbd snap create sata/webserver@snap1
 ## Transfer image
 # rbd export sata/webserver@snap1 - | pv | ssh root@remote-server 'dd of=/dev/lvm-storage/webserver'
+
+# rbd map libvirt-pool/template.raw
+# rbd resize libvirt-pool/template.raw --size 8G
+# growpart /dev/rbd0 1
+# mount /dev/rbd0p1 /mnt
+# xfs_growfs -d  -l  /mnt
+# umount /mnt
+# xfs_repair /dev/rbd0p1
+# rbd unmap /dev/rbd0
+
+# qemu-img convert -f qcow2 -O raw debian_squeeze.qcow2 rbd:data/squeeze
+# virsh start $VMNAME
+# ping -c1 -W2 ${ip} >/dev/null 2>&1 && echo OK || echo ERR
+
+# mount -o loop,offset=32256,uid=1000,gid=1000  fat32.dsk  ${mnt_point}
+# modprobe nbd max_part=63
+# qemu-nbd -c /dev/nbd0 fat32.dsk
+# mount /dev/nbd0p1 -o uid=1000,gid=1000 ${mnt_point}
+# qemu-nbd -d /dev/nbd0
+
+
+# qemu-img resize -f raw demo.disk +1G
+# fdisk demo.disk #add new partitions
+# fdisk -l demo.disk
+# kpartx -av demo.disk 
+# ll /dev/mapper/loop0p1 
+# mkfs.xfs /dev/mapper/loop0p2 
+# kpartx -d demo.disk 
+
+
