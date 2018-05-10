e8f67c5a3ca2a64f846f31d330970ad0f7209aa2
johnyin <johnyin.news@163.com>
2018-05-10 09:41:29 +0800

    build_centos.sh add package

--- build_centos.sh
+++ build_centos.sh
@@ -11,7 +11,7 @@ fi
 TOMCAT_USR=${TOMCAT_USR:-false}
 REPO=${REPO:-${dirname}/local.repo}
 ADDITION_PKG=${ADDITION_PKG:-""}
-ADDITION_PKG="${ADDITION_PKG} lvm2 wget rsync bind-utils sysstat tcpdump nmap-ncat telnet lsof unzip ftp wget strace ltrace python-virtualenv qemu-guest-agent"
+ADDITION_PKG="${ADDITION_PKG} lvm2 wget rsync bind-utils sysstat tcpdump nmap-ncat telnet lsof unzip ftp wget strace ltrace python-virtualenv qemu-guest-agent traceroute rsync pciutils lrzsz"
 ROOTFS=${ROOTFS:-${dirname}/rootfs}
 NEWPASSWORD=${NEWPASSWORD:-"password"}
 DISK_FILE=${DISK_FILE:-"${dirname}/disk"}
