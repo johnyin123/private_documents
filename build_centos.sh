f1503136f51235cea342589bfaf47858e14f7d74
johnyin <johnyin.news@163.com>
2018-08-08 08:13:17 +0800

    sysctl.conf

--- build_centos.sh
+++ build_centos.sh
@@ -279,24 +279,24 @@ echo "MACs    hmac-sha1" >> ${ROOTFS}/etc/ssh/sshd_config
 log "info" "tune kernel parametres"
 cat >> ${ROOTFS}/etc/sysctl.conf << EOF
 net.core.netdev_max_backlog = 30000
-net.core.rmem_max=16777216
+net.core.rmem_max = 16777216
 net.core.somaxconn = 65535
-net.core.wmem_max=16777216
-net.ipv4.ip_local_port_range = 1024 65500
+net.core.wmem_max = 16777216
+net.core.wmem_default = 16777216
+net.ipv4.ip_local_port_range = 1024 65530
 net.ipv4.tcp_fin_timeout = 10
 net.ipv4.tcp_keepalive_time = 1200
 net.ipv4.tcp_max_orphans = 3276800
 net.ipv4.tcp_max_syn_backlog = 262144
 net.ipv4.tcp_no_metrics_save=1
-net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
 net.ipv4.tcp_syn_retries = 1
 net.ipv4.tcp_synack_retries = 1
-net.ipv4.tcp_syncookies = 1
-
+net.ipv4.tcp_syncookies = 0
 net.ipv4.tcp_timestamps = 0
 net.ipv4.tcp_tw_recycle = 0
 net.ipv4.tcp_tw_reuse = 0
-net.ipv4.tcp_wmem=4096 65536 16777216
 EOF
 cat >${ROOTFS}/etc/motd.sh<<EOF
 #!/bin/bash
