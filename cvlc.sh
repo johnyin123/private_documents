298be2af356c477d60a090a972a8e189fd698cf7
johnyin <johnyin.news@163.com>
2018-10-22 08:11:26 +0800

    fix

--- cvlc.sh
+++ cvlc.sh
@@ -2,3 +2,7 @@
 cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=WMV2,vb=800,acodec=wma2,ab=128,channels,samplerate=44100}:http{dst=0.0.0.0:8080/stream.asf}' --no-sout-all --sout-keep
 cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=WMV2,vb=400,wicodec=none}:http{dst=:8080/stream.asf}'
 arecord  -f cd -t raw | oggenc - -r | ssh -p60022 root@10.32.166.33 mplayer -
+ssh <user>@<remotehost> 'arecord -f cd -t raw | oggenc - -r' | mplayer -
+ssh <user>@<remotehost> 'arecord -f cd -D plughw:1 | ffmpeg -ac 1 -i - -f ogg -' | mplayer - -idle -demuxer ogg^C
+arecord -l
+
