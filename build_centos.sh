efbd293763831a74883a6c8f4073a59befa011c8
johnyin <johnyin.news@163.com>
2019-01-11 15:25:53 +0800

    add virt-install win10

--- build_centos.sh
+++ build_centos.sh
@@ -523,4 +523,16 @@ exit 0
 #    --graphics none \
 #    --network bridge=br-mgr,model=virtio \
 #    --extra-args 'ks=http://10.32.166.41:8080/ks.ks ksdevice=eth0 ip=10.3.60.100 netmask=255.255.255.128 gateway=10.3.60.1 console=ttyS0'
+#  virt-install \
+#     --name=Windows10 \
+#     --ram=4096 \
+#     --cpu host --hvm \
+#     --vcpus=2 \
+#     --os-type=windows \
+#     --os-variant=win8.1 \
+#     --disk /var/lib/libvirt/images/vms-win10,size=60,bus=virtio \
+#     --disk /var/lib/libvirt/boot/win-10-64bit-english.iso,device=cdrom,bus=ide \
+#     --disk /var/lib/libvirt/boot/virtio-win-drivers-20120712-1.iso,device=cdrom,bus=ide \
+#     --network bridge=br0 \
+#     --graphics vnc,listen=0.0.0.0
 
