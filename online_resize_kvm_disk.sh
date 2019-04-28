805b031a0f38df90c42c06a8aa7347f4837fc628
johnyin <johnyin.news@163.com>
2019-04-28 09:30:53 +0800

    add feature

--- online_resize_kvm_disk.sh
+++ online_resize_kvm_disk.sh
@@ -8,3 +8,25 @@ test on rbd storage for kvm
 virsh qemu-monitor-command domname balloon 1024 --hmp
 virsh qemu-monitor-command domname info balloon --hmp
 
+
+
+#    qemu-img resize -f rbd rbd:sata/disk3 40G
+#    virsh domblklist rbd-test
+#    > Target     Source
+#    > ------------------------------------------------
+#    > vdb        sata/disk2-qemu-5g:rbd_cache=1
+#    > vdc        sata/disk3:rbd_cache=1
+#    > hdc        -
+#    Then use virsh to tell the guest that the disk has a new size:
+#    virsh blockresize --domain rbd-test --path "vdc" --size 40G
+#    > Block device 'vdc' is resized
+#    Check raw rbd info
+#    rbd --pool sata info disk3
+#    > rbd image 'disk3':
+#    >        size 40960 MB in 10240 objects
+#    >        order 22 (4096 KB objects)
+#    >        block_name_prefix: rb.0.13fb.23353e97
+#    >        parent:  (pool -1)
+#    Make sure you can see the change from dmesg (Guest should see the new size change).
+
+
