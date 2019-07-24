eb3c511b4df03ca902213df862c101cc760c62da
johnyin <johnyin.news@163.com>
2019-07-24 09:04:22 +0800

    feature try.sh

--- try.sh
+++ try.sh
@@ -9,25 +9,55 @@ if [ "${DEBUG:=false}" = "true" ]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-
-# Set color variables.
-readonly GREEN='\e[0;32m'
-readonly RED='\e[0;31m'
-readonly BLUE='\e[0;34m'
-readonly endColor='\e[0m'
-msg()
+##################################################
+readonly RED="\033[1;31m"
+readonly GREEN="\033[1;32m"
+readonly YELLOW="\033[1;33m"
+readonly BLUE="\033[1;34m"
+readonly ENDCLR="\033[0m"
+_log_msg()
 {
-	m_prefix="$1"
-    beginColor=""
-	case "$2" in
-		GREEN) beginColor="$GREEN";;
-		BLUE) beginColor="$BLUE";;
-		RED) beginColor="$RED";;
-        *) beginColor="";;
-	esac
-	shift 2
-	echo -e "${m_prefix}${beginColor}$*${endColor}"; >&2
+	if [ "${quiet:-n}" = "y" ]; then return; fi
+	# shellcheck disable=SC2059
+	printf "$@"
 }
+
+log_success_msg()
+{
+	_log_msg "${GREEN}Success:${ENDCLR} %s\\n" "$*"
+}
+
+log_failure_msg()
+{
+	_log_msg "${RED}Failure:${ENDCLR} %s\\n" "$*"
+}
+
+log_warning_msg()
+{
+	_log_msg "${YELLOW}Warning:${ENDCLR} %s\\n" "$*"
+}
+
+log_begin_msg()
+{
+	_log_msg "${BLUE}Begin:${ENDCLR} %s ... " "$*"
+}
+
+log_end_msg()
+{
+	_log_msg "${BLUE}done.${ENDCLR}\\n"
+}
+##################################################
+run_scripts()
+{
+	initdir=${1}
+	[ ! -d "${initdir}" ] && return
+
+	shift
+    for i in ${initdir}/*; do
+	    . "${initdir}/$i"
+    done
+}
+##################################################
 #******************************************************************************
 # try: Execute a command with error checking.  Note that when using this, if a piped
 # command is used, the '|' must be escaped with '\' when calling try (i.e.
@@ -36,22 +66,23 @@ msg()
 try ()
 {
 	# Execute the command and fail if it does not return zero.
-    msg "[ * ]" RED "${*}"
+    log_begin_msg "${*}"
 	eval ${*} || failure && success
+    log_end_msg
 }
 
 success()
 {
-    msg "" GREEN " OK"
+    log_success_msg "OK"
     return 0
 }
 
 failure ()
 {
-    msg "" RED " ERR"
+    log_failure_msg "ERR"
     return 1
 }
-
+##################################################
 cleanup() {
     echo "EXIT!!!"
 }
@@ -59,6 +90,7 @@ cleanup() {
 trap cleanup EXIT
 trap cleanup TERM
 trap cleanup INT
+##################################################
 
 function main {
     echo "MAIN!!!"
