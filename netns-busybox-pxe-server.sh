71bc42398b2aa5c7c0cc6c49d17316c947aed829
johnyin <johnyin.news@163.com>
2019-08-13 11:28:59 +0800

    fix netns pxe

--- netns-busybox-pxe-server.sh
+++ netns-busybox-pxe-server.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-PXE_NS=pxe
-PXE_IP=192.168.1.1/24
-#EXT_BRIDGE=virbr0
-#PXE_IP=10.32.166.44/25
-EXT_BRIDGE=br-ext
-
-
 BASEDIR="$(readlink -f "$(dirname "$0")")"
 
-mkdir -p ${BASEDIR}/var/lib/misc/ ${BASEDIR}/var/run ${BASEDIR}/etc ${BASEDIR}/lib ${BASEDIR}/tftpd
+PXE_NS=pxe
+PXE_IP=192.168.1.1
+EXT_BRIDGE=br-ext
+DVD=/home/johnyin/disk/iso/CentOS-7-x86_64-Everything-1708.iso
+BUSYBOX=/home/johnyin/disk/private/shell/busybox-tools/busybox
+PXELINUX_DIR=${BASEDIR}/pxelinux/
+
+mkdir -p ${BASEDIR}/var/lib/misc/ ${BASEDIR}/var/run ${BASEDIR}/etc ${BASEDIR}/lib
+mkdir -p ${BASEDIR}/bin/ ${BASEDIR}/dev/ ${BASEDIR}/tftpd/dvdrom/ ${BASEDIR}/tftpd/pxelinux.cfg/
 
 for cmd in tftpd httpd ftpd udhcpd
 do
-./busybox $cmd && echo "check $cmd ok" || { echo "check $cmd error"; exit 1; }
+    ${BUSYBOX} --list | grep $cmd > /dev/null && echo "check $cmd ok" || { echo "check $cmd error"; exit 1; }
 done
+
+cp ${BUSYBOX} ${BASEDIR}/bin/
+cp ${PXELINUX_DIR}/* ${BASEDIR}/tftpd/
 
 [ -e ${BASEDIR}/bin/tftpd   ] || ln -s /bin/busybox ${BASEDIR}/bin/tftpd
 [ -e ${BASEDIR}/bin/httpd   ] || ln -s /bin/busybox ${BASEDIR}/bin/httpd
@@ -29,7 +33,7 @@ done
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
 export PS1="\\[\\033[1;31m\\]\\u\\[\\033[m\\]@\\[\\033[1;32m\\]**${PXE_NS}**:\\[\\033[33;1m\\]\\w\\[\\033[m\\]\\$"
-alias ll='ls -lh'
+alias ll='/bin/busybox ls -lh'
 EOF
 }
 [ -e ${BASEDIR}/etc/passwd ] || {
@@ -60,11 +64,170 @@ option  lease   864000
 EOF
 }
 
+[ -e ${BASEDIR}/tftpd/ks.cfg ] || {
+    cat > ${BASEDIR}/tftpd/ks.cfg <<'KSEOF'
+firewall --disabled
+install
+
+text
+firstboot --enable
+
+lang zh_CN.UTF-8
+keyboard us
+network --onboot yes --bootproto dhcp --noipv6
+network --hostname=server1
+rootpw  --iscrypted $6$Tevn5ihz1h7MHhMV$Zt7r1ocJqZXhNfVntdsDuGWU42BkQKdpqp0EosOhaYS46zzOEcYALmH5mkDWoYmRvFBs0lBNM/LUiGJAmmx7Q.
+#password
+url --url="http://192.168.1.1/dvdrom/"
+
+firewall --disabled
+authconfig --enableshadow --passalgo=sha512
+selinux --disabled
+# services --enabled=NetworkManager,sshd
+#
+# Reboot after installation
+reboot
+#timezone --utc America/New_York
+timezone  Asia/Shanghai
+
+#user --groups=wheel --name=admin --password=password
+
+bootloader --location=mbr --driveorder=sda --append=" console=ttyS0 net.ifnames=0 biosdevname=0"
+# The following is the partition information you requested
+# Note that any partitions you deleted are not expressed
+# here so unless you clear all partitions first, this is
+# not guaranteed to work
+#clearpart --none
+#ignoredisk --only-use=sda
+
+#part / --fstype=ext4 --grow --size=200
+clearpart --all
+# part / --asprimary --size=2048 --ondisk=vda
+# part swap --asprimary --size=1024 --ondisk=vda
+# lvm 
+part /boot --fstype="xfs" --size=200
+part pv.01 --size=1500 --grow
+volgroup vg_root pv.01
+logvol / --vgname=vg_root --size=1500 --name=lv_root
+# part pv.02 --size=2048
+# volgroup vg_swap pv.02
+# logvol swap --vgname=vg_swap --size=1 --grow --name=lv_swap                
+
+#part / --fstype ext4 --size=100 --grow --ondisk=sda
+#net.ifnames=0 biosdevname=0 ...........eth0
+#grub2-mkconfig -o /boot/grub2/grub.cfg
+%packages
+@core
+lvm2
+wget
+rsync
+bind-utils
+sysstat
+tcpdump
+nmap-ncat
+telnet
+lsof
+unzip
+ftp
+wget
+strace
+ltrace
+python-virtualenv
+net-tools
+chrony
+traceroute
+lrzsz
+iotop
+
+%end
+%post
+#!/bin/bash
+# echo "闲置用户在 15 分钟后会被删除"
+echo "export readonly TMOUT=900" >> /etc/profile.d/os-security.sh
+echo "export readonly HISTFILE" >> /etc/profile.d/os-security.sh
+chmod +x /etc/profile.d/os-security.sh
+
+cat >/etc/profile.d/johnyin.sh<<"EOF"
+export PS1="\[\033[1;31m\]\u\[\033[m\]@\[\033[1;32m\]\h:\[\033[33;1m\]\w\[\033[m\]$"
+set -o vi
+EOF
+
+cat >> /etc/security/limits.conf << EOF
+*           soft   nofile       102400
+*           hard   nofile       102400
+EOF
+#disable selinux
+sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+#set sshd
+sed -i 's/#UseDNS.*/UseDNS no/' /etc/ssh/sshd_config
+sed -i 's/#MaxAuthTries.*/MaxAuthTries 3/' /etc/ssh/sshd_config
+sed -i 's/#Port.*/Port 60022/' /etc/ssh/sshd_config
+sed -i 's/GSSAPIAuthentication.*/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/#MaxAuthTries.*/MaxAuthTries 3/g' /etc/ssh/sshd_config
+echo "Ciphers aes256-ctr,aes192-ctr,aes128-ctr" >> /etc/ssh/sshd_config
+echo "MACs    hmac-sha1" >> /etc/ssh/sshd_config
+#tune kernel parametres
+cat >> /etc/sysctl.conf << EOF
+net.core.rmem_max = 134217728
+net.core.wmem_max = 134217728
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 65535
+net.core.wmem_default = 16777216
+net.ipv4.ip_local_port_range = 1024 65530
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.tcp_max_orphans = 3276800
+net.ipv4.tcp_max_syn_backlog = 262144
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_syncookies = 0
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_tw_reuse = 0
+EOF
+
+cat > /etc/default/grub <<'EOF'
+GRUB_TIMEOUT=5
+GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
+GRUB_DEFAULT=saved
+GRUB_DISABLE_SUBMENU=true
+GRUB_TERMINAL_OUTPUT="console"
+GRUB_CMDLINE_LINUX="console=ttyS0 net.ifnames=0 biosdevname=0"
+GRUB_DISABLE_RECOVERY="true"
+EOF
+
+systemctl set-default multi-user.target
+systemctl enable getty@tty1
+chkconfig 2>/dev/null | egrep -v "crond|sshd|network|rsyslog|sysstat"|awk '{print "chkconfig",$1,"off"}' | bash
+systemctl list-unit-files | grep service | grep enabled | egrep -v "getty|autovt|sshd.service|rsyslog.service|crond.service|auditd.service|sysstat.service|chronyd.service" | awk '{print "systemctl disable", $1}' | bash
+%end
+KSEOF
+}
+
+[ -e ${BASEDIR}/tftpd/pxelinux.cfg/default ] || {
+    cat > ${BASEDIR}/tftpd/pxelinux.cfg/default <<EOF
+default menu.c32
+prompt 0
+timeout 300
+ONTIMEOUT local
+menu title ########## PXE Boot Menu ##########
+label 1
+menu label ^1) Install CentOS 7 x64 with Local Repo
+kernel /dvdrom/images/pxeboot/vmlinuz
+append initrd=/dvdrom/images/pxeboot/initrd.img ks=http://${PXE_IP}/ks.cfg net.ifnames=0 biosdevname=0
+label 2
+menu label ^2) Boot from local drive
+localboot 0xffff
+EOF
+}
+
 ip netns add ${PXE_NS}
 ip netns exec ${PXE_NS} ip link set dev lo up
 #ip netns exec ${PXE_NS} sysctl -w net.ipv4.conf.default.forwarding=1
 ip link add ${PXE_NS}-eth0 type veth peer name ${PXE_NS}-eth1
-echo "add "
 #brctl addif br-ext ${PXE_NS}-eth1
 ip link set dev ${PXE_NS}-eth1 promisc on
 ip link set dev ${PXE_NS}-eth1 up
@@ -72,14 +235,14 @@ ip link set dev ${PXE_NS}-eth1 master ${EXT_BRIDGE}
 
 ip link set ${PXE_NS}-eth0 netns ${PXE_NS}
 ip netns exec ${PXE_NS} ip link set dev ${PXE_NS}-eth0 name eth0 up
-ip netns exec ${PXE_NS} ip address add ${PXE_IP} dev eth0
+ip netns exec ${PXE_NS} ip address add ${PXE_IP}/24 dev eth0
 # ip netns exec ${PXE_NS} ip route add default via 10.32.166.1 dev eth0
 
-mount /home/johnyin/disk/iso/CentOS-7-x86_64-Everything-1708.iso ${BASEDIR}/tftpd/dvdrom/
+mount ${DVD} ${BASEDIR}/tftpd/dvdrom/
 
 # ip netns exec ${PXE_NS} chroot ${BASEDIR} /usr/sbin/dnsmasq --user=root --group=root
-ip netns exec ${PXE_NS} chroot ${BASEDIR} busybox udhcpd 
-ip netns exec ${PXE_NS} chroot ${BASEDIR} busybox inetd
+ip netns exec ${PXE_NS} chroot ${BASEDIR} /bin/busybox udhcpd 
+ip netns exec ${PXE_NS} chroot ${BASEDIR} /bin/busybox inetd
 ip netns exec ${PXE_NS} chroot ${BASEDIR} /bin/busybox sh -l
 
 umount ${BASEDIR}/tftpd/dvdrom/
