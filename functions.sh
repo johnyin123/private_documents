e43510b59629d475ff8d2bcacddd515b34e63a94
johnyin <johnyin.news@163.com>
2019-09-26 10:43:51 +0800

    add func

--- functions.sh
+++ functions.sh
@@ -340,6 +340,28 @@ str_replace() {
 
     echo "${DATA//$ORIG/$DEST}"
 }
+
+ip2int() {
+    local a b c d
+    { IFS=. read a b c d; } <<< $1
+    echo $(((((((a << 8) | b) << 8) | c) << 8) | d))
+}
+
+int2ip() {
+    local ui32=$1; shift
+    local ip n
+    for n in 1 2 3 4; do
+        ip=$((ui32 & 0xff))${ip:+.}$ip
+        ui32=$((ui32 >> 8))
+    done
+    echo $ip
+}
+
+netmask() {
+    local mask=$((0xffffffff << (32 - $1))); shift
+    int2ip $mask
+}
+
 is_ipv4() {
     local -r regex='^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
 
