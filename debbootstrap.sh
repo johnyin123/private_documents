465d5520bcedd4e64cb3276680dfc555381fee68
johnyin <johnyin.news@163.com>
2019-12-16 15:33:51 +0800

    batman-adv

--- debbootstrap.sh
+++ debbootstrap.sh
@@ -23,7 +23,7 @@ OVERLAY_LABEL="EMMCOVERLAY"
 ZRAMSWAP="udisks2"
 #ZRAMSWAP="zram-tools"
 PKG="libc-bin,tzdata,locales,dialog,apt-utils,systemd-sysv,dbus-user-session,ifupdown,initramfs-tools,u-boot-tools,fake-hwclock,openssh-server,busybox"
-PKG="${PKG},udev,isc-dhcp-client,netbase,console-setup,pkg-config,net-tools,wpasupplicant,iputils-ping,telnet,vim,ethtool,${ZRAMSWAP},bridge-utils,dosfstools,iw,ipset,nmap,ipvsadm"
+PKG="${PKG},udev,isc-dhcp-client,netbase,console-setup,pkg-config,net-tools,wpasupplicant,iputils-ping,telnet,vim,ethtool,${ZRAMSWAP},bridge-utils,dosfstools,iw,ipset,nmap,ipvsadm,batctl"
 
 if [ "$UID" -ne "0" ]
 then 
