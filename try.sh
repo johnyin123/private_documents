999acc8975564aa0969c46da5c4d4ccc07176672
johnyin <johnyin.news@163.com>
2020-05-14 11:00:57 +0800

    add pipe execute status check

--- try.sh
+++ try.sh
@@ -126,6 +126,14 @@ __trace_OFF__
     echo "MAIN!!!"
     # echo > aa.txt
     # ${EDITOR:-${VISUAL:-vi}}  aa.txt
+    netstat -tulpn | grep nginx
+    echo "${PIPESTATUS[@]}"
+    true | true
+    echo "The exit status of first command ${PIPESTATUS[0]}, and the second command ${PIPESTATUS[1]}"
+    true | false
+    echo "The exit status of first command ${PIPESTATUS[0]}, and the second command ${PIPESTATUS[1]}"
+    false | false | true
+    echo "The exit status of first command ${PIPESTATUS[0]}, second command ${PIPESTATUS[1]}, and third command ${PIPESTATUS[2]}"
     return 0
 }
 main "$@"
