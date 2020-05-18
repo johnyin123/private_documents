7eb53c80bb22d34c585a2f3bd28041bf193cebf1
johnyin <johnyin.news@163.com>
2020-05-18 17:08:05 +0800

    add tunnel.sh

---
+++ tunnel.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+readonly SCRIPTNAME=${0##*/}
+if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
+    exec 5> ${DIRNAME}/$(date '+%Y%m%d%H%M%S').${SCRIPTNAME}.debug.log
+    BASH_XTRACEFD="5"
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+[ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true
+################################################################################
+
+del_ns() {
+    debug_msg "enter [%s]\n" "${FUNCNAME[0]} $*"
+    local ns_name="$1"
+    #try brctl delif br-ext ${ns_name}-eth1
+    try ip link set ${ns_name}-eth1 promisc off || true
+    try ip link set ${ns_name}-eth1 down || true
+    try ip link set dev ${ns_name}-eth1 nomaster || true
+    try ip netns del ${ns_name} || true
+    ip link delete ${ns_name}-eth1 || true
+    try rm -rf "/etc/netns/${ns_name}" || true
+    return 0
+}
+
+add_ns() {
+    debug_msg "enter [%s]\n" "${FUNCNAME[0]} $*"
+    local ns_name="$1"
+    local host_br="$2"
+    local ns_cidr="$3"
+    try ip netns add ${ns_name} || return 4
+    try ip netns exec ${ns_name} ip addr add 127.0.0.1/8 dev lo || return 4
+    try ip netns exec ${ns_name} ip link set lo up || return 4
+    try ip link add ${ns_name}-eth0 type veth peer name ${ns_name}-eth1 || return 4
+    #try brctl addif br-ext ${ns_name}-eth1
+    try ip link set dev ${ns_name}-eth1 promisc on || return 4
+    try ip link set dev ${ns_name}-eth1 up || return 4
+    try ip link set dev ${ns_name}-eth1 master ${host_br} || return 4
+    try ip link set ${ns_name}-eth0 netns ${ns_name} || return 4
+    try ip netns exec ${ns_name} ip link set dev ${ns_name}-eth0 name eth0 up || return 4
+    try ip netns exec ${ns_name} ip address add ${ns_cidr} dev eth0 || return 4
+    return 0
+}
+
+error_clean() {
+    local ns_name="$1";shift 1
+    del_ns ${ns_name} 1>/dev/null 2>&1 || true
+    exit_msg "clean over! $* error\n";
+}
+
+main() {
+    local NS_CIDR=${NS_CIDR:-"10.32.149.223/24"}
+    local NS_NAME=${NS_NAME:-"web_ns"}
+    local HOST_BR=${1:-br-srvzone}; shift || exit_msg "IPADDR=$NS_CIDR <bridge>\n"
+
+    add_ns ${NS_NAME} ${HOST_BR} "${NS_CIDR}" || error_clean "${NS_NAME}" "add netns $?"
+    #ip netns exec ${NS_NAME} /bin/bash || true
+    #su johnyin /opt/google/chrome/google-chrome
+    try ip netns exec ${NS_NAME} "ip link set eth0 mtu 1300" || true
+    try ip netns exec ${NS_NAME} "ip route add default via 10.32.149.1" || true
+    ( nsenter --net=/var/run/netns/${NS_NAME} su johnyin /opt/google/chrome/google-chrome || true )&
+    unset PROMPT_COMMAND
+    ip netns exec ${NS_NAME} /bin/bash --rcfile <(echo "PS1=\"namespace ${NS_NAME}> \"") || true
+    del_ns ${NS_NAME}
+    info_msg "Exit success\n"
+    return 0
+}
+main "$@"
