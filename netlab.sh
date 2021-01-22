083badef75e9fc558568a9204199346c73f241cd
johnyin123 <johnyin.news@163.com>
2021-01-23 07:47:58 +0800

    rename

--- netlab.sh
+++ netlab.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("netlap.sh - bb43f23 - 2021-01-22T22:21:48+08:00")
+VERSION+=("netlab.sh - initversion - 2021-01-23T07:48:08+08:00")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true
 ################################################################################
 startup() {
