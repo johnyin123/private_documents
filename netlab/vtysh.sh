c3c4719b8b604ea95122a397e09d7d98a0207d31
Your Name <you@example.com>
2021-03-01 10:24:57 +0800

    demo

--- netlab/vtysh.sh
+++ netlab/vtysh.sh
@@ -7,4 +7,13 @@
 # QUAGGA_CONF="/etc/quagga/bgpd.conf"
 # NETWORK_ROUTEMAP_CMD=`cat $QUAGGA_CONF | grep "$1" | head -1`
 # vtysh -c "conf t" -c "router bgp $BGP_LOCAL_AS" -c "$NETWORK_ROUTEMAP_CMD"
+touch /etc/quagga/ospfd.conf
+chown quagga:quagga /etc/quagga/ospfd.conf
+chmod 640 /etc/quagga/ospfd.conf
+/sbin/ospfd
 
+vtysh  -c "conf t" -c "interface eth0" -c "ip ospf authentication message-digest" -c "ip ospf message-digest-key 1 md5 pass4OSPF"
+vtysh  -c "conf t" -c  "router ospf" -c "ospf router-id 1.1.1.1"
+vtysh  -c "write"
+vtysh  -c "conf t" -c  "router ospf" -c "log-adjacency-changes"
+cat /etc/quagga/ospfd.conf
