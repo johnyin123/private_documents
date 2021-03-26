ee31e9ec06cfd0c46a99ba9ea2ff7a894502f9ee
johnyin <johnyin.news@163.com>
2021-03-26 09:50:55 +0800

    add os_debian_init.sh

---
+++ os_debian_init.sh
@@ -0,0 +1,245 @@
+#!/bin/echo Warnning, this library must only be sourced!
+# shellcheck disable=SC2086 disable=SC2155
+
+# TO BE SOURCED ONLY ONCE:
+if [ -z ${__debina__inc+x} ]; then
+    __debina__inc=1
+else
+    return 0
+fi
+# Disable unicode.
+LC_ALL=C
+LANG=C
+
+# LC_ALL=C LANGUAGE=C LANG=C chroot ${root_dir} /bin/bash <<EOSHELL
+#     autologin_root
+# EOSHELL
+# ssh user@host << EOF
+#     $(typeset -f autologin_root)
+#     autologin_root
+# EOF
+debian_apt_init() {
+    echo 'Acquire::http::User-Agent "debian dler";' > /etc/apt/apt.conf
+    # echo 'APT::Install-Recommends "0";'> /etc/apt/apt.conf.d/71-no-recommends
+    # echo 'APT::Install-Suggests "0";'> /etc/apt/apt.conf.d/72-no-suggests
+    cat > /etc/apt/sources.list << EOF
+deb http://mirrors.163.com/debian ${DEBIAN_VERSION:-buster} main non-free contrib
+deb http://mirrors.163.com/debian ${DEBIAN_VERSION:-buster}-proposed-updates main non-free contrib
+deb http://mirrors.163.com/debian-security ${DEBIAN_VERSION:-buster}/updates main contrib non-free
+deb http://mirrors.163.com/debian ${DEBIAN_VERSION:-buster}-backports main contrib non-free
+EOF
+}
+export -f debian_apt_init
+
+debian_limits_init() {
+    #set the file limit
+    cat > /etc/security/limits.d/tun.conf << EOF
+*           soft   nofile       102400
+*           hard   nofile       102400
+EOF
+}
+export -f debian_limits_init() {
+
+debian_sysctl_init() {
+    mv /etc/sysctl.conf /etc/sysctl.conf.bak
+    cat << EOF > /etc/sysctl.conf
+net.core.rmem_max = 134217728 
+net.core.wmem_max = 134217728 
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 65535
+net.core.wmem_default = 16777216
+net.ipv4.ip_local_port_range = 1024 65531
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.tcp_max_orphans = 3276800
+net.ipv4.tcp_max_syn_backlog = 262144
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_syncookies = 0
+net.ipv4.tcp_timestamps = 0
+#net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_tw_reuse = 0
+net.ipv4.ip_forward = 1
+EOF
+}
+export -f debian_sysctl_init
+
+debian_sshd_regenkey() {
+  # Remove ssh host keys
+  rm -f /etc/ssh/ssh_host_*
+  systemctl stop sshd
+
+  # Regenerate ssh host keys
+  ssh-keygen -q -t rsa -N "" -f /etc/ssh/ssh_host_rsa_key
+  ssh-keygen -q -t dsa -N "" -f /etc/ssh/ssh_host_dsa_key
+  ssh-keygen -q -t ecdsa -N "" -f /etc/ssh/ssh_host_ecdsa_key
+  ssh-keygen -q -t ed25519 -N "" -f /etc/ssh/ssh_host_ed25519_key
+  systemctl start sshd
+}
+export -f debian_sshd_regenkey
+
+debian_sshd_init() {
+    apt -y install openssh-server
+    # dpkg-reconfigure -f noninteractive openssh-server
+    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+    sed -i 's/#UseDNS.*/UseDNS no/g' /etc/ssh/sshd_config
+    sed -i 's/#MaxAuthTries.*/MaxAuthTries 3/g' /etc/ssh/sshd_config
+    sed -i 's/#Port.*/Port 60022/g' /etc/ssh/sshd_config
+    sed -i 's/GSSAPIAuthentication.*/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
+    (grep -v -E "^Ciphers|^MACs" /etc/ssh/sshd_config ; echo "Ciphers aes256-ctr,aes192-ctr,aes128-ctr"; echo "MACs    hmac-sha1"; ) | tee /etc/ssh/sshd_config.bak
+    mv /etc/ssh/sshd_config.bak /etc/ssh/sshd_config
+    # root login only prikey
+    echo "PermitRootLogin without-password">> /etc/ssh/sshd_config
+
+    [ ! -d /root/.ssh ] && mkdir -m0700 /root/.ssh
+    cat <<EOF >/root/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKxdriiCqbzlKWZgW5JGF6yJnSyVtubEAW17mok2zsQ7al2cRYgGjJ5iFSvZHzz3at7QpNpRkafauH/DfrZz3yGKkUIbOb0UavCH5aelNduXaBt7dY2ORHibOsSvTXAifGwtLY67W4VyU/RBnCC7x3HxUB6BQF6qwzCGwry/lrBD6FZzt7tLjfxcbLhsnzqOG2y76n4H54RrooGn1iXHBDBXfvMR7noZKbzXAUQyOx9m07CqhnpgpMlGFL7shUdlFPNLPZf5JLsEs90h3d885OWRx9Kp+O05W2gPg4kUhGeqO6IY09EPOcTupw77PRHoWOg4xNcqEQN2v2C1lr09Y9 root@yinzh
+EOF
+    chmod 0600 /root/.ssh/authorized_keys
+}
+export -f debian_sshd_init
+
+debian_zswap_init() {
+    local zram_size=$1
+    apt -y install udisks2
+    # "Enable udisk2 ${zram_size}M zram swap"
+    mkdir -p /usr/local/lib/zram.conf.d/
+    (grep -v -E "zram" /etc/modules; echo "zram"; ) | tee /etc/modules
+    cat << EOF > /usr/local/lib/zram.conf.d/zram0-env
+ZRAM_NUM_STR=lzo
+ZRAM_DEV_SIZE=$((${zram_size}*1024*1024))
+SWAP=y
+EOF
+}
+export -f debian_zswap_init
+
+debian_vim_init() {
+    apt -y install vim
+    cat <<'EOF' > /etc/vim/vimrc.local
+syntax on
+" color evening
+set number
+set nowrap
+set fileencodings=utf-8,gb2312,gbk,gb18030
+" set termencoding=utf-8
+let &termencoding=&encoding
+set fileformats=unix
+set hlsearch                 " highlight the last used search pattern
+set noswapfile
+set tabstop=4                " 设置tab键的宽度
+set shiftwidth=4             " 换行时行间交错使用4个空格
+set expandtab                " 用space替代tab的输入
+set autoindent               " 自动对齐
+set backspace=2              " 设置退格键可用
+set cindent shiftwidth=4     " 自动缩进4空格
+set smartindent              " 智能自动缩进
+"Paste toggle - when pasting something in, don't indent.
+set pastetoggle=<F7>
+set mouse=r
+"disable .viminfo file
+set viminfo=
+let g:is_bash=1
+
+"新建.py,.sh文件，自动插入文件头"
+autocmd BufNewFile *.py,*.c,*.sh,*.h exec ":call SetTitle()"
+"定义函数SetTitle，自动插入文件头"
+func SetTitle()
+    if expand ("%:e") == 'sh'
+        call setline(1, "#!/usr/bin/env bash")
+        call setline(2, "readonly DIRNAME=\"$(readlink -f \"$(dirname \"$0\")\")\"")
+        call setline(3, "readonly SCRIPTNAME=${0##*/}")
+        call setline(4, "if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then")
+        call setline(5, "    exec 5> \"${DIRNAME}/$(date '+%Y%m%d%H%M%S').${SCRIPTNAME}.debug.log\"")
+        call setline(6, "    BASH_XTRACEFD=\"5\"")
+        call setline(7, "    export PS4='[\\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'")
+        call setline(8, "    set -o xtrace")
+        call setline(9, "fi")
+        call setline(10, "VERSION+=()")
+        call setline(11, "[ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true")
+        call setline(12, "################################################################################")
+        call setline(13, "usage() {")
+        call setline(14, "    [ \"$#\" != 0 ] && echo \"$*\"")
+        call setline(15, "    cat <<EOF")
+        call setline(16, "${SCRIPTNAME}")
+        call setline(17, "        -q|--quiet")
+        call setline(18, "        -l|--log <int> log level")
+        call setline(19, "        -V|--version")
+        call setline(20, "        -d|--dryrun dryrun")
+        call setline(21, "        -h|--help help")
+        call setline(22, "EOF")
+        call setline(23, "    exit 1")
+        call setline(24, "}")
+        call setline(25, "main() {")
+        call setline(26, "    local opt_short=\"\"")
+        call setline(27, "    local opt_long=\"\"")
+        call setline(28, "    opt_short+=\"ql:dVh\"")
+        call setline(29, "    opt_long+=\"quite,log:,dryrun,version,help\"")
+        call setline(30, "    __ARGS=$(getopt -n \"${SCRIPTNAME}\" -o ${opt_short} -l ${opt_long} -- \"$@\") || usage")
+        call setline(31, "    eval set -- \"${__ARGS}\"")
+        call setline(32, "    while true; do")
+        call setline(33, "        case \"$1\" in")
+        call setline(34, "            ########################################")
+        call setline(35, "            -q | --quiet)   shift; QUIET=1;;")
+        call setline(36, "            -l | --log)     shift; set_loglevel ${1}; shift;;")
+        call setline(37, "            -d | --dryrun)  shift; DRYRUN=1;;")
+        call setline(38, "            -V | --version) shift; for _v in \"${VERSION[@]}\"; do echo \"$_v\"; done; exit 0;;")
+        call setline(39, "            -h | --help)    shift; usage;;")
+        call setline(40, "            --)             shift; break;;")
+        call setline(41, "            *)              usage \"Unexpected option: $1\";;")
+        call setline(42, "        esac")
+        call setline(43, "    done")
+        call setline(44, "    return 0")
+        call setline(45, "}")
+        call setline(46, "main \"$@\"")
+    endif
+    if expand ("%:e") == 'py'
+        call setline(1, "#!/usr/bin/env python3")
+        call setline(2, "# -*- coding: utf-8 -*- ")
+        call setline(3, "")
+        call setline(4, "def main():")
+        call setline(5, "    return 0")
+        call setline(6, "")
+        call setline(7, "if __name__ == '__main__':")
+        call setline(8, "    main()")
+    endif
+endfunc
+EOF
+    sed -i "/mouse=a/d" /usr/share/vim/vim81/defaults.vim
+}
+export -f debian_vim_init
+
+debian_locale_init() {
+    #dpkg-reconfigure locales
+    sed -i "s/^# *zh_CN.UTF-8/zh_CN.UTF-8/g" /etc/locale.gen
+    locale-gen
+    echo -e 'LANG="zh_CN.UTF-8"\nLANGUAGE="zh_CN:zh"\nLC_ALL="zh_CN.UTF-8"\n' > /etc/default/locale
+    #echo "Asia/Shanghai" > /etc/timezone
+    ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    dpkg-reconfigure -f noninteractive tzdata
+}
+export -f debian_locale_init
+
+debian_root_chpasswd() {
+    local password=$1
+    usermod -p "$(echo ${password} | openssl passwd -1 -stdin)" root
+    # echo "root:${password}" |chpasswd
+    # Force Users To Change Their Passwords Upon First Login
+    chage -d 0 root
+}
+export -f debian_root_chpasswd
+
+autologin_root() {
+    # auto login as root
+    sed -i "s|#NAutoVTs=6|NAutoVTs=1|" /etc/systemd/logind.conf
+    mkdir -p /etc/systemd/system/getty@tty1.service.d
+    cat <<EOF | tee /etc/systemd/system/getty@tty1.service.d/override.conf
+    [Service]
+        ExecStart=
+        ExecStart=-/sbin/agetty --autologin root --noclear %I 38400 linux
+EOF
+    systemctl enable getty@tty1.service
+}
+export -f autologin_root
