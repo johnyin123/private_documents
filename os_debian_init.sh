9c28296fc33c36ce386682cde2913a25f10668b8
johnyin <johnyin.news@163.com>
2021-04-16 14:16:27 +0800

    fix

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("os_debian_init.sh - 10a578c - 2021-04-08T09:43:43+08:00")
+VERSION+=("os_debian_init.sh - 4d6de0d - 2021-04-14T14:05:03+08:00")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -486,6 +486,7 @@ do
 done
 EOF
   } > ${home}
+    chown ${user}:${user} ${home}
 }
 export -f debian_bash_init
 
