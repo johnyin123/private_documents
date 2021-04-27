856971a7ef17c57d9d278946d4b9da48e272e7b5
johnyin <johnyin.news@163.com>
2021-04-27 08:59:33 +0800

    fix disk_bootup.sh

--- kvm/disk_bootup.sh
+++ kvm/disk_bootup.sh
@@ -7,18 +7,19 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("disk_bootup.sh - c9fa17c - 2021-04-25T06:36:55+08:00")
+VERSION+=("disk_bootup.sh - 84b78f7 - 2021-04-26T14:54:19+08:00")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true
 ################################################################################
 usage() {
     [ "$#" != 0 ] && echo "$*"
     cat <<EOF
 ${SCRIPTNAME}
-        -c|--cpu          number of cpus (default 1)
-        -m|--mem          mem size MB (default 2048)
-        -D|--disk    *    disk image
-        -b|--bridge  *    host net bridge
-        -f|--fmt          disk image format(default raw)
+        -c|--cpu    <int>     number of cpus (default 1)
+        -m|--mem    <int>     mem size MB (default 2048)
+        -D|--disk   <file> *  disk image
+        -b|--bridge <br>   *  host net bridge
+        -f|--fmt    <fmt>     disk image format(default raw)
+        --cdrom     <iso>     iso file
         -q|--quiet
         -l|--log <int> log level
         -V|--version
@@ -28,9 +29,9 @@ EOF
     exit 1
 }
 main() {
-    local cpu=1 mem=2048 disk= bridge= fmt=raw
+    local cpu=1 mem=2048 disk= bridge= fmt=raw cdrom=
     local opt_short="c:m:D:b:f:"
-    local opt_long="cpu:,mem:,disk:,bridge:,fmt:,"
+    local opt_long="cpu:,mem:,disk:,bridge:,fmt:,cdrom:,"
     opt_short+="ql:dVh"
     opt_long+="quite,log:,dryrun,version,help"
     __ARGS=$(getopt -n "${SCRIPTNAME}" -o ${opt_short} -l ${opt_long} -- "$@") || usage
@@ -42,6 +43,7 @@ main() {
             -D | --disk)    shift; disk=${1}; shift;;
             -b | --bridge)  shift; bridge=${1}; shift;;
             -f | --fmt)     shift; fmt=${1}; shift;;
+            --cdrom)        shift; cdrom=${1}; shift;;
             ########################################
             -q | --quiet)   shift; QUIET=1;;
             -l | --log)     shift; set_loglevel ${1}; shift;;
@@ -58,13 +60,14 @@ main() {
     file_exists "${disk}" || usage "disk nofound"
     bridge_exists "${bridge}" || usage "bridge nofound"
 
-    try mkdir -p /etc/qemu/
+    directory_exists /etc/qemu/ || try mkdir -p /etc/qemu/
     grep "\s*allow\s*all" /etc/qemu/bridge.conf || {
         try "echo 'allow all' >> /etc/qemu/bridge.conf"
         try chmod 640 /etc/qemu/bridge.conf
     }
 
     try qemu-system-x86_64 -enable-kvm -cpu kvm64 -smp ${cpu} -m ${mem} \
+        ${cdrom:+-cdrom ${cdrom} -boot menu=on} \
         -drive file=${disk},index=0,cache=none,aio=threads,if=virtio,format=${fmt} \
         -netdev bridge,br=${bridge},id=net0 -device virtio-net-pci,netdev=net0
     return 0
