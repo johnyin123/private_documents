7ae95d27167dca9692e92737367f40171aa6eb45
johnyin <johnyin.news@163.com>
2021-09-29 08:55:46 +0800

    add presigned upload

--- ceph/s3_ceph_test.sh
+++ ceph/s3_ceph_test.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("s3_ceph_test.sh - ead2b83 - 2021-09-28T15:16:02+08:00")
+VERSION+=("s3_ceph_test.sh - c6d25c2 - 2021-09-28T15:19:57+08:00")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true
 ################################################################################
 usage() {
@@ -18,6 +18,7 @@ ${SCRIPTNAME}
         -s|--secret_key  *   secret_key
         -u|--url         *   s3 url
         -e|--expire <int>    presigned url expire seconds, default 600
+        --upload             presigned url upload, default is download
         -q|--quiet
         -l|--log <int> log level
         -V|--version
@@ -25,7 +26,9 @@ ${SCRIPTNAME}
         -h|--help help
        create bucket: ${SCRIPTNAME} -a key -s key -u url <bucket>
        upload:        ${SCRIPTNAME} -a key -s key -u url <localfile> name@bucket
+                      curl -X PUT -T <file> <presigned_url>
        download:      ${SCRIPTNAME} -a key -s key -u url name@bucket <localfile> 
+                      curl <presigned_url> <output>
 EOF
     exit 1
 }
@@ -102,28 +105,30 @@ presigned_url() {
     local expire=$4
     local secret_key=$5
     local access_key=$6
+    local method=${7:-GET}
     local uri="/${bucket}/${source}"
     local date=$(date -d "+${expire} second" +%s)
-    local str="GET\n\n\n${date}\n${uri}"
+    local str="${method}\n\n\n${date}\n${uri}"
     local signature=$(echo -en "${str}" | openssl sha1 -hmac "${secret_key}" -binary | base64)
     echo "${host}${uri}?AWSAccessKeyId=${access_key}&Expires=${date}&Signature=$(urlencode ${signature})"
 }
 
 
 main() {
-    local access_key="" secret_key="" s3_host="" srcfile="" bucket="" tgtfile="" expire=600
+    local access_key="" secret_key="" s3_host="" srcfile="" bucket="" tgtfile="" expire=600 method=GET
     local opt_short="a:s:u:e:"
-    local opt_long="access_key:,secret_key:,url:,expire:,"
+    local opt_long="access_key:,secret_key:,url:,expire:,upload,"
     opt_short+="ql:dVh"
     opt_long+="quiet,log:,dryrun,version,help"
     __ARGS=$(getopt -n "${SCRIPTNAME}" -o ${opt_short} -l ${opt_long} -- "$@") || usage
     eval set -- "${__ARGS}"
     while true; do
         case "$1" in
-            -a | --access_key) shift;  access_key=${1}; shift;;
-            -s | --secret_key) shift;  secret_key=${1}; shift;;
-            -u | --url)        shift;  s3_host=${1}; shift;;
-            -e | --expire)     shift;  expire=${1}; shift;;
+            -a | --access_key) shift; access_key=${1}; shift;;
+            -s | --secret_key) shift; secret_key=${1}; shift;;
+            -u | --url)        shift; s3_host=${1}; shift;;
+            -e | --expire)     shift; expire=${1}; shift;;
+            --upload)          shift; method=PUT;; 
             ########################################
             -q | --quiet)   shift; QUIET=1;;
             -l | --log)     shift; set_loglevel ${1}; shift;;
@@ -150,7 +155,7 @@ main() {
         elif [[ " $@" =~ .*?[[:space:]]([^[:space:]]+)@([^[:space:]]*)[[:space:]]* ]] ; then
             srcfile=${BASH_REMATCH[1]}
             bucket=${BASH_REMATCH[2]}
-            presigned_url "${s3_host}" ${bucket} "${srcfile}" "${expire}" ${secret_key} ${access_key}
+            presigned_url "${s3_host}" ${bucket} "${srcfile}" "${expire}" ${secret_key} ${access_key} ${method}
         else
             bucket=${1:-}
             [ -z ${bucket} ] && usage "bucket name"
