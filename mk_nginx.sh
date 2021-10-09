3006f9ae010f1ef51efdc016b2cfab54b82b0ccd
johnyin <johnyin.news@163.com>
2021-10-09 09:17:54 +0800

    fix static build nginx

--- mk_nginx.sh
+++ mk_nginx.sh
@@ -15,6 +15,10 @@ auto/lib/openssl/conf
  41             CORE_LIBS="$CORE_LIBS $OPENSSL/lib/libssl.a"
  42             CORE_LIBS="$CORE_LIBS $OPENSSL/lib/libcrypto.a"
 EOF
+# geoip static error
+# --with-http_geoip_module=dynamic \
+# --with-stream_geoip_module=dynamic \
+
 ./configure --prefix=/usr/share/nginx \
 --sbin-path=/usr/sbin/nginx \
 --conf-path=/etc/nginx/nginx.conf \
@@ -49,8 +53,7 @@ EOF
 --with-stream_realip_module \
 --with-stream_ssl_preread_module \
  \
---with-http_geoip_module=dynamic \
---with-stream_geoip_module=dynamic \
+${STATIC:+--with-ld-opt=-static} \
  \
 --with-openssl=${CURPATH}/openssl-1.1.1l \
 --with-pcre=${CURPATH}/pcre-8.39 \
