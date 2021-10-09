27b5847e429fc2efe23f9ba24b1fb3e2b26afbdc
johnyin <johnyin.news@163.com>
2021-10-09 16:27:44 +0800

    rm web-cam-http.sh

--- web-cam-http.sh
+++
@@ -1,3 +0,0 @@
-1.
-cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=WMV2,vb=800,acodec=wma2,ab=128,channels,samplerate=44100}:http{dst=0.0.0.0:8080/stream.asf}' --no-sout-all --sout-keep
-cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=WMV2,vb=400,wicodec=none}:http{dst=:8080/stream.asf}'
