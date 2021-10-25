0a457b3b190e1c664a744075bb0037984deedc4b
johnyin <johnyin.news@163.com>
2021-10-25 09:08:18 +0800

    fix

--- debian_tuning.sh
+++ debian_tuning.sh
@@ -7,10 +7,10 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("centos_tuning.sh - f238473 - 2021-10-19T17:22:42+08:00")
+VERSION+=("debian_tuning.sh - initversion - 2021-10-25T09:08:18+08:00")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true
 ################################################################################
-source ${DIRNAME}/os_centos_init.sh
+source ${DIRNAME}/os_debian_init.sh
 
 usage() {
     [ "$#" != 0 ] && echo "$*"
@@ -56,18 +56,20 @@ main() {
     done
     [ -z ${ssh} ] && usage "ssh must input"
     [ -z ${password} ]  || set_sshpass "${password}"
+    ssh_func "${ssh}" "${port}" debian_apt_init "bullseye"
+    ssh_func "${ssh}" "${port}" debian_limits_init
+    ssh_func "${ssh}" "${port}" debian_sysctl_init
+    ssh_func "${ssh}" "${port}" debian_sshd_regenkey
+    ssh_func "${ssh}" "${port}" debian_sshd_init
+    [ -z "${zswap}" ] || ssh_func "${ssh}" "${port}" debian_zswap_init2 ${zswap}
+    ssh_func "${ssh}" "${port}" debian_locale_init
+    ssh_func "${ssh}" "${port}" debian_bash_init root
     [ -r "${DIRNAME}/motd.sh" ] && {
-        try "cat ${DIRNAME}/motd.sh | ssh -p${port} ${ssh} 'cat >/etc/motd.sh'"
+        try "cat ${DIRNAME}/motd.sh | ssh -p${port} ${ssh} 'cat >/etc/update-motd.d/11-motd'"
+        ssh_func "${ssh}" "${port}" "touch /etc/logo.txt;chmod 755 /etc/update-motd.d/11-motd"
     }
-    ssh_func "${ssh}" "${port}" centos_limits_init
-    ssh_func "${ssh}" "${port}" centos_disable_selinux
-    ssh_func "${ssh}" "${port}" centos_sshd_init
-    ssh_func "${ssh}" "${port}" centos_disable_ipv6
-    ssh_func "${ssh}" "${port}" centos_service_init
-    ssh_func "${ssh}" "${port}" centos_sysctl_init
-    [ -z "${zswap}" ] || ssh_func "${ssh}" "${port}" centos_zswap_init ${zswap}
-    ssh_func "${ssh}" "${port}" "sed -i '/motd.sh/d' /etc/profile ; echo 'sh /etc/motd.sh' >> /etc/profile;touch /etc/logo.txt /etc/motd.sh"
     ssh_func "${ssh}" "${port}" "[ -z '${name}' ] || echo '${name}' > /etc/hostname"
+    ssh_func "${ssh}" "${port}" debian_minimum_init
     return 0
 }
 main "$@"
--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,8 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("os_debian_init.sh - 09383b7 - 2021-10-12T07:24:27+08:00")
-FAKE_APT="apt -y -oAcquire::http::User-Agent=dler --no-install-recommends"
+VERSION+=("os_debian_init.sh - 829112e - 2021-10-14T08:30:20+08:00")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -91,8 +90,8 @@ EOF
             echo "deb http://ftp.cn.debian.org/debian-security ${ver}-security main contrib"  >> /etc/apt/sources.list
             ;;
     esac
-    ${FAKE_APT} -oAcquire::AllowInsecureRepositories=true update || true
-    ${FAKE_APT} --allow-unauthenticated install deb-multimedia-keyring || true
+    apt -y -oAcquire::http::User-Agent=dler --no-install-recommends -oAcquire::AllowInsecureRepositories=true update || true
+    apt -y -oAcquire::http::User-Agent=dler --no-install-recommends --allow-unauthenticated install deb-multimedia-keyring || true
 }
 export -f debian_apt_init
 
@@ -151,7 +150,7 @@ debian_sshd_regenkey() {
 export -f debian_sshd_regenkey
 
 debian_sshd_init() {
-    ${FAKE_APT} install openssh-server
+    apt -y -oAcquire::http::User-Agent=dler --no-install-recommends install openssh-server
     # dpkg-reconfigure -f noninteractive openssh-server
     cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
     sed -i 's/#UseDNS.*/UseDNS no/g' /etc/ssh/sshd_config
@@ -207,12 +206,12 @@ EOF
     eval $(grep -E "^VERSION_CODENAME=" /etc/os-release)
     case "$VERSION_CODENAME" in
         buster)
-            ${FAKE_APT} update && ${FAKE_APT} install udisks2
+            apt -y -oAcquire::http::User-Agent=dler --no-install-recommends update && apt -y -oAcquire::http::User-Agent=dler --no-install-recommends install udisks2
             mkdir -p /usr/local/lib/zram.conf.d/
             cfg=/usr/local/lib/zram.conf.d/zram0-env
             ;;
         bullseye)
-            ${FAKE_APT} update && ${FAKE_APT} install udisks2-zram
+            apt -y -oAcquire::http::User-Agent=dler --no-install-recommends update && apt -y -oAcquire::http::User-Agent=dler --no-install-recommends install udisks2-zram
             mkdir -p /usr/lib/zram.conf.d/
             cfg=/usr/lib/zram.conf.d/zram0
             ;;
@@ -227,7 +226,7 @@ EOF
 export -f debian_zswap_init
 
 debian_vim_init() {
-    ${FAKE_APT} install vim
+    apt -y -oAcquire::http::User-Agent=dler --no-install-recommends install vim
     cat <<'EOF' > /etc/vim/vimrc.local
 syntax on
 " color evening
