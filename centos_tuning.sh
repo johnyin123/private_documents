2b749894a6ef8e220e9942fc03a4c17e5daacec1
johnyin <johnyin.news@163.com>
2021-11-12 13:07:34 +0800

    fix

--- centos_tuning.sh
+++ centos_tuning.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("centos_tuning.sh - f238473 - 2021-10-19T17:22:42+08:00")
+VERSION+=("centos_tuning.sh - 0a457b3 - 2021-10-25T09:08:18+08:00")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true
 ################################################################################
 source ${DIRNAME}/os_centos_init.sh
@@ -57,7 +57,8 @@ main() {
     [ -z ${ssh} ] && usage "ssh must input"
     [ -z ${password} ]  || set_sshpass "${password}"
     [ -r "${DIRNAME}/motd.sh" ] && {
-        try "cat ${DIRNAME}/motd.sh | ssh -p${port} ${ssh} 'cat >/etc/motd.sh'"
+        try "cat ${DIRNAME}/motd.sh | ssh -p${port} ${ssh} 'cat >/etc/profile.d/motd.sh'"
+        ssh_func "${ssh}" "${port}" "touch /etc/logo.txt"
     }
     ssh_func "${ssh}" "${port}" centos_limits_init
     ssh_func "${ssh}" "${port}" centos_disable_selinux
@@ -66,7 +67,6 @@ main() {
     ssh_func "${ssh}" "${port}" centos_service_init
     ssh_func "${ssh}" "${port}" centos_sysctl_init
     [ -z "${zswap}" ] || ssh_func "${ssh}" "${port}" centos_zswap_init ${zswap}
-    ssh_func "${ssh}" "${port}" "sed -i '/motd.sh/d' /etc/profile ; echo 'sh /etc/motd.sh' >> /etc/profile;touch /etc/logo.txt /etc/motd.sh"
     ssh_func "${ssh}" "${port}" "[ -z '${name}' ] || echo '${name}' > /etc/hostname"
     return 0
 }
