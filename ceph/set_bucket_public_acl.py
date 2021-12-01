ed7c08833088be5c8d321d96c2e6eee349b5219e
johnyin <johnyin.news@163.com>
2021-12-02 07:51:09 +0800

    fix

--- ceph/set_bucket_public_acl.py
+++ ceph/set_bucket_public_acl.py
@@ -1,6 +1,61 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Operation                          Permission
+# s3:GetObject                       READ
+# s3:GetObjectTorrent                READ
+# s3:GetObjectVersion                READ
+# s3:GetObjectVersionTorrent         READ
+# s3:GetObjectTagging                READ
+# s3:GetObjectVersionTagging         READ
+# s3:ListAllMyBuckets                READ
+# s3:ListBucket                      READ
+# s3:ListBucketMultipartUploads      READ
+# s3:ListBucketVersions              READ
+# s3:ListMultipartUploadParts        READ
+# s3:AbortMultipartUpload            WRITE
+# s3:CreateBucket                    WRITE
+# s3:DeleteBucket                    WRITE
+# s3:DeleteObject                    WRITE
+# s3:s3DeleteObjectVersion           WRITE
+# s3:PutObject                       WRITE
+# s3:PutObjectTagging                WRITE
+# s3:PutObjectVersionTagging         WRITE
+# s3:DeleteObjectTagging             WRITE
+# s3:DeleteObjectVersionTagging      WRITE
+# s3:RestoreObject                   WRITE
+# s3:GetAccelerateConfiguration      READ_ACP
+# s3:GetBucketAcl                    READ_ACP
+# s3:GetBucketCORS                   READ_ACP
+# s3:GetBucketLocation               READ_ACP
+# s3:GetBucketLogging                READ_ACP
+# s3:GetBucketNotification           READ_ACP
+# s3:GetBucketPolicy                 READ_ACP
+# s3:GetBucketRequestPayment         READ_ACP
+# s3:GetBucketTagging                READ_ACP
+# s3:GetBucketVersioning             READ_ACP
+# s3:GetBucketWebsite                READ_ACP
+# s3:GetLifecycleConfiguration       READ_ACP
+# s3:GetObjectAcl                    READ_ACP
+# s3:GetObjectVersionAcl             READ_ACP
+# s3:GetReplicationConfiguration     READ_ACP
+# s3:DeleteBucketPolicy              WRITE_ACP
+# s3:DeleteBucketWebsite             WRITE_ACP
+# s3:DeleteReplicationConfiguration  WRITE_ACP
+# s3:PutAccelerateConfiguration      WRITE_ACP
+# s3:PutBucketAcl                    WRITE_ACP
+# s3:PutBucketCORS                   WRITE_ACP
+# s3:PutBucketLogging                WRITE_ACP
+# s3:PutBucketNotification           WRITE_ACP
+# s3:PutBucketPolicy                 WRITE_ACP
+# s3:PutBucketRequestPayment         WRITE_ACP
+# s3:PutBucketTagging                WRITE_ACP
+# s3:PutPutBucketVersioning          WRITE_ACP
+# s3:PutBucketWebsite                WRITE_ACP
+# s3:PutLifecycleConfiguration       WRITE_ACP
+# s3:PutObjectAcl                    WRITE_ACP
+# s3:PutObjectVersionAcl             WRITE_ACP
+# s3:PutReplicationConfiguration     WRITE_ACP
 import boto3
 import json
 
