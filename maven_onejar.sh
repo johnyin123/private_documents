ad44b2558cbb450f46f11cafe1baa4e65f4c8e1b
johnyin <johnyin.news@163.com>
2021-12-02 12:42:16 +0800

    fix

--- maven_onejar.sh
+++ maven_onejar.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
 readonly SCRIPTNAME=${0##*/}
-VERSION+=("initver[2021-12-02T12:12:11+08:00]:maven_onejar.sh")
+VERSION+=("8aac1b0[2021-12-02T12:12:11+08:00]:maven_onejar.sh")
 main() {
+    local app=${1:-myapp}
     echo "REPOSITORY: ~/.m2/repository/"
-    mvn archetype:generate -DgroupId=com.mytest -DartifactId=myapp -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+    mvn archetype:generate -DgroupId=com.mytest -DartifactId=${app} -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
     cat <<'EOF'
   <properties>
     <maven.compiler.source>14</maven.compiler.source>
     <maven.compiler.target>14</maven.compiler.target>
     <junit.version>3.8.1</junit.version>
-    <httpclient.version>4.5.3</httpclient.version>
+    <httpclient.version>3.1</httpclient.version>
   </properties>
   <dependencies>
     <dependency>
@@ -20,8 +21,8 @@ main() {
       <scope>test</scope>
     </dependency>
     <dependency>
-      <groupId>org.apache.httpcomponents</groupId>
-      <artifactId>httpclient</artifactId>
+      <groupId>commons-httpclient</groupId>
+      <artifactId>commons-httpclient</artifactId>
       <version>${httpclient.version}</version>
     </dependency>
   </dependencies>
@@ -54,7 +55,6 @@ main() {
       </plugin>
     </plugins>
   </build>
-
 EOF
     mvn package
     return 0
