97de1e0eb2cbaa40f4416549a99ccaf5626667e4
johnyin <johnyin.news@163.com>
2021-12-27 10:10:30 +0800

    fix

--- nginx/ngx_demo.sh
+++ nginx/ngx_demo.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("4fbd12f[2021-12-24T10:39:19+08:00]:ngx_demo.sh")
+VERSION+=("a37c788[2021-12-27T09:22:29+08:00]:ngx_demo.sh")
 
 set -o errtrace
 set -o nounset
@@ -1629,6 +1629,13 @@ server {
         resolver 127.0.0.1 ipv6=off;
         # use proxy_pass with variables and the resolver directive to force nginx to resolve names at run-time;
         set $target http://proxytarget.example.com;
+        proxy_pass $target;
+    }
+    location /proxypass {
+        # echo "www.test.com 192.168.168.1" > dnsd.conf
+        # busybox dnsd -c dnsd.conf -v -s
+        resolver 127.0.0.1 ipv6=off;
+        set $target http://www.test.com:9999;
         proxy_pass $target;
     }
 }
