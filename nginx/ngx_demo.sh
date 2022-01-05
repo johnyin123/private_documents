ccddc77c3ef7b2e991efbdd50540fae4c260d541
johnyin <johnyin.news@163.com>
2022-01-05 15:02:10 +0800

    fix

--- nginx/ngx_demo.sh
+++ nginx/ngx_demo.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("c5a699d[2022-01-05T12:43:31+08:00]:ngx_demo.sh")
+VERSION+=("e7ac0f4[2022-01-05T13:31:51+08:00]:ngx_demo.sh")
 
 set -o errtrace
 set -o nounset
@@ -32,6 +32,19 @@ nginx -t 2>&1 && {
 rm -f ${target}
 EOF
 chmod 755 check_conf.sh
+cat <<'EOF'>ssl_client_cert.conf
+# curl -k --key client_test.key --cert client_test.crt --cacert ca.crt  https://localhost/
+server {
+    listen 443 ssl reuseport;
+    server_name _;
+    ssl_certificate /etc/nginx/www.test.com.crt;
+    ssl_certificate_key /etc/nginx/www.test.com.key;
+    ssl_dhparam /etc/nginx/dh2048.pem;
+    ssl_client_certificate /etc/nginx/ca.crt;
+    ssl_verify_client on;
+    location / { return 200 "$ssl_client_s_dn"; }
+}
+EOF
 cat <<"EOF">location.txt
 git clone https://github.com/nginx/nginx-tests.git
 =：精确匹配，优先级最高。如果找到了这个精确匹配，则停止查找。
