32a4aa7f4a267dcd7453db0d7a4ed7aec4334af6
johnyin <johnyin.news@163.com>
2022-02-08 13:15:20 +0800

    new impl ssl

---
+++ nginx/newssl.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+readonly SCRIPTNAME=${0##*/}
+if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
+    exec 5> "${DIRNAME}/$(date '+%Y%m%d%H%M%S').${SCRIPTNAME}.debug.log"
+    BASH_XTRACEFD="5"
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+VERSION+=("initver[2022-02-08T13:15:20+08:00]:newssl.sh")
+[ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || { echo '**ERROR: functions.sh nofound!'; exit 1; }
+################################################################################
+usage() {
+    [ "$#" != 0 ] && echo "$*"
+    cat <<EOF
+${SCRIPTNAME}
+        -i|--init   <str> *  init ca, sign a server cert with DN=<str>
+        -c|--client <str>  * create client cert keys
+        --caroot             CA root(default ${DIRNAME}/ca)
+        -q|--quiet
+        -l|--log <int> log level
+        -V|--version
+        -d|--dryrun dryrun
+        -h|--help help
+            apt -y install gnutls-bin
+            yum -y install gnutls-utils
+EOF
+    exit 1
+}
+
+init_ca() {
+    local caroot=${1}
+    local dn=${2}
+    [ -d "${caroot}" ] && return 1
+    try mkdir -p ${caroot}
+    info_msg "creating ca key\n"
+    try openssl genrsa -out ${caroot}/ca.key 2048
+    info_msg "creating ca cert\n"
+    try openssl req -new -x509 -days $((365*5)) -key ${caroot}/ca.key \
+        -out ${caroot}/ca.pem -utf8 -subj \"/C=CN/L=LN/O=${dn}/CN=self sign root ca\"
+    try openssl x509 -text -noout -in ${caroot}/ca.pem
+}
+
+gen_clent_cert() {
+    local caroot=${1}
+    local cid=${2}
+    [ -e "${caroot}/${cid}.csr" ] && return 1
+    info_msg "create key\n"
+    try openssl genrsa -out ${caroot}/${cid}.key 2048
+    info_msg "create certificate signing request (csr)\n"
+    try openssl req -new -key ${caroot}/${cid}.key -out ${caroot}/${cid}.csr \
+        -utf8 -subj \"/C=CN/L=LN/O=mycompany/CN=${cid}\"
+    info_msg "signing our certificate with my ca"
+    try openssl x509 -req -days $((365*5)) -in ${caroot}/${cid}.csr \
+        -CA ${caroot}/ca.pem -CAkey ${caroot}/ca.key -CAcreateserial -out ${caroot}/${cid}.pem
+    try openssl x509 -text -noout -in ${caroot}/${cid}.pem
+    # info_msg "conver to broswer support format(p12).\n"
+    # try openssl pkcs12 -export -in ${caroot}/${cid}.pem -out ${caroot}/${cid}.p12 -inkey ${caroot}/${cid}.key
+    try "tar -C ${caroot} -cv ca.pem ${cid}.key ${cid}.pem | gzip > ${caroot}/${cid}.tar.gz"
+    info_msg "${caroot}/${cid}.tar.gz --> TO CLIENT\n"
+}
+
+main() {
+    local init="" client=""  caroot="${DIRNAME}/ca"
+    local opt_short="i:c:"
+    local opt_long="init:,client:,caroot:,"
+    opt_short+="ql:dVh"
+    opt_long+="quiet,log:,dryrun,version,help"
+    __ARGS=$(getopt -n "${SCRIPTNAME}" -o ${opt_short} -l ${opt_long} -- "$@") || usage
+    eval set -- "${__ARGS}"
+    while true; do
+        case "$1" in
+            -i | --init)    shift; init=${1}; shift;;
+            -c | --client)  shift; client=${1}; shift;;
+            --caroot)       shift; caroot=${1}; shift;;
+            ########################################
+            -q | --quiet)   shift; QUIET=1;;
+            -l | --log)     shift; set_loglevel ${1}; shift;;
+            -d | --dryrun)  shift; DRYRUN=1;;
+            -V | --version) shift; for _v in "${VERSION[@]}"; do echo "$_v"; done; exit 0;;
+            -h | --help)    shift; usage;;
+            --)             shift; break;;
+            *)              usage "Unexpected option: $1";;
+        esac
+    done
+    [ -z "${init}" ] || {
+        init_ca "${caroot}" "${init}" || exit_msg "${caroot} dir exists\n"
+    }
+    [ -z "${client}" ] || {
+        info_msg "generate client [${client}] cert\n"
+        gen_clent_cert "${caroot}" "${client}" || {
+            retval=$?
+            error_msg "generate client [${client}] cert error[${retval}]\n"
+        }
+        info_msg "conver to broswer support format.\n"
+        info_msg "openssl pkcs12 -export -clcerts -in ${client}.pem -inkey ${client}.key -out ${client}.p12\n"
+    }
+    info_msg "ALL DONE\n"
+    return 0
+}
+main "$@"
