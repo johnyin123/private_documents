fdd544c63e28b7dfd459a93b46a40621212c6a4d
johnyin <johnyin.news@163.com>
2022-04-15 10:11:38 +0800

    change s905_autoscript

--- s905_debootstrap.sh
+++ s905_debootstrap.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("ea0b49e[2022-04-14T21:12:27+08:00]:s905_debootstrap.sh")
+VERSION+=("4b77ced[2022-04-14T21:30:11+08:00]:s905_debootstrap.sh")
 ################################################################################
 source ${DIRNAME}/os_debian_init.sh
 
@@ -938,12 +938,6 @@ prepair_disk() {
 
     mke2fs -FL "${OVERLAY_LABEL}" -t ext4 -E lazy_itable_init,lazy_journal_init ${PART_OVERLAY}
 }
-gen_uEnv_ini() {
-    cat > /boot/uEnv.ini <<EOF
-dtb_name=/dtb/meson-gxl-s905d-phicomm-n1.dtb
-bootargs=root=LABEL=${ROOT_LABEL} rootflags=rw fsck.fix=yes fsck.repair=yes net.ifnames=0 console=ttyAML0,115200n8 console=tty1 no_console_suspend consoleblank=0
-EOF
-}
 gen_s905_emmc_autoscript() {
     cat > /boot/emmc_autoscript <<'EOF'
 setenv env_addr "0x10400000"
@@ -975,9 +969,15 @@ setenv kernel_addr  "0x11000000"
 setenv initrd_addr  "0x13000000"
 setenv dtb_mem_addr "0x1000000"
 setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_mem_addr}
-if fatload usb 0 ${kernel_addr} zImage; then if fatload usb 0 ${initrd_addr} uInitrd; then if fatload usb 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};fi; if fatload usb 0 ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
-if fatload usb 1 ${kernel_addr} zImage; then if fatload usb 1 ${initrd_addr} uInitrd; then if fatload usb 1 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};fi; if fatload usb 1 ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
-if fatload mmc 0 ${kernel_addr} zImage; then if fatload mmc 0 ${initrd_addr} uInitrd; then if fatload mmc 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};fi; if fatload mmc 0 ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
+if fatload usb 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize}; if fatload usb 0 ${kernel_addr} ${image}; then if fatload usb 0 ${initrd_addr} ${initrd}; then if fatload usb 0 ${dtb_mem_addr} ${dtb}; then run boot_start; fi; fi; fi; fi;
+if fatload usb 1 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize}; if fatload usb 1 ${kernel_addr} ${image}; then if fatload usb 1 ${initrd_addr} ${initrd}; then if fatload usb 1 ${dtb_mem_addr} ${dtb}; then run boot_start; fi; fi; fi; fi;
+if fatload mmc 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize}; if fatload mmc 0 ${kernel_addr} ${image}; then if fatload mmc 0 ${initrd_addr} ${initrd}; then if fatload mmc 0 ${dtb_mem_addr} ${dtb}; then run boot_start; fi; fi; fi; fi;
+EOF
+    cat > /boot/uEnv.ini <<EOF
+image=vmlinuz
+initrd=uInitrd
+dtb=/dtb/meson-gxl-s905d-phicomm-n1.dtb
+bootargs=root=LABEL=${ROOT_LABEL} rootflags=rw fsck.fix=yes fsck.repair=yes net.ifnames=0 console=ttyAML0,115200n8 console=tty1 no_console_suspend consoleblank=0
 EOF
 }
 
