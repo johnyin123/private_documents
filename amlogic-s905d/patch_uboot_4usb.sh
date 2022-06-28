3aa5b93e16207904466ed85edc93996a457f8aaa
johnyin <johnyin.news@163.com>
2022-06-29 07:38:11 +0800

    fix patch_uboot_4usb.sh

--- amlogic-s905d/patch_uboot_4usb.sh
+++ amlogic-s905d/patch_uboot_4usb.sh
@@ -16,4 +16,12 @@ xxd -p -c 2127800 u-boot.bin \
     | sed "s/$(echo -n ${bootcmd_mmc2} | xxd -p -c 500)/$(echo -n ${bootcmd_usb1} | xxd -p -c 500)/g" \
     | xxd -p -r > u-boot.usb.bin
 
+new_tgts1="boot_targets=pxe mmc0 mmc1 usb0 usb1 dhcp"
+new_tgts2="boot_targets=pxe mmc0 mmc1 usb0 usb1 romusb dhcp"
+xxd -p -c 2127800 u-boot.bin \
+    | sed "s/$(echo -n ${org_tgts1} | xxd -p -c 500)/$(echo -n ${new_tgts1} | xxd -p -c 500)/g" \
+    | sed "s/$(echo -n ${org_tgts2} | xxd -p -c 500)/$(echo -n ${new_tgts2} | xxd -p -c 500)/g" \
+    | sed "s/$(echo -n ${bootcmd_mmc2} | xxd -p -c 500)/$(echo -n ${bootcmd_usb1} | xxd -p -c 500)/g" \
+    | xxd -p -r > u-boot.mmc.bin
+
 # ethaddr
