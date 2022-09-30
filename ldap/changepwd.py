86060734215105e3f84019b29eb0029594b6453b
johnyin <johnyin.news@163.com>
2022-09-30 08:39:29 +0800

    fix

--- ldap/changepwd.py
+++ ldap/changepwd.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# python3 -m venv ldap3
+# pip install ldap3
+
 from ldap3 import Server, Connection, ALL, MODIFY_REPLACE
-# Server(host=test_server,
-#  use_ssl=use_ssl,
-#  port=test_port_ssl if use_ssl else test_port,
-#  allowed_referral_hosts=('*', True),
-#  get_info=get_info,
-#  mode=test_server_mode)
-s = Server('127.0.0.1', get_info=ALL)
-c = Connection(s, user='uid=user1,ou=people,dc=xikang,dc=com', password='password')
-c.bind()
-changes = {"userPassword": [(MODIFY_REPLACE, '111111')]}
-c.modify('uid=user1,ou=people,dc=xikang,dc=com', changes)
-print(c.result)
-c.unbind()
+
+def init_connection(url, binddn, password):
+    srv = Server(url, get_info=ALL)
+    conn = Connection(srv, user=binddn, password=password)
+    conn.bind()
+    return conn
+
+def search():
+    c=init_connection('ldaps://127.0.0.1:636', 'uid=user1,ou=people,dc=xikang,dc=com', '111111')
+    c.search('ou=people,dc=xikang,dc=com', '(&(objectclass=posixAccount)(uid=user2))', attributes=['*'])
+    for entry in c.entries:
+        print(entry)
+    c.unbind()
+
+def modify():
+    c=init_connection('ldaps://127.0.0.1:636', 'uid=user1,ou=people,dc=xikang,dc=com', '111111')
+    changes = {"userPassword": [(MODIFY_REPLACE, '111111')]}
+    c.modify('uid=user2,ou=people,dc=xikang,dc=com', changes)
+    print(c.result)
+    c.unbind()
+
+search()
+modify()
