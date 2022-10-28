ecf13b8a35aaab92cdb3fda28af90289bfb17779
johnyin <johnyin.news@163.com>
2022-10-28 10:56:16 +0800

    fix INST_ARCH nodefine error

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("74ff91a[2022-10-27T13:24:16+08:00]:os_debian_init.sh")
+VERSION+=("e8c35d4[2022-10-28T07:36:08+08:00]:os_debian_init.sh")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -32,7 +32,7 @@ debian_build() {
     [ -d "${root_dir}" ] || mkdir -p ${root_dir}
     debootstrap --verbose ${cache_dir:+--cache-dir=${cache_dir}} --no-check-gpg --arch ${INST_ARCH:-amd64} --variant=minbase --include=${include_pkg} --foreign ${DEBIAN_VERSION:-buster} ${root_dir} ${REPO:-http://mirrors.163.com/debian}
 
-    [ ${INST_ARCH} = "arm64" ] && {
+    [ ${INST_ARCH:-amd64} = "arm64" ] && {
         [ -e "/usr/bin/qemu-aarch64-static" ] || { echo "Need: apt install qemu-user-static"; return 1; }
         cp /usr/bin/qemu-aarch64-static ${root_dir}/usr/bin/
     }
