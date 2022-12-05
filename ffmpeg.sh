990f855b2c720c32d7ceda0e412b33a2a2baaef4
johnyin <johnyin.news@163.com>
2022-12-05 14:39:33 +0800

    fix

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -7,18 +7,19 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("79c342f[2022-12-02T08:07:46+08:00]:ffmpeg.sh")
+VERSION+=("983b3d0[2022-12-02T09:31:33+08:00]:ffmpeg.sh")
 ################################################################################
 
-name=${1:?input err}
-scale=${scale:-iw/2:ih/2}
-frame=${frame:-}
+name=${1:?input err scale= $0 video.mkv}
+scale=${scale-iw/2:ih/2}
+frame=${frame-}
 
 ffmpeg -i ${name} 2>&1 | grep Stream
+ffmpeg -hide_banner -i ${name} 2>&1 | grep -o -Ei "Video:\s*([^ ]*)"
 
 # pv -f -F 'Converted: %b Elapsed: %t Current: %r Average: %a %p %e' "${name}" | ffmpeg -i pipe:0 \
 ffmpeg -hwaccel auto -i ${name} -v error -map 0:a:? -map 0:s:? -map 0:v:? \
-    -vf scale=${scale} -c:v libx264 -crf 18  \
+    ${scale:+-vf scale=${scale}} -c:v libx264 -crf 18  \
     ${frame:+-filter:v fps=fps=${frame}} -preset slow \
     -c:a copy -c:s copy ${name%.*}.convert.${name##*.}
 
