1e31d6cfafeb457c41552ad1960eafac7dc3000f
johnyin <johnyin.news@163.com>
2023-01-10 13:38:20 +0800

    build_centos_no_kernel.sh add shim-x64 package

--- custom_os/build_centos_no_kernel.sh
+++ custom_os/build_centos_no_kernel.sh
@@ -7,13 +7,13 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("initver[2023-01-06T14:50:41+08:00]:build_centos_no_kernel.sh")
+VERSION+=("c518be6[2023-01-06T14:50:41+08:00]:build_centos_no_kernel.sh")
 [ -e ${DIRNAME}/os_centos_init.sh ] && . ${DIRNAME}/os_centos_init.sh || { echo '**ERROR: os_centos_init.sh nofound!'; exit 1; }
 ################################################################################
 log() { echo "######$*" >&2; }
 export -f log
 
-PKG="grub2-common grub2-tools-minimal grub2-tools-extra grub2-efi-x64 grub2-pc-modules grub2-tools grub2-pc grub2 xfsprogs biosdevname"
+PKG="grub2-common grub2-tools-minimal grub2-tools-extra grub2-efi-x64 grub2-pc-modules grub2-tools grub2-pc grub2 shim-x64 xfsprogs biosdevname"
 PKG+=" iputils openssh-server rsync openssh-clients net-tools"
 PKG+=" $*"
 
