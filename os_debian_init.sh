6f534d0f67263d3c1a11361685ac4484fdd8a750
johnyin <johnyin.news@163.com>
2023-01-13 07:36:13 +0800

    remove ll list file sort by size

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("7d88cd2[2023-01-06T14:29:36+08:00]:os_debian_init.sh")
+VERSION+=("f885adb[2023-01-11T10:49:48+08:00]:os_debian_init.sh")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -692,7 +692,7 @@ umask 022
 
 alias cal='ncal -b'
 alias ls='ls --color=auto --group-directories-first'
-alias ll='ls -lhFS'
+alias ll='ls -lh'
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
