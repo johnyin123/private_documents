9ca067a5a0385d058cdffe20c1a95afb827c6d37
johnyin <johnyin.news@163.com>
2023-01-12 13:03:52 +0800

    fix

--- custom_os/chroot.sh
+++ custom_os/chroot.sh
@@ -5,16 +5,16 @@ readonly SCRIPTNAME=${0##*/}
 ROOTFS=${1:?rootfs need input}
 
 for i in /dev /dev/pts /proc /sys /sys/firmware/efi/efivars /run; do
-    mount -o bind $i "${root_dir}${i}" 2>/dev/null && echo "mount root $i ...." || true
+    mount -o bind $i "${ROOTFS}${i}" 2>/dev/null && echo "mount root $i ...." || true
 done
 
 # mount -t tmpfs -o size=32M tmpfs ${ROOTFS}/dev
 # chroot ${ROOTFS} busybox mdev -s
 
 echo "Running chroot with mounted sysfs, proc and mdev..."
-chroot ${ROOTFS}
+chroot ${ROOTFS} || true
 
 echo "Unmounting..."
 for i in /dev /proc /sys /run; do
-    umount -R -v "${root_dir}${i}" || true
+    umount -R -v "${ROOTFS}${i}" || true
 done
