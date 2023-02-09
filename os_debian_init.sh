5cff632866e6f6312675c1cd9c67dfec12445c25
johnyin <johnyin.news@163.com>
2023-02-09 09:25:02 +0800

    doc

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("1a6da23[2023-02-03T12:44:49+08:00]:os_debian_init.sh")
+VERSION+=("b8cca73[2023-02-09T09:22:20+08:00]:os_debian_init.sh")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -112,6 +112,9 @@ GRUB_CMDLINE_LINUX=""
 EOF
     mkdir -p /etc/default/grub.d || true
     echo 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT apparmor=0"' | tee /etc/default/grub.d/apparmor.cfg
+    echo "need run: update-grub"
+    echo "cat /sys/module/apparmor/parameters/enabled"
+    echo "enable apparmor: apparmor=1 security=apparmor"
 }
 export -f debian_grub_init
 
