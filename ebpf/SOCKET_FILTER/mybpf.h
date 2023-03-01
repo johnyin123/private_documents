3e6b293f748d7edb5cee735d9a19850876e9a59c
johnyin <johnyin.news@163.com>
2023-03-01 10:41:29 +0800

    add SOCKET_FILTER bpf demo

---
+++ ebpf/SOCKET_FILTER/mybpf.h
@@ -0,0 +1,16 @@
+#ifndef __SOCKFILTER_H
+#define __SOCKFILTER_H
+
+struct so_event {
+	__be32 src_addr;
+	__be32 dst_addr;
+	union {
+		__be32 ports;
+		__be16 port16[2];
+	};
+	__u32 ip_proto;
+	__u32 pkt_type;
+	__u32 ifindex;
+};
+
+#endif /* __SOCKFILTER_H */
