758df0a4aff8c77216ff2ac4d36af02bd0915487
johnyin <johnyin.news@163.com>
2023-03-10 09:25:52 +0800

    add init_submodule.sh

---
+++ ebpf/init_submodule.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+
+# git submodule add https://github.com/libbpf/libbpf.git libbpf
+# git add libbpf/
+# git commit
+# git push
+
+# git submodule update --init
+
+make -C ${DIRNAME}/libbpf/src \
+         BUILD_STATIC_ONLY=1 \
+         OBJDIR=${DIRNAME} DESTDIR= \
+         INCLUDEDIR=${DIRNAME}/inc LIBDIR=${DIRNAME}/lib UAPIDIR= \
+         install
