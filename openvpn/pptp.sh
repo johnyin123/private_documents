adee68f13864804eec0b4c2f649091eea1f3a74b
johnyin <johnyin.news@163.com>
2023-05-17 16:25:41 +0800

    fix pptp.sh

--- openvpn/pptp.sh
+++ openvpn/pptp.sh
@@ -27,3 +27,49 @@ sed --quiet -i -E \
 cat <<EOF | tee -a /etc/ppp/chap-secrets
 user1   *   password1   *
 EOF
+
+cat <<EOF
+# pptpd-options
+# The name of the local system for authentication purposes
+name pptpd
+
+# Refuse EAP, PAP, CHAP or MS-CHAP connections
+# Accept ONLY MS-CHAPv2 or MPPE with 128-bit encryption
+refuse-eap
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe
+require-mppe-128
+
+# Require authorization
+auth
+
+# Add entry to the ARP system table
+proxyarp
+
+# For the serial device to ensure exclusive access to the device
+lock
+
+# Disable BSD-Compress and Van Jacobson TCP/IP header compression
+nobsdcomp
+novj
+novjccomp
+
+# Disable logging
+nolog
+nologfd
+
+# LCP echo-requests options
+lcp-echo-interval 30
+lcp-echo-failure 5
+
+# MTU MRU options
+mtu 1200
+mru 1200
+
+# DNS options for Windows clients
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
+EOF
