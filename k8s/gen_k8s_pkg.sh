735a20a13a4c9f4feae59a4f925a69652fd04fc7
johnyin <johnyin.news@163.com>
2023-07-19 08:57:37 +0800

    gen k8s env static package, deb/rpm

---
+++ k8s/gen_k8s_pkg.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+readonly SCRIPTNAME=${0##*/}
+set -o errtrace
+set -o nounset
+set -o errexit
+VERSION+=("initver[2023-07-19T08:57:38+08:00]:gen_k8s_pkg.sh")
+################################################################################
+PKG_DIR=${1?"$SCRIPTNAME <src_dir> <amd64/arm64>"}
+ARCH=${2?"$SCRIPTNAME <src_dir> <amd64/arm64>"}
+# # requires start
+TGZ_CRICTL=crictl-v1.27.1-linux-${ARCH}.tar.gz 
+TGZ_CONTAINERD=containerd-static-1.7.2-linux-${ARCH}.tar.gz 
+TGZ_CNI_PLUGINS=cni-plugins-linux-${ARCH}-v1.3.0.tgz 
+BIN_KUBEADM=kubeadm.v1.27.3.${ARCH}
+BIN_KUBECTL=kubectl.v1.27.3.${ARCH}
+BIN_KUBELET=kubelet.v1.27.3.${ARCH}
+BIN_RUNC=runc.${ARCH}
+BIN_CALICOCTL=calicoctl-linux-${ARCH}
+# # requires end
+[ -e "${TGZ_CRICTL}" ] && \
+[ -e "${TGZ_CONTAINERD}" ] && \
+[ -e "${TGZ_CNI_PLUGINS}" ] && \
+[ -e "${BIN_KUBEADM}" ] && \
+[ -e "${BIN_KUBECTL}" ] && \
+[ -e "${BIN_KUBELET}" ] && \
+[ -e "${BIN_RUNC}" ] && \
+[ -e "${BIN_CALICOCTL}" ] || {
+    cat<<EOF
+Require:
+    ${TGZ_CRICTL}
+    ${TGZ_CONTAINERD}
+    ${TGZ_CNI_PLUGINS}
+    ${BIN_KUBEADM}
+    ${BIN_KUBECTL}
+    ${BIN_KUBELET}
+    ${BIN_RUNC}
+    ${BIN_CALICOCTL}
+https://github.com/containerd/containerd/releases/download/v1.7.2/containerd-static-1.7.2-linux-${ARCH}.tar.gz
+https://github.com/opencontainers/runc/releases/download/v1.1.7/runc.${ARCH}
+https://github.com/containernetworking/plugins/releases/download/v1.3.0/cni-plugins-linux-${ARCH}-v1.3.0.tgz
+https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.27.1/crictl-v1.27.1-linux-${ARCH}.tar.gz
+https://dl.k8s.io/release/1.27.3/bin/linux/${ARCH}/kubelet
+https://dl.k8s.io/release/1.27.3/bin/linux/${ARCH}/kubeadm
+https://dl.k8s.io/release/1.27.3/bin/linux/${ARCH}/kubectl
+EOF
+    exit 1
+}
+
+for d in /opt/cni /usr/bin/ /etc/systemd/system/kubelet.service.d /lib/systemd/system /etc/kubernetes/; do
+    mkdir -p ${PKG_DIR}/${d}
+done
+cat <<'EOF' > ${PKG_DIR}/etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+[Service]
+Environment="KUBELET_KUBECONFIG_ARGS=--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf"
+Environment="KUBELET_CONFIG_ARGS=--config=/var/lib/kubelet/config.yaml"
+# This is a file that "kubeadm init" and "kubeadm join" generates at runtime, populating the KUBELET_KUBEADM_ARGS variable dynamically
+EnvironmentFile=-/var/lib/kubelet/kubeadm-flags.env
+# This is a file that the user can use for overrides of the kubelet args as a last resort. Preferably, the user should use
+# the .NodeRegistration.KubeletExtraArgs object in the configuration files instead. KUBELET_EXTRA_ARGS should be sourced from this file.
+EnvironmentFile=-/etc/default/kubelet
+ExecStart=
+ExecStart=/usr/bin/kubelet $KUBELET_KUBECONFIG_ARGS $KUBELET_CONFIG_ARGS $KUBELET_KUBEADM_ARGS $KUBELET_EXTRA_ARGS
+EOF
+chmod 644 ${PKG_DIR}/etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+cat <<EOF > ${PKG_DIR}/lib/systemd/system/kubelet.service
+[Unit]
+Description=kubelet: The Kubernetes Node Agent
+Documentation=https://kubernetes.io/docs/home/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/usr/bin/kubelet
+Restart=always
+StartLimitInterval=0
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod 644 ${PKG_DIR}/lib/systemd/system/kubelet.service
+cat <<EOF > ${PKG_DIR}/lib/systemd/system/containerd.service
+# Copyright The containerd Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network.target local-fs.target
+
+[Service]
+#uncomment to enable the experimental sbservice (sandboxed) version of containerd/cri integration
+#Environment="ENABLE_CRI_SANDBOXES=sandboxed"
+ExecStartPre=-/sbin/modprobe overlay
+ExecStart=/usr/bin/containerd
+
+Type=notify
+Delegate=yes
+KillMode=process
+Restart=always
+RestartSec=5
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNPROC=infinity
+LimitCORE=infinity
+LimitNOFILE=infinity
+# Comment TasksMax if your systemd version does not supports it.
+# Only systemd 226 and above support this version.
+TasksMax=infinity
+OOMScoreAdjust=-999
+
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod 644 ${PKG_DIR}/lib/systemd/system/containerd.service
+
+install --mode=0755 ${BIN_KUBEADM} ${PKG_DIR}/usr/bin/kubeadm
+install --mode=0755 ${BIN_KUBECTL} ${PKG_DIR}/usr/bin/kubectl
+install --mode=0755 ${BIN_KUBELET} ${PKG_DIR}/usr/bin/kubelet
+install --mode=0755 ${BIN_RUNC} ${PKG_DIR}/usr/bin/runc
+install --mode=0755 ${BIN_CALICOCTL} ${PKG_DIR}/usr/bin/calicoctl
+tar -C ${PKG_DIR}/usr/bin  -xvf ${TGZ_CRICTL}
+tar -C ${PKG_DIR}/usr/     -xvf ${TGZ_CONTAINERD}
+tar -C ${PKG_DIR}/opt/cni/ -xvf ${TGZ_CNI_PLUGINS}
+
+fpm --package . -s dir -t rpm -C ${PKG_DIR}/ --name tsd_cnap --conflicts containerd  --conflicts kubelet --conflicts kubeadm --conflicts kubectl --depends conntrack --depends socat --version 0.9 --description "tsd cnap ${ARCH} env $(echo "${VERSION[@]}" | cut -d'[' -f 1)"
+fpm --package . -s dir -t deb -C ${PKG_DIR}/ --name tsd_cnap --conflicts containerd  --conflicts kubelet --conflicts kubeadm --conflicts kubectl --depends conntrack --depends socat --version 0.9 --description "tsd cnap ${ARCH} env $(echo "${VERSION[@]}" | cut -d'[' -f 1)"
