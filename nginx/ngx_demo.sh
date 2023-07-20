0589153c609d1ba253a82e913801f0ef6db9025a
johnyin <johnyin.news@163.com>
2023-07-20 10:49:50 +0800

    nginx demo add docker-registry

--- nginx/ngx_demo.sh
+++ nginx/ngx_demo.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("7b33615[2023-05-09T11:10:45+08:00]:ngx_demo.sh")
+VERSION+=("faf9207[2023-06-01T09:38:45+08:00]:ngx_demo.sh")
 
 set -o errtrace
 set -o nounset
@@ -4213,6 +4213,49 @@ server {
         ssi on;
         default_type text/html;
         return 200 '<!--#config timefmt="%A, %H:%M:%S" --><!--#set var="v" value="$date_gmt" --><!--#echo var="v" -->';
+    }
+}
+EOF
+cat <<'EOF' >docker_registry.http
+upstream docker-registry {
+    server registry:5000;
+}
+
+## Set a variable to help us decide if we need to add the
+## 'Docker-Distribution-Api-Version' header.
+## The registry always sets this header.
+## In the case of nginx performing auth, the header is unset
+## since nginx is auth-ing before proxying.
+map $upstream_http_docker_distribution_api_version $docker_distribution_api_version {
+    '' 'registry/2.0';
+}
+server {
+    listen 443 ssl;
+    server_name _;
+    ssl_certificate /etc/nginx/conf.d/domain.crt;
+    ssl_certificate_key /etc/nginx/conf.d/domain.key;
+
+    # disable any limits to avoid HTTP 413 for large image uploads
+    client_max_body_size 0;
+
+    # required to avoid HTTP 411: see Issue #1486 (https://github.com/moby/moby/issues/1486)
+    chunked_transfer_encoding on;
+
+    location /v2/ {
+        # Do not allow connections from docker 1.5 and earlier
+        # docker pre-1.6.0 did not properly set the user agent on ping, catch "Go *" user agents
+        if ($http_user_agent ~ "^(docker\/1\.(3|4|5(?!\.[0-9]-dev))|Go ).*$" ) {
+            return 404;
+        }
+        # auth_basic "Registry realm";
+        # auth_basic_user_file /etc/nginx/conf.d/nginx.htpasswd;
+        add_header 'Docker-Distribution-Api-Version' $docker_distribution_api_version always;
+        proxy_pass                         http://docker-registry;
+        proxy_set_header Host              $http_host;   # required for docker client's sake
+        proxy_set_header X-Real-IP         $remote_addr; # pass on real client's IP
+        proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_read_timeout                 900;
     }
 }
 EOF
