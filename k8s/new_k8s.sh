2b1d7cea328782d32c17232a4b1c6fbc20678a20
johnyin <johnyin.news@163.com>
2023-07-20 08:20:01 +0800

    fix

--- k8s/new_k8s.sh
+++ k8s/new_k8s.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("fb8a033[2023-07-20T07:40:53+08:00]:new_k8s.sh")
+VERSION+=("d2508f9[2023-07-20T07:48:03+08:00]:new_k8s.sh")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || { echo '**ERROR: functions.sh nofound!'; exit 1; }
 ################################################################################
 SSH_PORT=${SSH_PORT:-60022}
@@ -228,6 +228,8 @@ init_calico_cni() {
     [ -e "${calico_yml}" ] || [ -e "${calico_cust_yml}" ] || return 1
     # sed -i "s|replicas\s*:.*|replicas: ${TYPHA_REPLICAS:-1}|g" "${calico_typha_yml}"
     sed -i "s|cidr\s*:.*|cidr: ${pod_cidr}|g" "${calico_cust_yml}"
+    # # IPIPCrossSubnet,IPIP,VXLAN,VXLANCrossSubnet,None
+    # sed -i "s|encapsulation\s*:.*|encapsulation: ${crossnet_method}|g" "${calico_cust_yml}"
     export KUBECONFIG=/etc/kubernetes/admin.conf
     kubectl create -f "${calico_yml}"
     kubectl create -f "${calico_cust_yml}"
@@ -340,6 +342,7 @@ EOF
         sed -i -e "s/sandbox_image\s*=.*\/pause.*/sandbox_image = \"registry.k8s.io\/${pausekey}\"/g" /etc/containerd/config.toml
         # kubernets自v1.24.0后，就不再使用docker.shim，需要安装containerd(在docker基础下安装)
         sed -i 's/SystemdCgroup\s*=.*$/SystemdCgroup = true/g' /etc/containerd/config.toml
+        sed -i 's/systemd_cgroup\s*=.*/systemd_cgroup = true/g' /etc/containerd/config.toml
         # [plugins."io.containerd.grpc.v1.cri".registry.mirrors]
         #   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
         #     endpoint = ["https://registry-1.docker.io"]
