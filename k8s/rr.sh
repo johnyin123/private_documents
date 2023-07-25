5689dc19af07f010967a6b3ff4249279d574f997
johnyin <johnyin.news@163.com>
2023-07-25 09:49:43 +0800

    fix rr.sh

--- k8s/rr.sh
+++ k8s/rr.sh
@@ -1,11 +1,28 @@
-kubectl get nodes -o wide
-# choose some nodes as reflector nodes
-command -v calicoctl &> /dev/null || { exit 1; }
-calicoctl node status || true
-# # Configure BGP Peering
-RR_LABEL=route-reflector
-# # # Add peering between the RouteReflectors themselves.
-cat <<EOF | calicoctl apply -f -
+#!/usr/bin/env bash
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+readonly SCRIPTNAME=${0##*/}
+if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
+    exec 5> "${DIRNAME}/$(date '+%Y%m%d%H%M%S').${SCRIPTNAME}.debug.log"
+    BASH_XTRACEFD="5"
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+VERSION+=("1ad89a9[2023-07-24T17:17:26+08:00]:rr.sh")
+[ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || { echo '**ERROR: functions.sh nofound!'; exit 1; }
+################################################################################
+remote_rr() {
+    local asnumber=${1}
+    local clusterid=${2}
+    shift 2;
+    local nodes=${*}
+    local RR_LABEL=route-reflector
+    echo "check requires ...."
+    command -v calicoctl &> /dev/null || { echo "***************calicoctl nofound"; return 1; }
+    kubectl get nodes -o wide || true
+    echo "Configure BGP Peering"
+    calicoctl node status || true
+    echo "Add peering between the RouteReflectors themselves."
+    cat <<EOF | calicoctl apply -f -
 kind: BGPPeer
 apiVersion: projectcalico.org/v3
 metadata:
@@ -14,8 +31,7 @@ spec:
   nodeSelector: has(${RR_LABEL})
   peerSelector: has(${RR_LABEL})
 EOF
-
-cat <<EOF | calicoctl apply -f -
+    cat <<EOF | calicoctl apply -f -
 kind: BGPPeer
 apiVersion: projectcalico.org/v3
 metadata:
@@ -24,16 +40,13 @@ spec:
   nodeSelector: !has(${RR_LABEL})
   peerSelector: has(${RR_LABEL})
 EOF
-nodes=(srv150 srv151)
-for node in  ${nodes[@]}; do
-calicoctl patch node ${node} -p '{"spec": {"bgp": {"routeReflectorClusterID": "224.0.0.1"}}}'
-kubectl label node ${node} ${RR_LABEL}=true
-done
-
-# # Disable node-to-node Mesh
-ASNUMBER=63401
-calicoctl get bgpconfig default || true
-cat <<EOF | calicoctl create -f -
+    for node in ${nodes}; do
+        calicoctl patch node ${node} -p "{\"spec\": {\"bgp\": {\"routeReflectorClusterID\": \"${clusterid}\"}}}"
+        kubectl label node ${node} ${RR_LABEL}=true
+    done
+    echo "Disable node-to-node Mesh"
+    calicoctl get bgpconfig default || true
+    cat <<EOF | calicoctl create -f -
 apiVersion: projectcalico.org/v3
 kind: BGPConfiguration
 metadata:
@@ -41,11 +54,67 @@ metadata:
 spec:
   logSeverityScreen: Info
   nodeToNodeMeshEnabled: false
-  asNumber: ${ASNUMBER}
+  asNumber: ${asnumber}
 EOF
-calicoctl patch bgpconfiguration default -p '{"spec": {"nodeToNodeMeshEnabled": false}}'
-# # modify asnumber
-# calicoctl patch bgpconfiguration default -p '{"spec": {"asNumber": "64513"}}'
-# # on non reflector node run:
-calicoctl node status || true
-# for ip in $(ip r | grep bird | awk '{ print $1 }' | grep -v blackhole ); do ping -W1 -c1 ${ip%/*} &>/dev/null && echo "${ip} OK" || echo "${ip} ERR"; done
+    calicoctl patch bgpconfiguration default -p '{"spec": {"nodeToNodeMeshEnabled": false}}'
+}
+usage() {
+    [ "$#" != 0 ] && echo "$*"
+    cat <<EOF
+${SCRIPTNAME}
+        -m | --master    *  <ip>    master ipaddr
+        -r | --reflector *  <node>  reflector node name, multi input.
+        -U | --user         <user>  master ssh user, default root
+        -P | --port         <int>   master ssh port, default 60022
+        --asnumber          <int>   bgp as number, default 63401
+        --clusterid         <ip>    master mulicast ipaddr default 224.0.0.1
+        --sshpass           <str>   master ssh password, default use keyauth
+        -q|--quiet
+        -l|--log <int> log level
+        -V|--version
+        -d|--dryrun dryrun
+        -h|--help help
+EOF
+    exit 1
+}
+main() {
+    local master="" reflector=() user="root" port=60022 asnumber=63401 clusterid=224.0.0.1
+    local opt_short="m:r:U:P:"
+    local opt_long="master:,reflector:,user:,port:,asnumber:,clusterid:,sshpass:,"
+    opt_short+="ql:dVh"
+    opt_long+="quiet,log:,dryrun,version,help"
+    __ARGS=$(getopt -n "${SCRIPTNAME}" -o ${opt_short} -l ${opt_long} -- "$@") || usage
+    eval set -- "${__ARGS}"
+    while true; do
+        case "$1" in
+            -m | --master)    shift; master=${1}; shift;;
+            -r | --reflector) shift; reflector+=("${1}"); shift;;
+            -U | --user)      shift; user=${1}; shift;;
+            -P | --port)      shift; port=${1}; shift;;
+            --asnumber)       shift; asnumber=${1}; shift;;
+            --clusterid)      shift; clusterid=${1}; shift;;
+            --sshpass)        shift; set_sshpass "${1}"; shift;;
+            ########################################
+            -q | --quiet)   shift; QUIET=1;;
+            -l | --log)     shift; set_loglevel ${1}; shift;;
+            -d | --dryrun)  shift; DRYRUN=1;;
+            -V | --version) shift; for _v in "${VERSION[@]}"; do echo "$_v"; done; exit 0;;
+            -h | --help)    shift; usage;;
+            --)             shift; break;;
+            *)              usage "Unexpected option: $1";;
+        esac
+    done
+    [ -z "${master}" ] && usage "master must input"
+    [ "$(array_size reflector)" -gt "0" ] || usage "reflector must input"
+    info_msg "choose some nodes as reflector nodes\n"
+    ssh_func "${user}@${master}" "${port}" remote_rr "${asnumber}" "${clusterid}" ${reflector[@]}
+    # # modify asnumber
+    # calicoctl patch bgpconfiguration default -p '{"spec": {"asNumber": "64513"}}'
+    info_msg "on non reflector node run: calicoctl node status\n"
+    # for ip in $(ip r | grep bird | awk '{ print $1 }' | grep -v blackhole ); do ping -W1 -c1 ${ip%/*} &>/dev/null && echo "${ip} OK" || echo "${ip} ERR"; done
+    info_msg "ALL DONE\n" 
+    return 0
+}
+main "$@"
+
+
