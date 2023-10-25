a5bdd11648f5867b5422c79d81f58944c39fcaf0
johnyin <johnyin.news@163.com>
2023-10-25 17:32:35 +0800

    fix

--- kvm/virt-volupload.sh
+++ kvm/virt-volupload.sh
@@ -7,23 +7,17 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("505ec4e[2023-07-14T10:35:33+08:00]:virt-volupload.sh")
+VERSION+=("ec7473b[2023-10-10T11:16:02+08:00]:virt-volupload.sh")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || true
 ################################################################################
-# KVM_USER=${KVM_USER:-root}
-# KVM_HOST=${KVM_HOST:-127.0.0.1}
-# KVM_PORT=${KVM_PORT:-60022}
-VIRSH_OPT="-q ${KVM_HOST:+-c qemu+ssh://${KVM_USER:-root}@${KVM_HOST}:${KVM_PORT:-60022}/system}"
-VIRSH="virsh ${VIRSH_OPT}"
-
 usage() {
     [ "$#" != 0 ] && echo "$*"
 cat <<EOF
 ${SCRIPTNAME}
-     env:
-        KVM_HOST: default <not define, local>
-        KVM_USER: default root
-        KVM_PORT: default 60022
+    -K|--kvmhost          <ipaddr>           kvm host address
+    -U|--kvmuser          <str>              kvm host ssh user
+    -P|--kvmport          <int>              kvm host ssh port
+    --kvmpass             <password>         kvm host ssh password
     -p|--pool              *                 pool
     -v|--vol               *                 vol
     -t|--template                            telplate disk for upload(or stdin)
@@ -33,24 +27,26 @@ ${SCRIPTNAME}
     -d|--dryrun                              dryrun
     -h|--help                                display this help and exit
     Example:
-       KVM_HOST=127.0.0.1 ${SCRIPTNAME} -p default -v disk.raw -t tpl/linux.raw
+       ${SCRIPTNAME} -p default -v disk.raw -t tpl/linux.raw
 EOF
 exit 1
 }
 
 main() {
-    local disk_tpl=
-    local vol_name=
-    local pool=
-    local rbd=""
-    local opt_short="p:v:t:"
-    local opt_long="pool:,vol:,template:,rbd:,"
+    local kvmhost="" kvmuser="" kvmport=""
+    local disk_tpl= vol_name= pool= rbd=""
+    local opt_short="K:U:P:p:v:t:"
+    local opt_long="kvmhost:,kvmuser:,kvmport:,kvmpass:,pool:,vol:,template:,rbd:,"
     opt_short+="ql:dVh"
     opt_long+="quiet,log:,dryrun,version,help"
     __ARGS=$(getopt -n "${SCRIPTNAME}" -a -o ${opt_short} -l ${opt_long} -- "$@") || usage
     eval set -- "${__ARGS}"
     while true; do
         case "$1" in
+            -K | --kvmhost)  shift; kvmhost=${1}; shift;;
+            -U | --kvmuser)  shift; kvmuser=${1}; shift;;
+            -P | --kvmport)  shift; kvmport=${1}; shift;;
+            --kvmpass)       shift; set_sshpass "${1}"; shift;;
             -p | --pool)     shift; pool=${1}; shift ;;
             -v | --vol)      shift; vol_name=${1}; shift ;;
             -t | --template) shift; disk_tpl=${1}; shift ;;
@@ -65,24 +61,24 @@ main() {
             *)              usage "Unexpected option: $1";;
         esac
     done
-    local upload_cmd=${VIRSH}
-    #stdin is redirect
-    #[ -p /dev/stdin ] || { disk_tpl=/dev/stdin; upload_cmd="cat | ${VIRSH}"; }
-    [[ -t 0 ]] || { disk_tpl=/dev/stdin; upload_cmd="cat | ${VIRSH}"; }
     [ ! -z "${disk_tpl}" ] && [ ! -z "${vol_name}" ] && [ ! -z "${pool}" ] || usage "vol_name/tpl/pool must input"
     [ -r ${disk_tpl} ] || exit_msg "template file ${disk_tpl} no found\n"
+    local upload_cmd="virsh_wrap '${kvmhost}' '${kvmport}' '${kvmuser}'"
+    #stdin is redirect
+    #[ -p /dev/stdin ] || { disk_tpl=/dev/stdin; upload_cmd="cat | ${VIRSH}"; }
+    [[ -t 0 ]] || { disk_tpl=/dev/stdin; upload_cmd="cat | virsh_wrap '${kvmhost}' '${kvmport}' '${kvmuser}'"; }
     [[ -t 0 ]] || disk_tpl=/dev/stdin    #stdin is redirect
     info_msg "upload ${disk_tpl} start\n"
-    ${VIRSH} pool-start ${pool} 2>/dev/null || true
+    virsh_wrap "${kvmhost}" "${kvmport}" "${kvmuser}" pool-start ${pool} 2>/dev/null || true
     [ -z ${rbd} ] && {
         try ${upload_cmd} vol-upload --pool ${pool} --vol ${vol_name} --file ${disk_tpl} || exit_msg "upload template file ${disk_tpl} error\n"
     } || {
-        local ceph_rbd_pool=$(${VIRSH} pool-dumpxml ${pool} | xmlstarlet sel -t -v "/pool/source/name")
+        local ceph_rbd_pool=$(virsh_wrap "${kvmhost}" "${kvmport}" "${kvmuser}" pool-dumpxml ${pool} | xmlstarlet sel -t -v "/pool/source/name")
         local orgsize=$(${KVM_HOST:+ssh -p ${KVM_PORT:-60022} ${KVM_USER:-root}@${KVM_HOST}} rbd --cluster ${rbd} info --format xml ${ceph_rbd_pool}/${vol_name} | xmlstarlet sel -t -v "/image/size")
         try "${KVM_HOST:+ssh -p ${KVM_PORT:-60022} ${KVM_USER:-root}@${KVM_HOST}} rbd --cluster ${rbd} rm --no-progress --pool ${ceph_rbd_pool} ${vol_name} 2>/dev/null" || true
         try "cat ${disk_tpl} | ${KVM_HOST:+ssh -p ${KVM_PORT:-60022} ${KVM_USER:-root}@${KVM_HOST}} rbd --cluster ${rbd} import --image-feature layering - ${ceph_rbd_pool}/${vol_name}" || exit_msg "upload template file ${disk_tpl} via rbd error\n"
         info_msg "restore ${vol_name} size to  ${orgsize}"
-        ${VIRSH} vol-resize --pool ${pool} --vol ${vol_name} --capacity ${orgsize}
+        virsh_wrap "${kvmhost}" "${kvmport}" "${kvmuser}" vol-resize --pool ${pool} --vol ${vol_name} --capacity ${orgsize}
     }
     info_msg "upload template file ${disk_tpl} ok\n"
     return 0
