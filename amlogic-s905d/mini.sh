44f32ca3babe9242f507f33087f8d89673f931ee
johnyin <johnyin.news@163.com>
2023-11-02 07:02:11 +0800

    fix

--- amlogic-s905d/mini.sh
+++ amlogic-s905d/mini.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("aa69917[2022-07-12T15:57:38+08:00]:mini.sh")
+VERSION+=("e71d887[2022-10-21T08:02:05+08:00]:mini.sh")
 ################################################################################
 source ${DIRNAME}/os_debian_init.sh
 
@@ -29,5 +29,5 @@ sed -i "s/^macaddr=.*/macaddr=${macaddr}/g" ${ROOTFS}/usr/lib/firmware/brcm/brcm
 grep "^macaddr=" ${ROOTFS}/usr/lib/firmware/brcm/brcmfmac43455-sdio.txt || true
 sed -i "s/^macaddr=.*/macaddr=${macaddr}/g" ${ROOTFS}/usr/lib/firmware/brcm/brcmfmac43455-sdio.phicomm,n1.txt || true
 grep "^macaddr=" ${ROOTFS}/usr/lib/firmware/brcm/brcmfmac43455-sdio.phicomm,n1.txt || true
-echo "rsync -avzP --numeric-ids -e 'ssh -p60022' --exclude=boot --delete ${ROOTFS}/* root@${IP:-10.32.166.32}:/overlay/lower/"
+echo "rsync -avzP --numeric-ids -e 'ssh -p60022' --exclude=/usr/bin/qemu-aarch64-static --exclude=boot --delete ${ROOTFS}/* root@${IP:-10.32.166.32}:/overlay/lower/"
 echo "rsync -avzP --numeric-ids -e 'ssh -p60022' --exclude=/usr/bin/qemu-aarch64-static --delete ${ROOTFS}/boot/* root@${IP:-10.32.166.32}:/boot/"
