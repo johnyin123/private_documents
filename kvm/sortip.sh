6d93d1d50ba39e72fce20cc100a463950e00e223
johnyin <johnyin.news@163.com>
2024-01-08 13:57:43 +0800

    doc

--- kvm/sortip.sh
+++ kvm/sortip.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 fname=${1:? must input}
-cat ${fname} | sed "s/Co., Ltd.//g" | awk -F, '{ printf "%s\n", $13 }'| grep -v "^$" | sort -t. -k 3,3n -k 4,4n
+tail -n +2  ${fname} | sed "s/Co., Ltd.//g" | awk -F, '{ printf "%s\n", $13 }'| grep -v "^$" | sort -t. -k 3,3n -k 4,4n
+tail -n +2  ${fname} | awk -F, '{print $2}' | sort -t. -k 3,3n -k 4,4n | uniq --count
