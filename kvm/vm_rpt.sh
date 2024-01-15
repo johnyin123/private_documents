6f2d91cab590a095ddff524ca8db7ad9ee54bf30
johnyin <johnyin.news@163.com>
2024-01-15 16:30:12 +0800

    fix

--- kvm/vm_rpt.sh
+++ kvm/vm_rpt.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
-
-amdvcpu=$(awk -F, '/AMD64/{sum+=$8} END{print sum;} ' ${1})
-amdvmem=$(awk -F, '/AMD64/{sum+=$9} END{print sum;} ' ${1})
-armvcpu=$(awk -F, '/ARM64/{sum+=$8} END{print sum;} ' ${1})
-armvmem=$(awk -F, '/ARM64/{sum+=$9} END{print sum;} ' ${1})
-X86CPU=$(awk -F, '{print $1 $2 $4}' ${1} | sort | uniq | awk -F'|' '/AMD64/{sum+=$3} END{print sum;} ' ${1})
-X86MEM=$(awk -F, '{print $1 $2 $4}' ${1} | sort | uniq | awk -F'|' '/AMD64/{sum+=$4} END{print sum;} ' ${1})
-ARMCPU=$(awk -F, '{print $1 $2 $4}' ${1} | sort | uniq | awk -F'|' '/ARM64/{sum+=$3} END{print sum;} ' ${1})
-ARMMEM=$(awk -F, '{print $1 $2 $4}' ${1} | sort | uniq | awk -F'|' '/ARM64/{sum+=$4} END{print sum;} ' ${1})
-echo "X86物理机CPU: ${X86CPU}C 内存: ${X86MEM}G"
-echo "ARM物理机CPU: ${ARMCPU}C 内存: ${ARMMEM}G"
-echo "X86虚拟机CPU: ${amdvcpu}C 内存: ${amdvmem}G"
-echo "ARM虚拟机CPU: ${armvcpu}C 内存: ${armvmem}G"
-echo "物理机IP    : 虚拟机数量"
-for key in $(tail -n +2 ${1} | awk -F, '{print $2}' | sort -t. -k 3,3n -k 4,4n | uniq); do
-    echo -n "$key ："
-    grep ",${key}," ${1} | wc -l
+fname=${1:? must input}
+amdvcpu=$(awk -F, '/AMD64/{sum+=$8} END{print sum;} ' ${fname})
+amdvmem=$(awk -F, '/AMD64/{sum+=$9} END{print sum;} ' ${fname})
+armvcpu=$(awk -F, '/ARM64/{sum+=$8} END{print sum;} ' ${fname})
+armvmem=$(awk -F, '/ARM64/{sum+=$9} END{print sum;} ' ${fname})
+AMDCPU=$(awk -F, '{print $1 $2 $4}' ${fname} | sort | uniq | awk -F'|' '/AMD64/{sum+=$3} END{print sum;} ' ${fname})
+AMDMEM=$(awk -F, '{print $1 $2 $4}' ${fname} | sort | uniq | awk -F'|' '/AMD64/{sum+=$4} END{print sum;} ' ${fname})
+ARMCPU=$(awk -F, '{print $1 $2 $4}' ${fname} | sort | uniq | awk -F'|' '/ARM64/{sum+=$3} END{print sum;} ' ${fname})
+ARMMEM=$(awk -F, '{print $1 $2 $4}' ${fname} | sort | uniq | awk -F'|' '/ARM64/{sum+=$4} END{print sum;} ' ${fname})
+printf "X86物理机CPU    : %10d 内存G: %10d\n" "${AMDCPU}" "${AMDMEM}"
+printf "X86虚拟机CPU    : %10d 内存G: %10d\n" "${amdvcpu}" "${amdvmem}"
+printf "                : %9d%%        %9d%%\n" "$((${amdvcpu}*100/${AMDCPU}))" "$((${amdvmem}*100/${AMDMEM}))"
+printf "##############################################\n"
+printf "ARM物理机CPU    : %10d 内存G: %10d\n" "${ARMCPU}" "${ARMMEM}"
+printf "ARM虚拟机CPU    : %10d 内存G: %10d\n" "${armvcpu}" "${armvmem}"
+printf "                : %9d%%        %9d%%\n" "$((${armvcpu}*100/${ARMCPU}))" "$((${armvmem}*100/${ARMMEM}))"
+printf "##############################################\n"
+printf "物理机IP        :      虚拟机数量\n"
+for key in $(tail -n +2 ${fname} | awk -F, '{print $2}' | sort -t. -k 3,3n -k 4,4n | uniq); do
+    printf "%-15s : %10d\n" "${key}" $(grep ",${key}," ${fname} | wc -l)
 done
 
