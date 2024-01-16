734b09d123deb47787d6d079986d0c028a13c181
johnyin <johnyin.news@163.com>
2024-01-16 14:01:39 +0800

    fix

--- ceph/presigned_post.py
+++ ceph/presigned_post.py
@@ -10,44 +10,83 @@ class S3:
     def __init__(self):
         self.S3_HOST = os.environ.get('S3_HOST', 'http://10.170.24.2')
         # self.S3_HOST = os.environ.get('S3_HOST', 'http://10.170.6.105:9999')
-        self.S3_BUCKET = os.environ.get('S3_BUCKET', 'public')
         self.S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY', 'admin')
         self.S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY', 'tsd@2023')
-        print("HOST={}\nBUCKET={}\nACCESS_KEY={}\nSECRET_KEY={}\n".format(self.S3_HOST,self.S3_BUCKET,self.S3_ACCESS_KEY,self.S3_SECRET_KEY))
-        self.client = boto3.client("s3", verify=False, use_ssl = False,
-                                   aws_access_key_id=self.S3_ACCESS_KEY,
-                                   aws_secret_access_key=self.S3_SECRET_KEY,
-                                   endpoint_url = self.S3_HOST,
-                                   config=boto3.session.Config(signature_version='s3v4')
-                                   )
-        # if not self.client.Bucket(bucket).creation_date:
-        #     self.client.create_bucket(Bucket=bucket)
-        # Delete: self.client.delete_bucket(Bucket=bucket_name)
-    def set_cors(self):
+        print("HOST={}\nACCESS_KEY={}\nSECRET_KEY={}\n".format(self.S3_HOST,self.S3_ACCESS_KEY,self.S3_SECRET_KEY))
+        self.session = boto3.Session(aws_access_key_id=self.S3_ACCESS_KEY, aws_secret_access_key=self.S3_SECRET_KEY)
+        self.client = self.session.client("s3", verify=False, use_ssl = False, endpoint_url = self.S3_HOST, config=boto3.session.Config(signature_version='s3v4'),aws_session_token="SESSION_TOKEN")
+
+    def create_bucket(self, bucket):
+        response = self.client.create_bucket(Bucket=bucket)
+        print(response)
+
+    def delete_bucket(self, bucket):
+        response = self.client.delete_bucket(Bucket=bucket)
+        print(response)
+
+    def set_policy(self, bucket):
+        bucket_policy = {
+          "Version":"2012-10-17",
+          "Statement":[
+            {
+              "Sid":"AddPerm",
+              "Effect":"Allow",
+              "Principal": "*",
+              "Action":["s3:ListBucket"],
+              "Resource":["arn:aws:s3:::{0}".format(bucket)]
+            },
+            {
+              "Sid":"AddPerm",
+              "Effect":"Allow",
+              "Principal": "*",
+              "Action":["s3:GetObject", "s3:PutObject", "s3:GetObjectAcl", "s3:PutObjectAcl", "s3:DeleteObject", "s3:AbortMultipartUpload", "s3:PutBucketCORS"],
+              "Resource":["arn:aws:s3:::{0}/*".format(bucket)]
+            }
+          ]
+        }
+        bucket_policy = json.dumps(bucket_policy)
+        self.client.put_bucket_policy(Bucket=bucket, Policy=bucket_policy)
+
+    def set_acl(self, bucket, acl='public-read'):
+        result = self.client.get_bucket_acl(Bucket=bucket)
+        print("org permission is {}".format(result))
+        response = self.client.put_bucket_acl(ACL=acl, Bucket=bucket)
+        result = self.client.get_bucket_acl(Bucket=bucket)
+        print("the new permissions for bucket {0} is {1}".format(bucket, result['Grants']))
+
+    def set_cors(self, bucket):
         BUCKET_CORS_CONFIG = {
-          "CORSRules": [{
-              "AllowedOrigins": ['"{}"'.format('http://192.168.169.192:8080')],
+          "CORSRules": [
+            {
+              "AllowedOrigins": ["*"],
               "AllowedHeaders": ["*"],
               "AllowedMethods": ["GET", "HEAD", "POST", "PUT", "DELETE"],
-              "MaxAgeSeconds": 3000,
+              "MaxAgeSeconds": 3600,
               "ExposeHeaders": ["Etag"]
-            }]
+            },
+            {
+              'AllowedHeaders': [ 'Authorization', ],
+              'AllowedMethods': [ 'GET', ],
+              'AllowedOrigins': [ '*', ],
+              'MaxAgeSeconds': 3600,
+            },]
         }
-        print("set cors")
-        self.client.put_bucket_cors(Bucket=self.S3_BUCKET, CORSConfiguration=BUCKET_CORS_CONFIG)
+        response = self.client.put_bucket_cors(Bucket=bucket, CORSConfiguration=BUCKET_CORS_CONFIG)
+        print(response)
+
     # presigned post notwork now TODO:::
-    def get_presigned_post_url(self, file_name, file_type, time=3600):
+    def get_presigned_post_url(self, bucket, file_name, file_type, time=3600):
         return self.client.generate_presigned_post(
-            Bucket = self.S3_BUCKET, Key = file_name, ExpiresIn = time,
-            Fields =       {"acl": "public",   "Content-Type": file_type},
-            Conditions = [ {"acl": "public"}, {"Content-Type": file_type}, ]
+            Bucket = bucket, Key = file_name, ExpiresIn = time,
+            Fields =       {"acl": "public-read",   "Content-Type": file_type},
+            Conditions = [ {"acl": "public-read"}, {"Content-Type": file_type}, ]
         )
-    def get_presigned_url(self, key, time=3600):
-        return self.client.generate_presigned_url(ClientMethod='put_object', ExpiresIn=time, Params={'Bucket': self.S3_BUCKET, 'Key': key})
-    def get_file(self, key, time=3600):
-        return self.client.generate_presigned_url(ClientMethod='get_object', ExpiresIn=time, Params={'Bucket': self.S3_BUCKET, 'Key': key})
-    def delete_file(self, key):
-        return self.client.delete_object(Bucket=self.S3_BUCKET, Key=key)
+    def get_presigned_url(self, bucket, file_name, time=3600):
+        return self.client.generate_presigned_url(ClientMethod='put_object', ExpiresIn=time, Params={'Bucket': bucket, 'Key': file_name})
+    def get_file(self, bucket, key, time=3600):
+        return self.client.generate_presigned_url(ClientMethod='get_object', ExpiresIn=time, Params={'Bucket': bucket, 'Key': key})
+    def delete_file(self, bucket, key):
+        return self.client.delete_object(Bucket=bucket, Key=key)
     def list_bucket(self):
         response = self.client.list_buckets()
         if 'Buckets' in response.keys():
@@ -55,88 +94,121 @@ class S3:
                 print(bucket['Name'])
             else:
                 print('Empty')
-# app.config.from_file("config.json", load=json.load)
-index_html="""
+html="""
 <!DOCTYPE html>
 <html lang="zh-CN">
-<head><meta charset="utf-8"><title>{{ title }}</title></head>
-<body>
-<input type="file" id="file_input"/>
-<img id="preview" src="" />
-<form method="POST" action="/form/">
-  <input type="submit" value="Upload">
-</form>
+<head><meta charset="utf-8"><title>{title}</title></head>
+  <body>
+    <h1>Welcome!</h1>
+    <br>Please select an file
+    <input type="file" id="image">
+    <br><img id="preview">
 <script>
-(function() {
-  document.getElementById("file_input").onchange = function(){
-    var files = document.getElementById("file_input").files;
-    var file = files[0];
-    if(!file){ return alert("No file selected."); }
-    getSignedRequest(file);
-  };
-})();
-</script>
-<script>
-function getSignedRequest(file){
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/sign_s3?file_name="+file.name+"&file_type="+file.type);
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-      if(xhr.status === 200){
-        var response = JSON.parse(xhr.responseText);
-        uploadFile(file, response.data, response.url);
-      }
-      else{ alert("Could not get signed URL."); }
-    }
-  };
-  xhr.send();
-}
-function uploadFile(file, s3Data, url){
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", s3Data.url);
-  var postData = new FormData();
-  for(key in s3Data.fields){ postData.append(key, s3Data.fields[key]); }
-  postData.append('file', file);
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState === 4){
-      if(xhr.status === 200 || xhr.status === 204){
-        document.getElementById("preview").src = url;
-      }
-      else{ alert("Could not upload file."); }
-   }
-  };
-  xhr.send(postData);
-  xhr.onload = function(e) {
-    var xhr = e.target;
-    console.log(xhr.responseText);
-  }
-}
+{script}
 </script>
 </body>
 </html>
 """
-@app.route('/', methods=["GET"])
-def index():
-    return render_template_string(index_html, title="upload to s3")
+put_js="""
+function upload(file, signed_request, url, done) {
+  console.log(signed_request)
+  console.log(url)
+  var xhr = new XMLHttpRequest()
+  xhr.open('PUT', signed_request)
+  xhr.setRequestHeader('x-amz-acl', 'public-read')
+  xhr.onload = function() {
+    if(xhr.status === 200 || xhr.status === 204) { done(); }
+    else{ alert("Could not upload file."); }
+  }
+  xhr.send(file)
+}
+function sign_request(file, done) {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', '/sign_put?file_name=' + file.name + '&file_type=' + file.type)
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText)
+      done(response)
+    }
+  }
+  xhr.send()
+}
+document.getElementById('image').onchange = function() {
+  var file = document.getElementById('image').files[0]
+  if (!file) { alert('Could not get file.'); return; }
+  sign_request(file, function(response) {
+    upload(file, response.signed_request, response.url, function() {
+      document.getElementById('preview').src = response.url
+    })
+  })
+}
+"""
+post_js="""
+function upload(file, signed_request, url, done) {
+  console.log(signed_request)
+  console.log(url)
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', signed_request.url);
+  var postData = new FormData();
+  for(key in signed_request.fields){ postData.append(key, signed_request.fields[key]); }
+  postData.append('file', file);
+  xhr.setRequestHeader('x-amz-acl', 'public-read')
+  xhr.onload = function() { if (xhr.status === 200) { done() } }
+  xhr.send(postData);
+}
+function sign_request(file, done) {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', '/sign_post?file_name=' + file.name + '&file_type=' + file.type)
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText)
+      done(response)
+    }
+  }
+  xhr.send()
+}
+document.getElementById('image').onchange = function() {
+  var file = document.getElementById('image').files[0]
+  if (!file) { alert('Could not get file.'); return; }
+  sign_request(file, function(response) {
+    upload(file, response.signed_request, response.url, function() {
+      document.getElementById('preview').src = response.url
+    })
+  })
+}
+"""
+@app.route('/put', methods=["GET"])
+def put():
+    return render_template_string(html.format(script=put_js, title="put upload to s3"))
 
-@app.route('/sign_s3/')
-def sign_s3():
+@app.route('/sign_put')
+def sign_put():
     file_name = request.args.get('file_name')
     file_type = request.args.get('file_type')
     print("{} {}\n".format(file_name, file_type))
     s3 = S3()
-    # s3.set_cors()
-    presigned_post = s3.get_presigned_post_url(file_name, file_type)
-    print(json.dumps({
-        'data': presigned_post,
-        'url': '%s/%s/%s' % (s3.S3_HOST, s3.S3_BUCKET, file_name)
-    }))
-    return json.dumps({
-        'data': presigned_post,
-        'url': '%s/%s/%s' % (s3.S3_HOST, s3.S3_BUCKET, file_name)
-    })
+    bucket=os.environ.get('S3_BUCKET', 'public')
+    presigned_put = s3.get_presigned_url(bucket, file_name, 3600)
+    print({ "signed_request": presigned_put, "url": "{}/{}/{}".format(s3.S3_HOST, bucket, file_name)})
+    return { "signed_request": presigned_put, "url": "{}/{}/{}".format(s3.S3_HOST, bucket, file_name)}
+
+@app.route('/post', methods=["GET"])
+def post():
+    return render_template_string(html.format(script=post_js, title="post upload to s3"))
+
+@app.route('/sign_post/')
+def sign_post():
+    file_name = request.args.get('file_name')
+    file_type = request.args.get('file_type')
+    print("{} {}\n".format(file_name, file_type))
+    s3 = S3()
+    bucket=os.environ.get('S3_BUCKET', 'public')
+    presigned_post= s3.get_presigned_post_url(bucket, file_name, file_type)
+    print(json.dumps({ 'signed_request': presigned_post, 'url': '%s/%s/%s' % (s3.S3_HOST, bucket, file_name) }))
+    return json.dumps({ 'signed_request': presigned_post, 'url': '%s/%s/%s' % (s3.S3_HOST, bucket, file_name) })
 
 if __name__ == '__main__':
+    print("First set bucket cors\n")
     app.run(host='0.0.0.0', port=8080) #, debug=True)
 """
 cat <<EOF > ~/.s3cfg
