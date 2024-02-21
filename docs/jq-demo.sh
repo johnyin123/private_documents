178ee7a054151cbfc986cdd673de14817e1af810
johnyin <johnyin.news@163.com>
2024-02-21 09:38:51 +0800

    jq select demo

--- docs/jq-demo.sh
+++ docs/jq-demo.sh
@@ -11,3 +11,6 @@ do
         jq --arg addTen "$addTen" '.properties.addTen = ($addTen|tonumber)' <<< $json
 	done
 done | jq -s
+
+# jq '.vms[]| select(.addr[0] != "")'| jq .addr[0] | sort -t. -k 3,3n -k 4,4n^C
+# jq '.vms[]| select(.addr[0] == "172.16.6.1/21")' | jq .addr[0] | sort -t. -k 3,3n -k 4,4n
