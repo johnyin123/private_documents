34c1ef554201c5e274fdb23303e54015c99f3263
johnyin <johnyin.news@163.com>
2024-03-04 07:59:45 +0800

    fix

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("c61ac96[2024-03-03T17:37:47+08:00]:os_debian_init.sh")
+VERSION+=("9be79db[2024-03-03T17:47:40+08:00]:os_debian_init.sh")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -535,7 +535,7 @@ func SetTitle()
         call setline(2, "# -*- coding: utf-8 -*-")
         call setline(3, "")
         call setline(4, "import logging")
-        call setline(5, "from typing import Iterable, Optional, Set, Tuple, Union, Dict
+        call setline(5, "from typing import Iterable, Optional, Set, Tuple, Union, Dict")
         call setline(6, "logging.basicConfig(encoding='utf-8',level=logging.INFO, format='%(levelname)s: %(message)s') ")
         call setline(7, "logger = logging.getLogger(__name__)")
         call setline(8, "")
