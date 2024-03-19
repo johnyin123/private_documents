e76ac8c2dd7875cd06a7b9ee7743a19629bc5d53
johnyin <johnyin.news@163.com>
2024-03-19 15:31:38 +0800

    fix

--- nginx/subfilter/sidenav.js
+++ nginx/subfilter/sidenav.js
@@ -9,7 +9,7 @@ function appendHtml(el, str) {
 }
 var html = `
 <div style="position:fixed;top:50%;left:30px;">
-<span style="font-size:30px;cursor:pointer;position:absolute;top: 50%;" onclick="openNav()">+</span>
+<span style="font-size:30px;cursor:pointer;position:absolute;top:50%;" onclick="openNav()">+</span>
 </div>
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
