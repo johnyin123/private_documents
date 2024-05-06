e514284b4b1ce665073194c2013118c42611c84c
johnyin <johnyin.news@163.com>
2024-05-06 08:36:01 +0800

    fix

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("6d64dee[2024-04-23T14:34:38+08:00]:os_debian_init.sh")
+VERSION+=("63c21a2[2024-04-25T13:55:58+08:00]:os_debian_init.sh")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -780,6 +780,7 @@ debian_minimum_init() {
            /var/cache/debconf/*-old \
            /var/lib/dpkg/*-old \
            /root/.bash_history \
+           /root/.lesshst \
            /root/.viminfo \
            /root/.vim/ || true
     find /var/log/ -type f | xargs truncate -s0
