84f8ba79f706fde1f20f3dd3c8e0ecf0705a3a2e
johnyin <johnyin.news@163.com>
2024-06-26 11:11:29 +0800

    systemd automount

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("4375af0[2024-06-19T10:35:04+08:00]:os_debian_init.sh")
+VERSION+=("157f39c[2024-06-19T13:08:38+08:00]:os_debian_init.sh")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -751,6 +751,10 @@ fi
 mkdir -p /overlay/upper
 mkdir -p /overlay/work
 mkdir -p /overlay/lower
+
+log_begin_msg "mount overlay: ${MNT_OPTS:+,${MNT_OPTS}}"
+log_success_msg "mount -t overlay overlay -olowerdir=/overlay/lower,upperdir=/overlay/upper,workdir=/overlay/work${MNT_OPTS:+,${MNT_OPTS}}
+log_end_msg
 
 # make the readonly root available
 mount -n -o move ${rootmnt} /overlay/lower
