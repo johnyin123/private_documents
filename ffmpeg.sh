c6694b84bd8a6da8e34d5950f35f9d7256a9bf30
johnyin <johnyin.news@163.com>
2024-07-17 14:24:20 +0800

    fix ffmpeg demo

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("b4a4a2a[2024-07-09T15:55:25+08:00]:ffmpeg.sh")
+VERSION+=("44394ce[2024-07-12T10:24:24+08:00]:ffmpeg.sh")
 ################################################################################
 
 name=${1:?input err scale= $0 video.mkv}
@@ -32,38 +32,16 @@ ffmpeg -hide_banner -hwaccel auto -i ${name} -loglevel info \
 
 ffmpeg -hide_banner -i ${name} 2>&1 | grep -o -Ei "Video:\s*([^ ]*)"
 
-# echo 1st pass:
-# ffmpeg -y -hwaccel auto -i ${name} \
-#     -codec:v libx264 -profile:v high -preset slow ${frame:+-filter:v fps=fps=${frame}} \
-#     -b:v 500k -maxrate 500k -bufsize 1000k \
-#     ${scale:+-vf scale=${scale}} -threads 0 \
-#     -pass 1 -an -f mp4 /dev/null
-# echo 2nd pass:
-# ffmpeg -y -hwaccel auto -i ${name} \
-#     -codec:v libx264 -profile:v high -preset slow ${frame:+-filter:v fps=fps=${frame}} \
-#     -b:v 500k -maxrate 500k -bufsize 1000k \
-#     ${scale:+-vf scale=${scale}} -threads 0 \
-#     -pass 2 -codec:a libfdk_aac -b:a 128k -c:s copy -f mp4 ${name%.*}.convert.${name##*.}
-#  
-# #Downgrade fps: from 60 to 30
-# ffmpeg -i ${name} -r 30 ${name%.*}.convert.${name##*.}
-# 增加字幕流
-# ffmpeg -i video.mkv -i sub.ass -map 0 -map 1 -acodec copy -vcodec copy -scodec copy out.mkv
-# ffmpeg -i input.mkv -i sub.srt -sub_charenc 'UTF-8' -f srt -map 0 -map 1:0 -c:v copy -c:a copy -c:s srt video.mkv
-
 # cat > file.lst<<EOF
 # file 1.mp4
 # file 2.mp4
 # EOF
 # ffmpeg -f concat  -safe 0 -i file.lst -c copy out.mp4
+
 # #Downgrade fps: from 60 to 30
 # ffmpeg -i ${name} -r 30 ${name%.*}.convert.${name##*.}
+
 # 增加字幕流
 # ffmpeg -i video.mkv -i sub.ass -map 0 -map 1 -acodec copy -vcodec copy -scodec copy out.mkv
 # ffmpeg -i input.mkv -i sub.srt -sub_charenc 'UTF-8' -f srt -map 0 -map 1:0 -c:v copy -c:a copy -c:s srt video.mkv
-
-# cat > file.lst<<EOF
-# file 1.mp4
-# file 2.mp4
-# EOF
-# ffmpeg -f concat  -safe 0 -i file.lst -c copy out.mp4
+# ffmpeg -i input.mkv -i sub.srt -map 0 -map 1 -c copy youroutput.mkv
