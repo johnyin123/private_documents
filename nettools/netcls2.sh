67bf0ee02ead0193b15f20a4009a4479ee63b39a
johnyin <johnyin.news@163.com>
2024-08-06 13:46:06 +0800

    cgroup2 netcls

---
+++ nettools/netcls2.sh
@@ -0,0 +1,158 @@
+#!/usr/bin/env bash
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+readonly SCRIPTNAME=${0##*/}
+if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
+    exec 5> "${DIRNAME}/$(date '+%Y%m%d%H%M%S').${SCRIPTNAME}.debug.log"
+    BASH_XTRACEFD="5"
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+VERSION+=("initver[2024-08-06T13:46:06+08:00]:netcls2.sh")
+[ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || { echo '**ERROR: functions.sh nofound!'; exit 1; }
+################################################################################
+usage() {
+    [ "$#" != 0 ] && echo "$*"
+    cat <<EOF
+${SCRIPTNAME}, cgroup v2 version
+        -m|--fwmark   <int>   *  *  fwmark, 1 to 2147483647
+        -p|--pid      <int>   *     pid, support multi input
+        --remove                 x  remove pid from netcls
+        -g|--gw       <ipv4>  x  *  gateway, ipv4 gateway address
+        --slice       <str>   x     slice name for cgroup2 netcls
+        --rule        <int>   x     ip rule table id, default fwmark%251+1
+        --destroy             x     destroy netcls and ip rules !!!
+        -q|--quiet
+        -l|--log <int> log level
+        -V|--version
+        -d|--dryrun dryrun
+        -h|--help help
+/proc/sys/net/ipv4/conf/all/rp_filter=0/2
+    exam:
+        # # list all fwmark
+        ip rule | grep fwmark | grep -o "0x[^ ]*" | xargs -I@  printf "FWMARK: %d\n" @
+        # create netcls via gateway 192.168.41.1, fwmark 44
+        ./${SCRIPTNAME} --fwmark 44 -g 192.168.41.1
+        # add pid in netcls
+        ./${SCRIPTNAME} --fwmark 44 -p <p1> --pid <p2>
+        # remove pid out netcls
+        ./${SCRIPTNAME} --fwmark 44 -p <p1> --pid <p2> --remove
+        # destroy netcls fwmark 44
+        ./${SCRIPTNAME} --fwmark 44 --destroy
+EOF
+    exit 1
+}
+
+addin_netcls() {
+    local slice=${1}
+    local pid=${2}
+    local process="$(cat /proc/${pid}/comm 2>/dev/null)"
+    info_msg "add pid: ${pid}(${process:-N/A}) in ${slice}\n"
+    try "echo ${pid} > /sys/fs/cgroup/${slice}.slice/cgroup.procs" 2>/dev/null || true
+}
+
+delout_netcls() {
+    local slice=${1}
+    local pid=${2}
+    local process="$(cat /proc/${pid}/comm 2>/dev/null)"
+    try "echo ${pid} > /sys/fs/cgroup/${slice}.slice/cgroup.procs" 2>/dev/null || true
+    try "echo ${pid} > /sys/fs/cgroup/cgroup.procs" 2>/dev/null || true
+}
+
+create_netrule() {
+    local fwmark=${1}
+    local rule_table=${2}
+    local slice=${3}
+    local gateway=${4}
+    try ip rule show fwmark ${fwmark} | grep -qe "lookup\s*${rule_table}" && { warn_msg "netrule: ${rule_table} already exists!!!\n"; return 1; }
+    info_msg "create new ip rule ${rule_table}\n"
+    try ip route flush table ${rule_table} 2>/dev/null || true
+    try ip rule delete fwmark ${fwmark} table ${rule_table} 2>/dev/null || true
+
+    try mkdir -p /sys/fs/cgroup/${slice}.slice
+    try iptables -t mangle -A OUTPUT -m cgroup --path ${slice}.slice -j MARK --set-mark ${fwmark}
+    try iptables -t nat -A POSTROUTING -m cgroup --path ${slice}.slice -j MASQUERADE
+
+    try ip rule add fwmark ${fwmark} table ${rule_table}
+    try ip route replace default via ${gateway} table ${rule_table}
+    local default_route=$(ip -4 route show default | awk '{ print $3 }')
+    info_msg "private ipaddress use ${default_route}\n"
+    try ip route replace 10.0.0.0/8     via ${default_route} || true
+    try ip route replace 172.16.0.0/12  via ${default_route} || true
+    try ip route replace 192.168.0.0/16 via ${default_route} || true
+}
+
+main() {
+    # # FWMARK 1 to 2147483647
+    # # RULE_TABLE 1 to 252
+    # # CLASSID 0x00000001 to 0xFFFFFFFF
+    local pid=() gateway="" fwmark=""
+    local slice="" rule="" remove="" destroy=""
+    local opt_short="p:g:m:"
+    local opt_long="pid:,gw:,fwmark:,rule:,remove,destroy,"
+    opt_short+="ql:dVh"
+    opt_long+="quiet,log:,dryrun,version,help"
+    __ARGS=$(getopt -n "${SCRIPTNAME}" -o ${opt_short} -l ${opt_long} -- "$@") || usage
+    eval set -- "${__ARGS}"
+    while true; do
+        case "$1" in
+            -p | --pid)     shift; pid+=(${1}); shift;;
+            --remove)       shift; remove=1;;
+            -g | --gw)      shift; is_ipv4 ${1} || usage "gateway is ipv4"; gateway=${1}; shift;;
+            -m | --fwmark)  shift; [[ "${1}" =~ ^[0-9]+$ ]] || usage "fwmark sould 1 to 2147483647"; fwmark=${1}; shift;;
+            --slice)        shift; slice=${1}; shift;;
+            --rule)         shift; rule=${1}; shift;;
+            --destroy)      shift; destroy=1;;
+            ########################################
+            -q | --quiet)   shift; QUIET=1;;
+            -l | --log)     shift; set_loglevel ${1}; shift;;
+            -d | --dryrun)  shift; DRYRUN=1;;
+            -V | --version) shift; for _v in "${VERSION[@]}"; do echo "$_v"; done; exit 0;;
+            -h | --help)    shift; usage;;
+            --)             shift; break;;
+            *)              usage "Unexpected option: $1";;
+        esac
+    done
+    [ -z "${fwmark}"  ] && usage "fwmark null"
+    rule=${rule:-$((fwmark%251+1))}
+    slice="${slice:-JOHNYIN-${fwmark}}"
+    local cgroup_v2_root="$(mount -t cgroup2 | head -n1 | grep -oP '^cgroup2 on \K\S+')"
+    [ -f "${cgroup_v2_root}/cgroup.procs" ] || exit_msg "only cgroup2 support!\n"
+    info_msg "FWMARK=${fwmark}.............\n"
+    [ "$(array_size pid)" -gt "0" ] && {
+        for _pid in ${pid[@]}; do
+            [ -z "${remove}" ] && {
+                addin_netcls "${slice}" "${_pid}"
+            } || {
+                delout_netcls "${slice}" "${_pid}"
+            }
+        done
+        info_msg "ALL DONE!\n"
+        return 0
+    }
+    [ -z "${destroy}" ] || {
+        try "ip route flush table ${rule} 2>/dev/nul"l || true
+        try "ip rule delete fwmark ${fwmark} table ${rule} 2>/dev/null" || true
+        try "iptables -t mangle -D OUTPUT -m cgroup --path ${slice}.slice -j MARK --set-mark ${fwmark} 2>/dev/null" || true
+        try "iptables -t nat -D POSTROUTING -m cgroup --path ${slice}.slice -j MASQUERADE 2>/dev/null" || true
+
+        for _pid in $(cat /sys/fs/cgroup/${slice}.slice/cgroup.procs); do
+            delout_netcls "${slice}" "${_pid}"
+        done
+        try rmdir /sys/fs/cgroup/${slice}.slice || true
+        info_msg "remove private ipaddr route\n"
+        try ip route del 10.0.0.0/8     || true
+        try ip route del 172.16.0.0/12  || true
+        try ip route del 192.168.0.0/16 || true
+        # # rm all sub dir so can purge /sys/fs/cgroup/net_cls
+        # umount /sys/fs/cgroup/net_cls && rmdir /sys/fs/cgroup/net_cls
+        info_msg "ALL DONE!\n"
+        return 0
+    }
+    create_netrule "${fwmark}" "${rule}" "${slice}" "${gateway}" || {
+        ip rule show fwmark ${fwmark}
+        ip route show table ${rule}
+    }
+    return 0
+}
+auto_su "$@"
+main "$@"
