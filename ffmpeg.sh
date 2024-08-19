f1f05883217c8849e9df17aa74bf26250cee42c3
johnyin <johnyin.news@163.com>
2024-08-19 13:02:08 +0800

    fix

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("44394ce[2024-07-12T10:24:24+08:00]:ffmpeg.sh")
+VERSION+=("c6694b8[2024-07-17T14:24:20+08:00]:ffmpeg.sh")
 ################################################################################
 
 name=${1:?input err scale= $0 video.mkv}
@@ -18,7 +18,11 @@ ffmpeg -hide_banner -i ${name} 2>&1 | grep Stream
 ffmpeg -hide_banner -i ${name} 2>&1 | grep -o -Ei "Video:\s*([^ ]*)"
 # # hevc to x264 for phicomm n1
 # ffmpeg -i input -map 0 -c:v libx264 -crf 18 -vf format=yuv420p -c:a copy output.mkv
-# ffmpeg -i input -vf scale=-1:720 -c:v libx264 -crf 18 -preset veryslow -c:a copy MyMovie_720p.mkv
+# ffmpeg -i ${name} -movflags +faststart \
+#     -map 0:a:? -map 0:s:? -map 0:v:? \
+#     -vf scale=-1:720 -c:v libx264 -crf 18 -preset veryslow \
+#     -c:a copy -c:s copy \
+#     ${name%.*}.convert.mkv
 
 # -movflags faststart 参数告诉 ffmpeg 重组 MP4 文件 atoms，将 moov 放到文件开头
 # pv -f -F 'Converted: %b Elapsed: %t Current: %r Average: %a %p %e' "${name}" | ffmpeg -i pipe:0 \
