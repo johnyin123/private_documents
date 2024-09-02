6bda72c7ee5130e88aaaa10b9da8fdb1b6749943
johnyin <johnyin.news@163.com>
2024-09-02 09:00:31 +0800

    fix

--- amlogic-s905d/mini.sh
+++ amlogic-s905d/mini.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("a925230[2024-07-29T10:30:29+08:00]:mini.sh")
+VERSION+=("465f551[2024-08-20T15:50:22+08:00]:mini.sh")
 ################################################################################
 source ${DIRNAME}/os_debian_init.sh
 
@@ -31,5 +31,29 @@ sed -i "s/^macaddr=.*/macaddr=${macaddr}/g" ${ROOTFS}/usr/lib/firmware/brcm/brcm
 grep "^macaddr=" ${ROOTFS}/usr/lib/firmware/brcm/brcmfmac43455-sdio.phicomm,n1.txt || true
 # # set fake-clock
 date --utc '+%Y-%m-%d %H:%M:%S' > ${ROOTFS}/etc/fake-hwclock.data
-echo "rsync -avzP --numeric-ids -e 'ssh -p60022' --exclude=/usr/bin/qemu-aarch64-static --exclude=/etc/wifi_mode.conf --exclude=boot --delete ${ROOTFS}/ root@${IP:-10.32.166.32}:/overlay/lower/"
-echo "rsync -avzP --numeric-ids -e 'ssh -p60022' --exclude=/usr/bin/qemu-aarch64-static --delete ${ROOTFS}/boot/ root@${IP:-10.32.166.32}:/boot/"
+cat <<EOF
+(cd ${ROOTFS}/root/tunnel/ && for i in *.sh;do find /home/johnyin/disk/mygit/github_private/ -name \$i; done | xargs -I@ cp @ .)
+
+rsync -avzP --numeric-ids \\
+    --exclude=/boot \\
+    --exclude=/usr/lib/modules/ \\
+    --exclude=/usr/include/ \\
+    --exclude=/var/log/ \\
+    --delete \\
+    6.6/ 6.10/
+
+# # sync rootfs.
+rsync -avzP --numeric-ids \\
+    -e 'ssh -p60022' \\
+    --exclude=/usr/bin/qemu-aarch64-static \\
+    --exclude=/etc/wifi_mode.conf \\
+    --exclude=boot \\
+    --delete \\
+    ${ROOTFS}/ root@${IP:-10.32.166.32}:/overlay/lower/
+
+# # sync boot partition.
+rsync -avzP --numeric-ids \\
+    -e 'ssh -p60022' \\
+    --delete \\
+    ${ROOTFS}/boot/ root@${IP:-10.32.166.32}:/boot/
+EOF
