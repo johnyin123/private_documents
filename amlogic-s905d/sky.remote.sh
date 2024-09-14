06ab4d1eb173ed53dc37504d5603a3d5f803ba4a
johnyin <johnyin.news@163.com>
2024-09-14 11:15:59 +0800

    new sky.remote impl

--- amlogic-s905d/sky.remote.sh
+++ amlogic-s905d/sky.remote.sh
@@ -1,15 +1,30 @@
 cat <<EOF
-/etc/johnyin/remote/osd.sh
 /etc/johnyin/remote/remoter.sh
 /etc/johnyin/triggerhappy/triggers.d/sky.conf
+/etc/systemd/system/osd.service
+/etc/systemd/system/osd.socket
 EOF
-cat <<'EOF' > osd.sh
-#!/usr/bin/env bash
-color=${1}
-shift
-echo "$*" | aosd_cat --font='DejaVu Serif:style=Book 32' --position=4 --fade-full=${TM:-800} --fore-color=${color} --shadow-opacity=0
+cat <<EOF > osd.socket
+[Socket]
+ListenFIFO=/tmp/osd.stdin
+Service=osd.service
 EOF
+cat <<'EOF' > osd.service
+[Service]
+User=johnyin
+Group=johnyin
+Environment=DISPLAY=:0
+ExecStart=/usr/bin/aosd_cat --font='DejaVu Serif:style=Book 88' --position=4 --fade-full=600 --fore-color=red --shadow-color=blue
+Sockets=osd.socket
+StandardInput=socket
+StandardOutput=journal
+StandardError=journal
+Restart=always
+RestartSec=4
 
+[Install]
+WantedBy=graphical.target
+EOF
 cat <<'EOF' > remoter.sh
 #!/usr/bin/env bash
 
@@ -20,33 +35,40 @@ if [ -f /etc/skyworth_120.conf ]; then
 fi
 log() { logger -t triggerhappy $*; }
 #############default start#########################################
-change_mode() {
-    local mode=${1}
-    log "change mode: ${mode}"
-    /usr/sbin/th-cmd --socket /var/run/thd.socket --mode ${mode} || true
-    /usr/bin/systemd-run --unit osd --uid=${USER} -E TM=700 -E DISPLAY=:0 /etc/johnyin/remote/osd.sh red "Mode ${mode}"
+osd_message() {
+    color=${1}; shift
+    [ -p  /tmp/osd.stdin ] && { echo "$*" > /tmp/osd.stdin; return 0; }
+    # # if aosc_cat service not exist, use commandlink mode
+    echo "$*" | systemd-run --unit -osd --pipe --uid=${USER} -E DISPLAY=:0 aosd_cat --font='DejaVu Serif:style=Book 88' --position=4 --fade-full=600 --fore-color=${color} --shadow-color=blue
 }
+change_mode() {
+    local mode=${1:-}
+    log "change mode: ${mode:-default}"
+    /usr/sbin/th-cmd --socket /var/run/thd.socket --mode ${mode} || true
+    osd_message red "Mode ${mode:-default}"
+}
+# # source remoter.state in every case, to impl press same key twice or more!!
 default_main() {
     local key=${1}
     local POWER_CNT=0
-    source /tmp/default.state 2>/dev/null || true
     case "${key}" in
-        KEY_POWER)       let POWER_CNT+=1
+        KEY_POWER)       source /tmp/remoter.state 2>/dev/null || true
+                         let POWER_CNT+=1
                          log "poweroff ${POWER_CNT}"
                          [ "${POWER_CNT}" -eq 2 ] && /usr/bin/systemctl poweroff
-                         /usr/bin/systemd-run --unit osd --uid=${USER} -E DISPLAY=:0 /etc/johnyin/remote/osd.sh red "再按关机!!!"
+                         osd_message red "再按POWER关机!!!"
                          ;;
-        KEY_LEFT)        ;;
-        KEY_RIGHT)       ;;
-        KEY_UP)          ;;
-        KEY_DOWN)        ;;
-        KEY_F5)          ;;
-        KEY_ESC)         systemctl -q is-active smplayer-johnyin.service || smplayer_start_stop; change_mode @media;;
-        KEY_VOLUMEUP)    ;;
-        KEY_VOLUMEDOWN)  ;;
+        KEY_LEFT)        log "undefined default key ${key}";;
+        KEY_RIGHT)       log "undefined default key ${key}";;
+        KEY_UP)          log "undefined default key ${key}";;
+        KEY_DOWN)        log "undefined default key ${key}";;
+        KEY_F5)          log "undefined default key ${key}";;
+        KEY_ESC)         systemctl -q is-active smplayer-johnyin.service || smplayer_start_stop; change_mode media;;
+        KEY_VOLUMEUP)    log "undefined default key ${key}";;
+        KEY_VOLUMEDOWN)  log "undefined default key ${key}";;
         *)               log "unknow key default ${key}";;
     esac
-    cat <<EOSTATE > /tmp/default.state
+    cat <<EOSTATE > /tmp/remoter.state
 POWER_CNT=${POWER_CNT}
 EOSTATE
 }
@@ -56,7 +78,7 @@ smplayer_start_stop() {
     systemctl -q is-active smplayer-johnyin.service && {
         systemctl stop smplayer-johnyin.service
         pgrep -u ${USER} smplayer >/dev/null && pkill -u ${USER} smplayer
-        change_mode @
+        change_mode
         log "smplayer stop"
     } || {
         [ -e "${LIST_FILE}" ] || systemd-run --scope --uid=${USER} /usr/bin/touch ${LIST_FILE}
@@ -66,7 +88,13 @@ smplayer_start_stop() {
     }
 }
 smplayer_action() {
-    systemctl -q is-active smplayer-johnyin.service || smplayer_start_stop
+
+    systemctl -q is-active smplayer-johnyin.service || {
+        osd_message green "先按POWER运行媒体播放器"
+        # smplayer_start_stop,  systemd service smplayer run  maybe not before you send actoin!!.
+        log "smplayer not run, action $* not send"
+        return 0
+    }
     systemd-run --scope --uid=${USER} -E DISPLAY=:0 smplayer -send-action $*
     log "smplayer_action $*"
     # systemd-run --uid=${USER} -E DISPLAY=:0 playerctl --player=smplayer ....act
@@ -74,21 +102,19 @@ smplayer_action() {
 media_main() {
     local key=${1}
     local RIGHT_CNT=0
-    source /tmp/media.state 2>/dev/null || true
     case "${key}" in
         KEY_POWER)       smplayer_start_stop;;
         KEY_LEFT)        smplayer_action rewind1;;
-        KEY_RIGHT)       let RIGHT_CNT+=1
-                         smplayer_action forward${RIGHT_CNT};;
+        KEY_RIGHT)       source /tmp/remoter.state 2>/dev/null || true; let RIGHT_CNT+=1; smplayer_action forward${RIGHT_CNT};;
         KEY_UP)          smplayer_action increase_volume;;
         KEY_DOWN)        smplayer_action decrease_volume;;
         KEY_F5)          smplayer_action mute;;
         KEY_ESC)         smplayer_action fullscreen;;
         KEY_VOLUMEUP)    smplayer_action play_or_pause;;
-        KEY_VOLUMEDOWN)  ;;
+        KEY_VOLUMEDOWN)  log "undefined media key ${key}";;
         *)               log "unknow key media ${key}";;
     esac
-    cat <<EOSTATE > /tmp/media.state
+    cat <<EOSTATE > /tmp/remoter.state
 RIGHT_CNT=$((RIGHT_CNT%3))
 EOSTATE
 }
@@ -97,9 +123,14 @@ EOSTATE
 # TH_VALUE=2 long press
 main() {
     local mode=${1}
+    # mode changed, the remoter.state shoule clear, so default/media use same state file!
     case "${mode}" in
         media)    media_main "${TH_EVENT}";;
         default)  default_main "${TH_EVENT}";;
+        osd)      local color=${2}; shift 2
+                  rm -f /tmp/remoter.state 2>/dev/null || true
+                  log "osd message[${color}] $*"
+                  osd_message "${color}" "$*";;
         *)        log "unknow mode ${mode}";;
     esac
 }
@@ -109,13 +140,12 @@ LOCK_FD=
 noflock () { log "lock ${LOCK_FILE} ${LOCK_FD} not acquired, giving up"; exit 1; }
 ( flock -n ${LOCK_FD} || noflock; main "$@"; ) {LOCK_FD}<>${LOCK_FILE}
 EOF
-
 cat <<EOF > sky.conf
 # # mode select keydefine
 KEY_SELECT@            1   @media
-KEY_COMPOSE@           1   /usr/bin/systemd-run --unit osd --uid=johnyin -E DISPLAY=:0 /etc/johnyin/remote/osd.sh red "模式:None"
+KEY_COMPOSE@           1   /etc/johnyin/remote/remoter.sh osd red "模式:None"
 KEY_SELECT@media       1   @
-KEY_COMPOSE@media      1   /usr/bin/systemd-run --unit osd --uid=johnyin -E DISPLAY=:0 /etc/johnyin/remote/osd.sh green "模式:Media"
+KEY_COMPOSE@media      1   /etc/johnyin/remote/remoter.sh osd green "模式:Media"
 # # default mode keydefine
 KEY_POWER@             1   /etc/johnyin/remote/remoter.sh default
 KEY_LEFT@              1   /etc/johnyin/remote/remoter.sh default
