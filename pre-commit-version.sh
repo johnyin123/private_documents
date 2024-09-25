db23312fd400efbc3977f39bb3ffa96ff7bc6057
johnyin <johnyin.news@163.com>
2024-09-25 15:24:32 +0800

    fix

--- pre-commit-version.sh
+++ pre-commit-version.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 exec 1>&2
 for f in $(git diff --cached --name-status  | grep -v '^D' | grep -E ".sh$"| sed 's/[A-Z][ \t]*//'); do
+    [ -e "$f" ] || continue
     __msg="$(git log --pretty='%h[%cI]' -n 1 $f)"
     [ -z "${__msg}" ] && __msg="initver[$(date --iso-8601=seconds)]"
     echo "${f##*/} - ${__msg}"
