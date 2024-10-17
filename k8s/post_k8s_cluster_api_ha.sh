a61115535caf5e98536f5ad907e1dc4b70dee938
johnyin <johnyin.news@163.com>
2024-10-17 16:05:26 +0800

    fix post_k8s_cluster_api_ha.sh

--- k8s/post_k8s_cluster_api_ha.sh
+++ k8s/post_k8s_cluster_api_ha.sh
@@ -3,7 +3,8 @@
 init_keepalived() {
     local id=${1}
     local vip=${2}
-    shift 2
+    local worker=${3}
+    shift 3
     local real_ips="${*}"
     local ip=""
 cat <<EOF 
@@ -14,7 +15,29 @@ virtual_server ${vip} 60443 {
     delay_loop 2
     lb_algo rr
     lb_kind NAT
-    persistence_timeout 360
+    protocol TCP
+$(for ip in ${real_ips}; do
+cat<<EO_REAL
+    real_server ${ip} 6443 {
+        weight 1
+        SSL_GET {
+            url {
+              path /healthz
+              digest 444bcb3a3fcf8389296c49467f27e1d6
+            }
+            connect_timeout 1
+            retry 3
+        }
+    }
+EO_REAL
+done)
+}
+EOF
+${worker} && cat <<EOF
+virtual_server ${vip} 6443 {
+    delay_loop 2
+    lb_algo rr
+    lb_kind NAT
     protocol TCP
 $(for ip in ${real_ips}; do
 cat<<EO_REAL
@@ -40,11 +63,11 @@ kubectl -n kube-system get daemonset kube-proxy -o yaml | \
   sed "s|--config=/var/lib/kube-proxy/config.conf|--config=/var/lib/kube-proxy/config.conf\n        - --ipvs-exclude-cidrs=${CIDRS}|g" | \
   kubectl apply -f -
 EOF
-# init_keepalived 9003 172.16.0.152 172.16.0.150 172.16.0.151 172.16.0.152 >152
+# init_keepalived 9003 172.16.0.152 false 172.16.0.150 172.16.0.151 172.16.0.152 >152
 MASTER_IPS="172.16.0.150 172.16.0.151 172.16.0.152"
 WORKER_IPS="172.16.0.153 172.16.0.154"
-for i in ${MASTER_IPS} ${WORKER_IPS}; do
-    init_keepalived 9999 ${i} ${MASTER_IPS} > ${i}.keepalived.conf
+for i in ${MASTER_IPS}; do
+    init_keepalived 9999 ${i} false ${MASTER_IPS} > ${i}.master.keepalived.conf
     cat <<EOF > ${i}.sh
 #!/bin/bash -x
 SERVER=\$(kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf view -o jsonpath='{.clusters[0].cluster.server}' | sed -E 's|https://(.*):6(0)?443|\1|')
@@ -52,5 +75,18 @@ NAME=\$(kubectl config view --kubeconfig=/etc/kubernetes/kubelet.conf -o jsonpat
 sed -i.bak --quiet -E -e "/\s(\${SERVER})\s*\\$/!p" -e "\\\$a${i} \${SERVER}" /etc/hosts
 kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf set-cluster \${NAME} --server=https://\${SERVER}:60443
 [ -f "\${HOME}/.kube/config" ] && sed -i "s|server:\s*https:.*|server: https://\${SERVER}:60443|" \${HOME}/.kube/config
+[ -f "/etc/kubernetes/admin.conf" ] && sed -i "s|server:\s*https:.*|server: https://\${SERVER}:60443|" /etc/kubernetes/admin.conf
+EOF
+done
+for i in ${WORKER_IPS}; do
+    init_keepalived 9999 ${i} true ${MASTER_IPS} > ${i}.worker.keepalived.conf
+    cat <<EOF > ${i}.sh
+#!/bin/bash -x
+SERVER=\$(kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf view -o jsonpath='{.clusters[0].cluster.server}' | sed -E 's|https://(.*):6(0)?443|\1|')
+NAME=\$(kubectl config view --kubeconfig=/etc/kubernetes/kubelet.conf -o jsonpath='{.clusters[0].name}')
+sed -i.bak --quiet -E -e "/\s(\${SERVER})\s*\\$/!p" -e "\\\$a${i} \${SERVER}" /etc/hosts
+kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf set-cluster \${NAME} --server=https://\${SERVER}:60443
+[ -f "\${HOME}/.kube/config" ] && sed -i "s|server:\s*https:.*|server: https://\${SERVER}:60443|" \${HOME}/.kube/config
+[ -f "/etc/kubernetes/admin.conf" ] && sed -i "s|server:\s*https:.*|server: https://\${SERVER}:60443|" /etc/kubernetes/admin.conf
 EOF
 done
