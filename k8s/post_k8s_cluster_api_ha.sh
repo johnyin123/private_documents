63c8a554935c56e363c4bfab06e573f06797319c
johnyin <johnyin.news@163.com>
2024-10-17 13:33:06 +0800

    fix

--- k8s/post_k8s_cluster_api_ha.sh
+++ k8s/post_k8s_cluster_api_ha.sh
@@ -35,14 +35,22 @@ done)
 EOF
 }
 cat <<'EOF'
-CIDRS=172.16.0.0/21
+CIDRS=172.16.0.0/21 # # k8s node host cidrs
 kubectl -n kube-system get daemonset kube-proxy -o yaml | \
   sed "s|--config=/var/lib/kube-proxy/config.conf|--config=/var/lib/kube-proxy/config.conf\n        - --ipvs-exclude-cidrs=${CIDRS}|g" | \
   kubectl apply -f -
 EOF
 # init_keepalived 9003 172.16.0.152 172.16.0.150 172.16.0.151 172.16.0.152 >152
 MASTER_IPS="172.16.0.150 172.16.0.151 172.16.0.152"
-for i in ${MASTER_IPS}; do
-    init_keepalived 9999 ${i} ${MASTER_IPS} > ${i}.conf
-    echo "on $i: modify ~/.kube/config(server: https://myserver:6443 --> https://myserver:60443), add '$i myserver' /etc/hosts"
+WORKER_IPS="172.16.0.153 172.16.0.154"
+for i in ${MASTER_IPS} ${WORKER_IPS}; do
+    init_keepalived 9999 ${i} ${MASTER_IPS} > ${i}.keepalived.conf
+    cat <<EOF > ${i}.sh
+#!/bin/bash -x
+SERVER=\$(kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf view -o jsonpath='{.clusters[0].cluster.server}' | sed -E 's|https://(.*):6(0)?443|\1|')
+NAME=\$(kubectl config view --kubeconfig=/etc/kubernetes/kubelet.conf -o jsonpath='{.clusters[0].name}')
+sed -i.bak --quiet -E -e "/\s(\${SERVER})\s*\\$/!p" -e "\\\$a${i} \${SERVER}" /etc/hosts
+kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf set-cluster \${NAME} --server=https://\${SERVER}:60443
+[ -f "\${HOME}/.kube/config" ] && sed -i "s|server:\s*https:.*|server: https://\${SERVER}:60443|" \${HOME}/.kube/config
+EOF
 done
