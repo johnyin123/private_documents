201d8cdeac009ec347c8776d86b94a364b731d7e
johnyin <johnyin.news@163.com>
2024-11-11 13:23:58 +0800

    fix

--- k8s/externalIPs.sh
+++ k8s/externalIPs.sh
@@ -1,29 +1,70 @@
-echo "test app: externip-test-app.yaml" && cat <<EOF | kubectl apply -f -
-kind: Pod
-apiVersion: v1
+echo "test app pod" && cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
 metadata:
-  name: externip-test-app
+  name: echo-app
   labels:
-    app: externip-test
+    app: echo-app
 spec:
-  containers:
-    - name: externip-test-app
-      image: registry.local/hashicorp/http-echo
-      args:
-        - "-text=externip-test"
----
+  selector:
+    matchLabels:
+      app: echo-app
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: echo-app
+    spec:
+      containers:
+        - name: echo-app
+          image: registry.local/hashicorp/http-echo
+          args:
+            - "-text=my echo app"
+            - "-listen=:8080"
+EOF
+
+create_svc() {
+    local svc_type=${1}
+    local port=${2}
+    local external_ip=${3:-}
+    case "${svc_type}" in
+        ########################################
+        NodePort|ClusterIP|LoadBalancer)
+            echo "Service expose mode ${svc_type}"
+            ;;
+        *)  echo "Unexpected option: ${svc_type}"; exit 1;;
+    esac
+    cat <<EOF | kubectl apply -f -
 kind: Service
 apiVersion: v1
 metadata:
-  name: externip-test-service
+  name: test-${svc_type}-service
 spec:
-  type: ClusterIP
+  type: ${svc_type}
   selector:
-    app: externip-test
+    app: echo-app
   ports:
     - name: http
       protocol: TCP
-      port: 5678
-  externalIPs: 
-    - 172.16.0.155
+      targetPort: 8080
+      port: ${port}
+$([ "${svc_type}" == "NodePort" ] && cat << EO_NODEPORT
+      nodePort: ${port}
+EO_NODEPORT
+[ -z "${external_ip}" ] || cat <<EO_IP
+  externalIPs:
+    - ${external_ip}
+EO_IP
+)
+EOF
+}
+
+create_svc NodePort 32000
+create_svc LoadBalancer 80 172.16.0.155
+create_svc ClusterIP 80 172.16.0.156
+
+cat <<EOF
+curl <pod ip>:8080
+curl <node ip>:32000
+curl <external ip>:80
 EOF
