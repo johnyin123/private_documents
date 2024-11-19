fba77868c63184ab4e50b5d4c25d0c960b874cca
johnyin <johnyin.news@163.com>
2024-11-19 11:26:00 +0800

    fix

--- k8s/post_k8s_cluster_api_ha.sh
+++ k8s/post_k8s_cluster_api_ha.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+API_SRV_NAME=myserver
+VIP=172.16.0.155
+MASTER_IPS="172.16.0.150 172.16.0.151 172.16.0.152"
 
 cat <<'EOF'
 # # modify etcd/apiserver/scheduler/controller-manager
@@ -9,70 +12,7 @@ cat <<'EOF'
 # # get all master ip:
 kubectl get nodes --selector=node-role.kubernetes.io/master -o jsonpath='{$.items[*].status.addresses[?(@.type=="InternalIP")].address}'
 EOF
-MASTER_IPS="172.16.0.150 172.16.0.151 172.16.0.152"
-WORKER_IPS="172.16.0.153 172.16.0.154"
-for i in ${MASTER_IPS} ${WORKER_IPS}; do
-    cat <<EOF > ${i}.sh
-#!/bin/bash -x
-SERVER=\$(kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf view -o jsonpath='{.clusters[0].cluster.server}' | sed -E 's|https://(.*):6(0)?443|\1|')
-NAME=\$(kubectl config view --kubeconfig=/etc/kubernetes/kubelet.conf -o jsonpath='{.clusters[0].name}')
-sed -i.bak --quiet -E -e "/\s(\${SERVER})\s*\\$/!p" -e "\\\$a${i} \${SERVER}" /etc/hosts
-kubectl config --kubeconfig=/etc/kubernetes/kubelet.conf set-cluster \${NAME} --server=https://\${SERVER}:60443
-[ -f "\${HOME}/.kube/config" ] && sed -i "s|server:\s*https:.*|server: https://\${SERVER}:60443|" \${HOME}/.kube/config
-[ -f "/etc/kubernetes/admin.conf" ] && sed -i "s|server:\s*https:.*|server: https://\${SERVER}:60443|" /etc/kubernetes/admin.conf
-EOF
-done
-: <<EOF
-allow 60443 can access outside k8s cluster.
-cat <<EORULE | kubectl apply -f -
-apiVersion: projectcalico.org/v3
-kind: GlobalNetworkPolicy
-metadata:
-  name: allow-60443-nodeports
-spec:
-  applyOnForward: true
-  preDNAT: true
-  ingress:
-  - action: Allow
-    destination:
-      ports:
-      - 60443
-    protocol: TCP
-    source:
-      nets:
-      - 0.0.0.0/0
-EORULE
-apiVersion: networking.k8s.io/v1
-kind: NetworkPolicy
-metadata:
-  name: open60443
-spec:
-  podSelector:
-      matchLabels:
-        role: client
-  policyTypes:
-    - Ingress
-  ingress:
-    - from:
-      - ipBlock:
-          cidr: 0.0.0.0/0
-      ports:
-        - port: 60443
-EOF
-echo 'can use nginx, k8s_api.stream for this'
 
-cat <<EOF > etc.kubernetes.api.conf
-upstream kubernetes {
-$(for i in ${MASTER_IPS}; do
-echo "    server $i:6443 fail_timeout=1s;"
-done)
-}
-server {
-    listen 60443;
-    access_log off;
-    proxy_pass kubernetes;
-}
-EOF
 cat <<EOF > api-lb.yaml
 # # bad idea, sometime not work on worker node
 apiVersion: v1
@@ -94,4 +34,84 @@ spec:
       path: /etc/kubernetes/api.conf
       type: FileOrCreate
     name: nginx-conf
+EOF
+init_keepalived() {
+    local id=${1}
+    local vip=${2}
+    local dev=${3}
+    local src=${4}
+    shift 4
+    local real_ips="${*}"
+    local ip=""
+cat <<EOF
+global_defs {
+    router_id ${id}
+    vrrp_skip_check_adv_addr
+    vrrp_garp_interval 0
+    vrrp_gna_interval 0
+}
+vrrp_instance kube-api-vip {
+    state BACKUP
+    priority 100
+    interface ${dev}
+    virtual_router_id 60
+    advert_int 1
+    authentication {
+        auth_type PASS
+        auth_pass passwd4kube-api-vip
+    }
+    unicast_src_ip ${src}
+    unicast_peer {
+$(for ip in ${real_ips}; do
+    [ "${src}" == "${ip}" ] && continue
+    cat<<EO_REAL
+        ${ip}
+EO_REAL
+done)
+    }
+    virtual_ipaddress {
+        ${vip}
+    }
+    # notify_master ""
+    # notify_backup ""
+    # notify_stop ""
+}
+EOF
+}
+# init_keepalived 9003 172.16.0.152 172.16.0.150 172.16.0.151 172.16.0.152 >152
+for i in ${MASTER_IPS}; do
+    init_keepalived 9999 ${VIP} "eth0" ${i} ${MASTER_IPS} > ${i}.conf
+    echo "on $i: modify ~/.kube/config(server: https://myserver:6443 --> https://myserver:60443), add '${VIP} myserver' /etc/hosts"
+done
+
+cat <<EOF > etc.kubernetes.api.conf
+upstream kube-api {
+$(for i in ${MASTER_IPS}; do
+echo "    server $i:6443 fail_timeout=1s;"
+done)
+}
+server {
+    listen 60443;
+    access_log off;
+    proxy_pass kube-api;
+}
+EOF
+cat <<EOF
+for ip in ${MASTER_IPS}; do
+    ssh root@\${ip} "rm -f /etc/keepalived/* /etc/nginx/stream-enabled/*; sed -i -e '/\s*${API_SRV_NAME}/d' /etc/hosts; echo '${VIP} ${API_SRV_NAME}' >> /etc/hosts"
+    scp \$ip.conf root@\${ip}:/etc/keepalived/keepalived.conf
+    scp etc.kubernetes.api.conf root@\${ip}:/etc/nginx/stream-enabled/
+done
+# kubectl -n kube-system edit configmaps coredns -o yaml
+# hosts {
+#    ${VIP} ${API_SRV_NAME}
+# }
+# # # forward . /etc/resolv.conf
+EOF
+: <<EOF
+# # sed -i "s/--advertise-address=/--bind-address=/g" /etc/kubernetes/manifests/kube-apiserver.yaml
+# # CIDRS=172.16.0.0/21
+# # kubectl -n kube-system get daemonset kube-proxy -o yaml | \
+# #   sed "s|--config=/var/lib/kube-proxy/config.conf|--config=/var/lib/kube-proxy/config.conf\n        - --ipvs-exclude-cidrs=${CIDRS}|g" | \
+# #   kubectl apply -f -
 EOF
