755562c53bbdc6659001ef72f1c9fb8702b755bb
johnyin <johnyin.news@163.com>
2024-12-04 09:57:54 +0800

    fix

--- systemd-container/modify_docker_image.sh
+++ systemd-container/modify_docker_image.sh
@@ -6,11 +6,15 @@ LOCAL_FILE=values.yaml
 IMG_FILE=/kubesphere/installer/roles/ks-core/ks-core/files/ks-core/values.yaml
 for arch in ${ARCH[@]}; do
     docker pull ${image} --platform ${arch}
-    docker run --rm --name badboy-image --entrypoint="busybox" ${image} sleep infinity
-    # docker cp badboy-image:/kubesphere/installer/roles/ks-core/ks-core/files/ks-core/values.yaml .
+    # # with entrypoint and parm, new image docker inspect entrypoint changed to busybox, after commit.
+    # docker run --rm --name badboy-image --entrypoint="busybox" ${image} sleep infinity
+    docker run --name badboy-image ${image}
+    # docker cp badboy-image:${IMG_FILE} backupfile
     docker cp ${LOCAL_FILE} badboy-image:${IMG_FILE}
     docker commit badboy-image ${image}-${arch}
-    docker kill badboy-image
+    # # maybe container is down now
+    docker kill badboy-image || true
+    docker rm badboy-image
     docker push ${image}-${arch}
 done
 ./make_docker_image.sh -c combine --tag ${image}
