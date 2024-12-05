f7f1df0137961a233d483a55398c7f2ffcc59038
johnyin <johnyin.news@163.com>
2024-12-05 08:40:10 +0800

    fix

--- k8s/offline.img.sh
+++ k8s/offline.img.sh
@@ -2,36 +2,36 @@
 # # in internet env(maybe netns/vm)
 # # PROXY=https://registry.k8s.io PORT=:5001 ./registry.sh
 # # docker pull <ip:5001>/<img> --platform <arch> / k8s.io image pull <ip:5001>/<img> --all-platforms
-mirror=registry.aliyuncs.com/google_containers
-local_registry=192.168.168.250
-for img in $(kubeadm config images list --kubernetes-version=v1.21.7 --image-repository=${mirror})
+from_registry=registry.aliyuncs.com/google_containers
+to_registry=192.168.168.250
+for img in $(kubeadm config images list --kubernetes-version=v1.21.7 --image-repository=${from_registry})
 do
     ctr -n k8s.io image pull ${img} --all-platforms
     ctr -n k8s.io image tag ${img} ${target_img}
-    target_img=${local_registry}/google_containers/${img##*/}
+    target_img=${to_registry}/google_containers/${img##*/}
     ctr -n k8s.io image push ${target_img} --platform amd64 --platform arm64 --plain-http
 done
 # # only pull/push arm64/amd64
 cat <<EOF
-mirror=127.0.0.1:5000
-local_registry=127.0.0.1:5001
+from_registry=127.0.0.1:5000
+to_registry=127.0.0.1:5001
 # # image.list
 library/busybox:latest
 EOF
 ARCH=(amd64 arm64)
 for img in $(cat lst); do
     for arch in ${ARCH[@]}; do
-        docker pull --quiet ${mirror}/${img} --platform ${arch}
-        docker tag ${mirror}/${img} ${local_registry}/${img}-${arch}
-        echo "rm ${mirror}/${img}"
-        docker image rm ${mirror}/${img} >/dev/null 2>/dev/null
-        docker push --quiet ${local_registry}/${img}-${arch}
+        docker pull --quiet ${from_registry}/${img} --platform ${arch}
+        docker tag ${from_registry}/${img} ${to_registry}/${img}-${arch}
+        echo "rm ${from_registry}/${img}"
+        docker image rm ${from_registry}/${img} >/dev/null 2>/dev/null
+        docker push --quiet ${to_registry}/${img}-${arch}
     done
-    ./make_docker_image.sh -c combine --tag ${local_registry}/${img}
+    ./make_docker_image.sh -c combine --tag ${to_registry}/${img}
     for arch in ${ARCH[@]}; do
-        echo "rm ${local_registry}/${img}-${arch}"
-        docker image rm ${local_registry}/${img}-${arch} >/dev/null 2>/dev/null
+        echo "rm ${to_registry}/${img}-${arch}"
+        docker image rm ${to_registry}/${img}-${arch} >/dev/null 2>/dev/null
     done
-    echo "rm ${local_registry}/${img}"
-    docker image rm ${local_registry}/${img} >/dev/null 2>/dev/null
+    echo "rm ${to_registry}/${img}"
+    docker image rm ${to_registry}/${img} >/dev/null 2>/dev/null
 done
