3423ca0f1b742b16b25cd7d75aaa1215649b251f
johnyin <johnyin.news@163.com>
2024-12-06 14:11:19 +0800

    fix

--- systemd-container/modify_docker_image.sh
+++ systemd-container/modify_docker_image.sh
@@ -4,20 +4,20 @@ ARCH=(amd64 arm64)
 image=registry.local/kubesphere/ks-installer:v3.3.2
 # # local file / directory
 echo "chown 1002:1002 kubesphere -R"
-LOCAL_FILE=kubesphere
+LOCAL_DIR=./kubesphere/
 # # docker image position
-IMG_FILE=/
+IMG_FILE=/kubesphere/
 for arch in ${ARCH[@]}; do
     docker pull ${image} --platform ${arch}
     # # with entrypoint and parm, new image docker inspect entrypoint changed to busybox, after commit.
     # docker run --rm --name badboy-${arch} --entrypoint="busybox" ${image} sleep infinity
     docker run --name badboy-${arch} ${image} >/dev/null 2>&1 &
-    # docker cp badboy-${arch}:${IMG_FILE} backupfile
     for cnt in $(seq 1 5); do
-        docker cp --archive badboy-${arch}:${IMG_FILE} - > $(date +'%Y%m%d%H%M%S').bak 2>/dev/null || { echo "wait $cnt"; sleep 2; continue; }
+        docker cp --archive badboy-${arch}:${IMG_FILE} - > $(date +'%Y%m%d%H%M%S').bak.tar 2>/dev/null || { echo "wait $cnt"; sleep 2; continue; }
         break
     done
-    docker cp ${LOCAL_FILE} badboy-${arch}:${IMG_FILE} && echo "COPY OK" || { echo "ERROR"; exit 1; }
+    # # for keep perm and directory position
+    (cd "${LOCAL_DIR}"  && tar c .) | docker cp - badboy-${arch}:${IMG_FILE} && echo "COPY OK" || { echo "ERROR"; exit 1; }
     docker commit badboy-${arch} ${image}-${arch}
     # # maybe container is down now
     docker kill badboy-${arch} || true
