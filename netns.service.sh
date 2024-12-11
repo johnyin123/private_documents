bebe3de5d54c2347db82c3fe5945aa88253d920c
johnyin <johnyin.news@163.com>
2024-12-11 19:10:54 +0800

    fix

--- netns.service.sh
+++ netns.service.sh
@@ -63,8 +63,17 @@ After=netns@${TEST_SVC}.service bridge-netns@${TEST_SVC}.service
 [Service]
 Type=simple
 ExecStart=ip netns exec ${TEST_SVC} /bin/bash /home/johnyin/disk/netsrv/${TEST_SVC}-startup.sh
+ExecStop=-ip netns exec ${TEST_SVC} /bin/bash /home/johnyin/disk/netsrv/${TEST_SVC}-teardown.sh
 [Install]
 WantedBy=multi-user.target
+EOF
+cat <<'EOF' > ${TEST_SVC}-teardown.sh
+#!/usr/bin/env bash
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+readonly SCRIPTNAME=${0##*/}
+wg-quick down client
+# # systemctl cmd can not run in this script
+#systemctl stop bridge-netns@netsrv.service
 EOF
 cat <<'EOF' > ${TEST_SVC}-startup.sh
 #!/usr/bin/env bash
@@ -74,6 +83,10 @@ wg-quick up client
 /usr/sbin/ip route replace default via 192.168.32.1 || true
 /usr/sbin/ip route add 95.169.24.101 via 192.168.168.1 || true
 /usr/sbin/ip route add 39.104.207.142 via 192.168.168.1 || true
+/usr/sbin/ip route add 10.0.0.0/8 via 192.168.168.1 || true
+/usr/sbin/ip route add 172.16.0.0/12 via 192.168.168.1 || true
+/usr/sbin/ip route add 192.168.0.0/16 via 192.168.168.1 || true
+
 ${DIRNAME}/v2ray -config ${DIRNAME}/config.json &
 ${DIRNAME}/v2ray.cli.tproxy.nft.sh
 # ${DIRNAME}/v2ray.cli.tproxy.ipt.sh
