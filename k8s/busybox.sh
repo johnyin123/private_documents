31196eebe9f437e4df07b04e8b4e50a7277ccd10
johnyin <johnyin.news@163.com>
2024-12-23 14:59:18 +0800

    rm busybox.sh

--- k8s/busybox.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "gunzip -c ~/busybox\:1.31.1.tar.gz | ctr --namespace k8s.io image import -"
-echo "ctr -n k8s.io image tag library/busybox:1.31.1 docker.io/library/busybox:1.31.1"
-kubectl run myapp2 --image=library/busybox:1.31.1 --restart=Never -- sleep 1d
-kubectl get pods -A -o wide | grep busybox
-echo "kubectl delete pod busybox --force"
-echo "nsenter -n -t <pid>"
