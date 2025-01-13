3aaf260c884dbddbe224518aa3fc7c9172b6ecba
johnyin <johnyin.news@163.com>
2025-01-13 13:00:59 +0800

    demo

--- kvm/docker-kvm/vmmmgr.py
+++ kvm/docker-kvm/vmmmgr.py
@@ -1,82 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys
+import flask_app, flask
+logger=flask_app.logger
+
+import jinja2
 import libvirt
+class DomainTemplate(object):
+    def __init__(self, filename):
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader('domains'))
+        self.template = env.get_template(filename)
+    def gen_domain(self, vm):
+        return self.template.render(vm=vm)
 
 class VMManager:
     def __init__(self, uri):
         self.conn = libvirt.open(uri)
 
     # Python3 f-string(formatted string literals)
-    def create_vm(self, vm_name, uuid, memory=512, vcpu=1, capacity=8, image_path='.'):
-        xml_config = f"""      
-                <domain type='qemu'>
-                {vmname}
-                </domain>"""
-        try:
-            self.conn.createXML(xml_config, 0)
-            with open('vm_info.txt', 'a') as f:
-                f.write(f'{uuid},{vm_name}\n')
-            return f"VM {vm_name} created successfully"
-        except libvirt.libvirtError as e:
-            return str(e)
+    def create_vm(self, xml):
+        self.conn.createXML(xml, 0)
 
     def delete_vm(self, vm_name):
-        try:
-            dom = self.conn.lookupByName(vm_name)
-            dom.destroy()  
-            dom.undefine()  
-            # Delete vm from log
-            with open("vm_info.txt", "r") as f:
-              logs = f.readlines()
-            with open("vm_info.txt", "w") as f:
-              for line in logs:
-                if vm_name not in line:
-                  f.write(line)
-            return f"VM {vm_name} deleted successfully"
-        except libvirt.libvirtError as e:
-            return str(e)
+        dom = self.conn.lookupByName(vm_name)
+        dom.destroy()
+        dom.undefine()
+        # except libvirt.libvirtError as e:
+        #     return str(e)
 
     def start_vm(self, vm_name):
-        try:
-            dom = self.conn.lookupByName(vm_name)
-            dom.create()
-            return f"VM {vm_name} started successfully"
-        except libvirt.libvirtError as e:
-            return str(e)
+        dom = self.conn.lookupByName(vm_name)
+        dom.create()
 
     def stop_vm(self, vm_name):
-        try:
-            dom = self.conn.lookupByName(vm_name)
-            dom.shutdown()
-            return f"VM {vm_name} stopped successfully"
-        except libvirt.libvirtError as e:
-            return str(e)
+        dom = self.conn.lookupByName(vm_name)
+        dom.shutdown()
 
+class MyApp(object):
+    @staticmethod
+    def create():
+        myapp=MyApp()
+        web=flask_app.create_app({}, json=True)
+        web.add_url_rule('/create_vm/<string:host>', view_func=myapp.create_vm, methods=['POST'])
+        web.add_url_rule('/start_vm/<string:host>/<string:name>', view_func=myapp.start_vm, methods=['GET'])
+        return web
 
-from flask import request, jsonify
-@app.route('/start_vm', methods=['POST'])
-def start_vm():
-    data = request.json
-    user_id = data.get('user_id')
-    vm_name = data.get('vm_name')
+    def vmmgr(self, user, host, port, parm='system'):
+        # parm='system?socket=/storage/run/libvirt/libvirt-sock'
+        return VMManager(f'qemu+ssh://{user}@{host}:{port}/${parm}').create_vm(domxml)
 
-    if not verify_user(user_id):
-        return jsonify({"error": "Invalid user"}), 403
+    def create_vm(self, host):
+        vm = request.json
+        vmtpl1=DomainTemplate('newvm.tpl')
+        # vm= { 'vm_name':'names', 'vm_uuid':'uuid' }
+        domxml=vmtpl1.gen_domain(vm)
+        logger.debug(domxml)
+        self.vmmgr('root', host, 60022).create_vm(domxml)
+        return '{ "OK" : "OK" }'
+        # return jsonify({"result": "ok"})
 
-    result = vm_manager.start_vm(vm_name)
-    return jsonify({"result": result})
+    def start_vm(self, host, name):
+        self.vmmgr('root', host, 60022).start_vm(name)
+        return jsonify({"result": "OK"})
 
-@app.route('/create_vm', methods=['POST'])
-def create_vm():
-    data = request.json
-    user_id = data.get('user_id')
-    vm_name = data.get('vm_name') 
-    memory = data.get('memory') # RAM in MB
-    vcpu = data.get('vcpu') # No of virtual CPUs
-    capacity = data.get('capacity') # disk size in GB
-    image_path = data.get('img-path') # path of the bootable image
+app=MyApp.create()
+def main():
+    logger.debug("uwsgi --http-socket :5999 --plugin python3 --module application:app")
+    host = os.environ.get('HTTP_HOST', '0.0.0.0')
+    port = int(os.environ.get('HTTP_PORT', '18888'))
+    app.run(host=host, port=port, debug=app.config['DEBUG'])
 
-    if not verify_user(user_id):
-        return jsonify({"error": "Invalid user"}), 403
-
-    result = vm_manager.create_vm(vm_name, user_id, memory, vcpu, capacity, image_path)
-    return jsonify({"result": result})
-
+if __name__ == '__main__':
+    exit(main())
