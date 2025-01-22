4a1ff2b7586f394087a395d4ebe8e12b8e9ccba6
johnyin <johnyin.news@163.com>
2025-01-22 08:30:59 +0800

    fix

--- kvm/docker-kvm/config.py
+++ kvm/docker-kvm/config.py
@@ -2,7 +2,7 @@
 import os
 DATABASE = 'sqlite:///./kvm.db'
 CONF_DIR = os.path.abspath(os.path.dirname(__file__))
-OUTDIR = os.environ.get('OUTDIR', os.path.join(CONF_DIR, '.')))
+OUTDIR = os.environ.get('OUTDIR', os.path.join(CONF_DIR, '.'))
 GOLD_DIR = os.path.join(CONF_DIR, 'disk')
 ACTION_DIR = os.path.join(CONF_DIR, 'actions')
 DEVICE_DIR = os.path.join(CONF_DIR, 'devices')
