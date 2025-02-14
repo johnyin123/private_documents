3dbc58b4d7ad1bafe4df9365ea844c64b869fbed
johnyin <johnyin.news@163.com>
2025-02-14 14:42:49 +0800

    fix

--- kvm/docker-kvm/ui/demo/script.js
+++
@@ -1,7 +0,0 @@
-function show(id)  {
-    var tabContents = document.getElementsByClassName('tabContent');
-    for (var i = 0; i < tabContents.length; i++) { 
-        tabContents[i].style.display = 'none';
-    }
-    document.getElementById(id).style.display = "block";
-}
