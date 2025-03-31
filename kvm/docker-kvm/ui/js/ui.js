4bac99778d85d933b52a10ca1688ccab5a631c7c
johnyin <johnyin.news@163.com>
2025-04-01 06:51:57 +0800

    fix

--- kvm/docker-kvm/ui/js/ui.js
+++ kvm/docker-kvm/ui/js/ui.js
@@ -451,31 +451,24 @@ function add_meta() {
 }
 /* include html */
 function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+  const elements = document.querySelectorAll('[w3-include-html]');
+  elements.forEach(elmnt => {
+    const file = elmnt.getAttribute('w3-include-html');
     if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
+      fetch(file).then(response => {
+          if (!response.ok) {
+            throw new Error('Page not found.');
+          }
+          return response.text();
+        }).then(html => {
+          elmnt.innerHTML = html;
+          elmnt.removeAttribute('w3-include-html');
+          includeHTML(); // Process any remaining elements
+        }).catch(error => {
+          elmnt.innerHTML = error.message;
+        });
     }
-  }
+  });
 }
 /* ------------------------- */
 window.addEventListener('load', function() {
@@ -549,7 +542,7 @@ function getjson_fetch_impl(method, url, callback, data=null, stream=null, tmout
       var result = JSON.parse(error.message);
       disperr(result.code, result.name, result.desc);
     } catch (e) {
-      disperr(xhr.status, `${method} ${url}`, `${error.message}`);
+      disperr(999, `${method} ${url}`, `${error.message}`);
     }
   }).finally(() => {
     overlayoff();
