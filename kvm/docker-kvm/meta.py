d87a4c5f97fe1b207c85dbb2fd9dee464d02bc02
johnyin <johnyin.news@163.com>
2025-04-21 10:54:28 +0800

    fix

--- kvm/docker-kvm/meta.py
+++ kvm/docker-kvm/meta.py
@@ -8,7 +8,15 @@ from flask_app import logger
 from typing import Iterable, Optional, Set, List, Tuple, Union, Dict, Generator
 import pycdlib, jinja2, os, utils
 
-class ISOMeta(object):
+def save_metaiso(fname, meta_str, user_str):
+    iso = pycdlib.PyCdlib()
+    iso.new(interchange_level=4, vol_ident='cidata')
+    iso.add_fp(BytesIO(bytes(meta_str,'ascii')), len(meta_str), '/meta-data')
+    iso.add_fp(BytesIO(bytes(user_str,'ascii')), len(user_str), '/user-data')
+    iso.write(fname)
+    iso.close()
+
+class MetaConfig(object):
     def __init__(self):
         env = jinja2.Environment(loader=jinja2.FileSystemLoader(f'{config.META_DIR}'))
         self.meta_data = env.get_template('meta_data')
@@ -16,29 +24,13 @@ class ISOMeta(object):
 
     def create(self, req_json:Dict, mdconfig:Dict) -> None:
         mdconfig_meta = {**config.META_DEFAULT, **req_json, **mdconfig}
-        iso = pycdlib.PyCdlib()
-        iso.new(interchange_level=4, vol_ident='cidata')
-        meta_data = self.meta_data.render(**mdconfig_meta)
-        iso.add_fp(BytesIO(bytes(meta_data,'ascii')), len(meta_data), '/meta-data')
-        user_data = self.user_data.render(**mdconfig_meta)
-        iso.add_fp(BytesIO(bytes(user_data,'ascii')), len(user_data), '/user-data')
-        iso.write(os.path.join(config.ISO_DIR, f'{req_json["vm_uuid"]}.iso'))
-        iso.close()
-
-class NOCLOUDMeta(object):
-    def __init__(self):
-        env = jinja2.Environment(loader=jinja2.FileSystemLoader(f'{config.META_DIR}'))
-        self.meta_data = env.get_template('meta_data')
-        self.user_data = env.get_template('user_data')
-
-    def create(self, req_json:Dict, mdconfig:Dict) -> None:
-        mdconfig_meta = {**config.META_DEFAULT, **req_json, **mdconfig}
-        nocloud_dir = os.path.join(config.ISO_DIR, f'{req_json["vm_uuid"]}')
-        # os.mkdir(), it may raise an error if the directory already exists, os.makedirs() with exist_ok=True to avoid that
-        os.makedirs(nocloud_dir, exist_ok=True)
-        utils.save(os.path.join(nocloud_dir, "meta-data"), self.meta_data.render(**mdconfig_meta))
-        utils.save(os.path.join(nocloud_dir, "user-data"), self.user_data.render(**mdconfig_meta))
+        meta_str = self.meta_data.render(**mdconfig_meta)
+        user_str = self.user_data.render(**mdconfig_meta)
+        output = os.path.join(config.ISO_DIR, f'{req_json["vm_uuid"]}')
+        os.makedirs(output, exist_ok=True)
+        utils.save(os.path.join(output, "meta-data"), meta_str)
+        utils.save(os.path.join(output, "user-data"), user_str)
+        save_metaiso(f'{output}.iso', meta_str, user_str)
 
 def gen_metafiles(mdconfig, req_json):
-    ISOMeta().create(req_json, mdconfig)
-    NOCLOUDMeta().create(req_json, mdconfig)
+    MetaConfig().create(req_json, mdconfig)
