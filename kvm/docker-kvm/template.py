de5d1f220c51f86bf138c376c82488fe72fab3f1
johnyin <johnyin.news@163.com>
2025-05-10 08:42:46 +0800

    fix

--- kvm/docker-kvm/template.py
+++ kvm/docker-kvm/template.py
@@ -3,13 +3,13 @@ import os, jinja2, xml.dom.minidom, utils, config, logging
 from jinja2 import meta as jameta
 logger = logging.getLogger(__name__)
 
-def get_variables(dirname, fn):
+def get_variables(dirname, filename):
     def remove_reserved(varset):
         for key in ['vm_uuid','vm_arch','vm_create','vm_creater','META_SRV','vm_last_disk']:
             varset.discard(key)
         return varset
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(dirname))
-    return remove_reserved(jameta.find_undeclared_variables(env.parse(env.loader.get_source(env, fn)[0])))
+    return remove_reserved(jameta.find_undeclared_variables(env.parse(env.loader.get_source(env, filename)[0])))
 
 class KVMTemplate:
     def __init__(self, dirname, filename):
