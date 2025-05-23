c4af9f9d6d7da5094722d701d70af95b3271eaec
johnyin <johnyin.news@163.com>
2025-05-23 09:11:46 +0800

    fix

--- kvm/docker-kvm/inst_vmmgr_api_srv.sh
+++ kvm/docker-kvm/inst_vmmgr_api_srv.sh
@@ -2,7 +2,7 @@
 set -o nounset -o pipefail -o errexit
 readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
 readonly SCRIPTNAME=${0##*/}
-VERSION+=("bbc7eb55[2025-05-23T07:28:30+08:00]:inst_vmmgr_api_srv.sh")
+VERSION+=("aa9e1043[2025-05-23T09:03:30+08:00]:inst_vmmgr_api_srv.sh")
 ################################################################################
 FILTER_CMD="cat"
 LOGFILE=
@@ -92,7 +92,7 @@ pkill --uid johnyin -9 websockify || true
 pkill --uid johnyin -9 gunicorn || true
 # nohup websockify --token-plugin TokenFile --token-source \${OUTDIR}/token/ 127.0.0.1:6800 &>\${OUTDIR}/websockify.log &
 websockify --token-plugin TokenFile --token-source \${OUTDIR}/token/ 127.0.0.1:6800 &
-gunicorn -b 127.0.0.1:5009 --preload --workers=2 --threads=2 --access-logformat '%(m)s %(s)s %(U)s msec=%(M)s len=%(B)s' --access-logfile='-' 'main:app'
+gunicorn --env LOG=ERROR -b 127.0.0.1:5009 --preload --workers=2 --threads=2 --access-logformat '%(m)s %(s)s %(U)s msec=%(M)s len=%(B)s' --access-logfile='-' 'main:app'
 EODOC
 }
 inst_app_outdir() {
