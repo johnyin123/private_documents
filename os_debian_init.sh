f7788cc35622896ccc4362f5dd37320c32281cfb
johnyin <johnyin.news@163.com>
2025-06-17 13:50:46 +0800

    fix

--- os_debian_init.sh
+++ os_debian_init.sh
@@ -16,7 +16,7 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-VERSION+=("3d8ed86a[2025-06-10T14:55:11+08:00]:os_debian_init.sh")
+VERSION+=("44ee8b31[2025-06-17T13:40:57+08:00]:os_debian_init.sh")
 # liveos:debian_build /tmp/rootfs "" "linux-image-${INST_ARCH:-amd64},live-boot,systemd-sysv"
 # docker:debian_build /tmp/rootfs /tmp/cache "systemd-container"
 # INST_ARCH=amd64
@@ -109,6 +109,7 @@ export -f debian_apt_init
 
 debian_grub_init() {
     echo "generate grub config"
+    cat /etc/default/grub > /etc/default/grub.orig
     cat << 'EOF' > /etc/default/grub
 GRUB_DEFAULT=0
 GRUB_TIMEOUT=5
