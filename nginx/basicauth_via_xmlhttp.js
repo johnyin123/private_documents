6a8034d08d72d301b42bd27bd16b8494e16f50c2
johnyin <johnyin.news@163.com>
2025-07-08 08:26:11 +0800

    doc

--- nginx/basicauth_via_xmlhttp.js
+++ nginx/basicauth_via_xmlhttp.js
@@ -1,3 +1,29 @@
+<form onsubmit="return load()">
+  <input type="text" id="user-name" placeholder="Name" value="USER" required>
+  <input type="password" id="user-pass" placeholder="Password" value="PASS" required>
+  <input type="submit" value="Load!">
+</form>
+<script>
+function load () {
+  // if (window.btoa === undefined) { LOAD POLYFILL }
+  var name = document.getElementById("user-name").value,
+      pass = document.getElementById("user-pass").value,
+      token = "Basic " + window.btoa(name + ":" + pass);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "protected/secret.html");
+  xhr.setRequestHeader("Authorization", token);
+  xhr.onload = function(){
+    if (this.status==200) {
+      document.getElementById("load-here").innerHTML = this.response;
+    } else {
+      alert("HTTP ERROR " + this.status);
+    }
+  };
+  xhr.send();
+  return false;
+}
+</script>
+
 // Basic HTTP Authentication over XMLHttpRequest
 var url = 'http://someurl.com';
 xml = new XMLHttpRequest();
