8502553e85448d46f243b3fdde70b9721b1b26d0
johnyin <johnyin.news@163.com>
2025-08-20 13:43:11 +0800

    fix

--- kvm/docker-kvm/meta.py
+++ kvm/docker-kvm/meta.py
@@ -6,22 +6,18 @@ except ImportError:
 import pycdlib, os, utils, config, template, logging
 logger = logging.getLogger(__name__)
 
-def save_metaiso(fname, directory):
-    iso = pycdlib.PyCdlib()
-    iso.new(interchange_level=4, vol_ident='cidata')
-    for file in os.listdir(directory):
-        meta_str = utils.load(os.path.join(directory, file))
-        iso.add_fp(BytesIO(bytes(meta_str,'ascii')), len(meta_str), f'/{file}')
-    iso.write(fname)
-    iso.close()
-
 def del_metafiles(uuid):
     utils.remove_file(os.path.join(config.DIR_CIDATA, uuid))
 
-def gen_metafiles(**kwargs) -> None:
+def gen_metafiles(**kwargs)->None:
+    iso = pycdlib.PyCdlib()
+    iso.new(interchange_level=4, vol_ident='cidata')
     output = os.path.join(config.DIR_CIDATA, f'{kwargs["vm_uuid"]}')
     os.makedirs(output, exist_ok=True)
     for file in [fn for fn in os.listdir(config.DIR_META) if fn.endswith('.tpl')]:
         meta_str = template.MetaDataTemplate(file).gen_xml(**kwargs)
         utils.save(os.path.join(output, file.removesuffix(".tpl")), meta_str)
-    save_metaiso(os.path.join(output, 'cidata.iso'), output)
+        iso.add_fp(BytesIO(bytes(meta_str,'ascii')), len(meta_str), f'/{file.removesuffix(".tpl")}')
+    iso.write(os.path.join(output, 'cidata.iso'))
+    iso.close()
+    # TODO: upload s3 here ...
