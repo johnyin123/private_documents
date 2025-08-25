fc9bb4b8c76bc015f08b12dcf425303bdd80b77d
johnyin <johnyin.news@163.com>
2025-08-25 08:42:24 +0800

    iso use stream write

--- kvm/docker-kvm/meta.py
+++ kvm/docker-kvm/meta.py
@@ -13,11 +13,12 @@ def gen_metafiles(**kwargs)->None:
     iso = pycdlib.PyCdlib()
     iso.new(interchange_level=4, vol_ident='cidata')
     output = os.path.join(config.DIR_CIDATA, f'{kwargs["vm_uuid"]}')
-    os.makedirs(output, exist_ok=True)
     for file in [fn for fn in os.listdir(config.DIR_META) if fn.endswith('.tpl')]:
         meta_str = template.MetaDataTemplate(file).gen_xml(**kwargs)
         utils.file_save(os.path.join(output, file.removesuffix(".tpl")), meta_str.encode('utf-8'))
         iso.add_fp(BytesIO(bytes(meta_str,'ascii')), len(meta_str), f'/{file.removesuffix(".tpl")}')
-    iso.write(os.path.join(output, 'cidata.iso'))
+    # iso.write(os.path.join(output, 'cidata.iso'))
+    outiso = BytesIO()
+    iso.write_fp(outiso)
+    utils.file_save(os.path.join(output, 'cidata.iso'), outiso.getvalue())
     iso.close()
-    # TODO: upload s3/etcd here ...
