0b5cf9900f044ce8f2edff92912df9d1471f7e78
johnyin <johnyin.news@163.com>
2025-10-15 15:11:09 +0800

    doc

--- kvm/docker-kvm/database.py
+++ kvm/docker-kvm/database.py
@@ -14,12 +14,12 @@ class SHM_KVMVar(utils.ShmListStore):
         var = cache[0] if len(cache) > 0 else {}
         return {key: var.get(key, 'n/a') for key in varset}
 
-KVMHost   = utils.ShmListStore(name='host' ,size=1*utils.MiB) #primary_key:name
-KVMDevice = utils.ShmListStore(name='dev'  ,size=2*utils.MiB) #primary_key:name, kvmhost
-KVMGold   = utils.ShmListStore(name='gold' ,size=200*utils.KiB) #primary_key:name, arch
-KVMIso    = utils.ShmListStore(name='iso'  ,size=100*utils.KiB) #primary_key:name
+KVMHost   = utils.ShmListStore(name='host' ,size=1*utils.MiB)   #recsize ~160b  , primary_key:name
+KVMDevice = utils.ShmListStore(name='dev'  ,size=2*utils.MiB)   #recsize ~260b*3, primary_key:name, kvmhost
+KVMGold   = utils.ShmListStore(name='gold' ,size=200*utils.KiB) #recsize ~220b,   primary_key:name, arch
+KVMIso    = utils.ShmListStore(name='iso'  ,size=100*utils.KiB) #recsize ~150b,   primary_key:name
 KVMVar    = SHM_KVMVar(        name='vars' ,size=100*utils.KiB)
-KVMGuest  = SHM_KVMGuest(      name='guest',size=10*utils.MiB)
+KVMGuest  = SHM_KVMGuest(      name='guest',size=10*utils.MiB)  #recsize ~512b
 
 def reload_all() -> None:
     cfg_class={
