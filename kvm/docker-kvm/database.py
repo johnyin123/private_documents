da44e5a03d1d8141060d3bdbfb6d62e42d1e2fcd
johnyin <johnyin.news@163.com>
2025-10-16 08:14:02 +0800

    doc

--- kvm/docker-kvm/database.py
+++ kvm/docker-kvm/database.py
@@ -15,7 +15,7 @@ class SHM_KVMVar(utils.ShmListStore):
         return {key: var.get(key, 'n/a') for key in varset}
 
 KVMHost   = utils.ShmListStore(name='host' ,size=1*utils.MiB)   #recsize ~160b  , primary_key:name
-KVMDevice = utils.ShmListStore(name='dev'  ,size=2*utils.MiB)   #recsize ~260b*3, primary_key:name, kvmhost
+KVMDevice = utils.ShmListStore(name='dev'  ,size=2*utils.MiB)   #recsize ~100b*3, primary_key:name, kvmhost
 KVMGold   = utils.ShmListStore(name='gold' ,size=200*utils.KiB) #recsize ~220b,   primary_key:name, arch
 KVMIso    = utils.ShmListStore(name='iso'  ,size=100*utils.KiB) #recsize ~150b,   primary_key:name
 KVMVar    = SHM_KVMVar(        name='vars' ,size=100*utils.KiB)
