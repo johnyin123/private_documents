b16639fce3184ff089622365894ae72d89581c5e
johnyin <johnyin.news@163.com>
2025-11-03 09:41:38 +0800

    cython demo

--- kvm/docker-kvm/cython-call-c.sh
+++ kvm/docker-kvm/cython-call-c.sh
@@ -30,3 +30,47 @@ import ${fn}
 if __name__ == '__main__':
     ${fn}.py_hello(b"world")
 EOF
+echo "---------------------------------------------------"
+fn=pymod
+cat <<EOF > ${fn}.c
+#include <Python.h>
+int fib_from_python(int a) {
+    int res;
+    PyObject *pModule,*pFunc,*pArgs,*pValue;
+    /* import */
+    const char* module_name = "${fn}";
+    PyObject *pName = PyUnicode_FromString(module_name);
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName); // Release the reference to the module name
+    if (pModule == NULL) {
+        PyErr_Print();
+        return -1;
+    }
+    /* great_module.great_function */
+    pFunc = PyObject_GetAttrString(pModule, "fib");
+    /* build args */
+    pArgs = PyTuple_New(1);
+    PyTuple_SetItem(pArgs,0,PyLong_FromLong(a));
+    /* call */
+    pValue = PyObject_CallObject(pFunc, pArgs);
+    res = PyLong_AsLong(pValue);
+    Py_DECREF(pModule); // Release the reference when done
+    return res;
+}
+int main(int argc, char *argv[]) {
+    Py_Initialize();
+    printf("fib = %d\n",fib_from_python(2));
+    Py_Finalize();
+    return 0;
+}
+EOF
+cat <<EOF > ${fn}.py
+def fib(n):
+    a,b = 1, 1
+    for i in range(n):
+        a,b = a+b, a
+    return a
+EOF
+gcc -c ${fn}.c -o ${fn}.o $(python3-config --includes)
+gcc ${fn}.o $(python3-config --embed --ldflags) -o ${fn}
+PYTHONPATH=`pwd` ./${fn}
