1676cbcec2450e696354fb7947827e9b25e0248f
johnyin <johnyin.news@163.com>
2025-11-13 14:55:54 +0800

    feature nettools/ssh_tunnel2.sh add remote tap name, support local & remote ipaddress

--- nettools/ssh_tunnel2.sh
+++ nettools/ssh_tunnel2.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("initver[2025-11-13T14:35:05+08:00]:ssh_tunnel2.sh")
+VERSION+=("ea230a3f[2025-11-13T14:35:05+08:00]:ssh_tunnel2.sh")
 [ -e ${DIRNAME}/functions.sh ] && . ${DIRNAME}/functions.sh || { echo '**ERROR: functions.sh nofound!'; exit 1; }
 ################################################################################
 readonly MAX_TAPDEV_NUM=10
@@ -15,12 +15,15 @@ SSH_OPTS="-F none -o StrictHostKeyChecking=no -o UpdateHostKeys=no -o UserKnownH
 usage() {
     R='\e[1;31m' G='\e[1;32m' Y='\e[33;1m' W='\e[0;97m' N='\e[m' usage_doc="$(cat <<EOF
 ${*:+${Y}$*${N}\n}${R}${SCRIPTNAME}${N}
-        -L|--local        LOCAL_BRIDGE
-        -R|--remote       REMOTE_BRIDGE
-        -s|--ssh      *   SSH_CONNECTION user@host | host
-        -p|--port  ${G}<int>${N}  SSH_PORT, default 60022
-        -J|--proxyjump    ProxyJump user@proxy:port, use ssh proxy jump
-                          u1@host1:port1,u2@host2:port2
+        -L|--local   <str>     LOCAL_BRIDGE
+        -R|--remote  <str>     REMOTE_BRIDGE
+        -s|--ssh      *        SSH_CONNECTION user@host | host
+        -p|--port  ${G}<int>${N}       SSH_PORT, default 60022
+        -J|--proxyjump         ProxyJump user@proxy:port, use ssh proxy jump
+                               u1@host1:port1,u2@host2:port2
+        --remote_tap <str>     remote tap name,default eth9
+        --l_ip      <ip cidr>  local tap dev ipaddr (no use bridge)
+        --r_ip      <ip cidr>  remote tap dev ipaddr (no use bridge)
         -q|--quiet
         -l|--log ${G}<int>${N} log level
         -V|--version
@@ -57,21 +60,25 @@ ssh_tunnel() {
         "${r_cmd}"
 }
 main() {
-    local local_br="" remote_br="" ssh_conn=""
+    local l_br="" r_br="" ssh_conn="" l_ip="" r_ip=""
     local ssh_port="60022"
+    local remote_tap="eth9"
     local opt_short="L:R:s:p:J:"
-    local opt_long="local:,remote:,ssh:,port:,proxyjump:,"
+    local opt_long="local:,remote:,ssh:,port:,proxyjump:,remote_tap:,l_ip:,r_ip:,"
     opt_short+="ql:dVh"
     opt_long+="quiet,log:,dryrun,version,help"
     __ARGS=$(getopt -n "${SCRIPTNAME}" -o ${opt_short} -l ${opt_long} -- "$@") || usage
     eval set -- "${__ARGS}"
     while true; do
         case "$1" in
-            -L | --local)   shift; local_br=${1}; shift;;
-            -R | --remote)  shift; remote_br=${1}; shift;;
+            -L | --local)   shift; l_br=${1}; shift;;
+            -R | --remote)  shift; r_br=${1}; shift;;
             -s | --ssh)     shift; ssh_conn=${1}; shift;;
             -p | --port)    shift; ssh_port=${1}; shift;;
             -J | --proxyjump) shift; SSH_OPT="-J ${1} ${SSH_OPT}"; shift;;
+            --remote_tap)   shift; remote_tap=${1}; shift;;
+            --l_ip)         shift; l_ip=${1}; shift;;
+            --r_ip)         shift; r_ip=${1}; shift;;
             ########################################
             -q | --quiet)   shift; QUIET=1;;
             -l | --log)     shift; set_loglevel ${1}; shift;;
@@ -85,13 +92,11 @@ main() {
     # is_user_root || exit_msg "root user need!!\n"
     require ssh socat systemd-run systemctl ip
     [ -z "${ssh_conn}" ] && usage "SSH_CONNECTION Must input"
-    [ -z "${local_br}" ] || bridge_exists "${local_br}" || exit_msg "local bridge ${local_br} nofound!!\n"
-
-    ssh_tunnel "${ssh_conn}" "${ssh_port}" "${local_br}" "${remote_br}"
-    # local l_ip=${5:-}
-    # local r_ip=${6:-}
-    # local r_tap=${7:-eth99}
-    info_msg "systemctl stop sslvpn.service"
+    [ -z "${l_br}" ] || bridge_exists "${l_br}" || exit_msg "local bridge ${l_br} nofound!!\n"
+    [ -z "${l_ip}" ] || l_br=""
+    [ -z "${r_ip}" ] || r_br=""
+    ssh_tunnel "${ssh_conn}" "${ssh_port}" "${l_br}" "${r_br}" "${l_ip}" "${r_ip}" "${remote_tap}"
+    info_msg "systemctl stop sslvpn.service\n"
     return 0
 }
 auto_su "$@"
