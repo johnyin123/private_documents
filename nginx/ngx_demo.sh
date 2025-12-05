a04103cc8ac7c394c68b8ceb4172b5ca72df8fb1
johnyin <johnyin.news@163.com>
2025-12-05 08:07:00 +0800

    nginx/ngx_demo.sh add njs jwt decode

--- nginx/ngx_demo.sh
+++ nginx/ngx_demo.sh
@@ -7,7 +7,7 @@ if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
     export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
     set -o xtrace
 fi
-VERSION+=("3204008d[2025-11-21T09:06:45+08:00]:ngx_demo.sh")
+VERSION+=("6d0269ac[2025-11-21T14:10:30+08:00]:ngx_demo.sh")
 
 set -o errtrace
 set -o nounset
@@ -6007,4 +6007,205 @@ if ($block_file_injections = 1) { return 403; }
 if ($block_common_exploits = 1) { return 403; }
 if ($block_spam = 1) { return 403; }
 if ($block_user_agents = 1) { return 403; }
+EOF
+cat <<'EOF' > jwt_mod.conf
+js_include /etc/nginx/jwt_mod.js;
+server {
+    listen 443 ssl;
+    server_name _;
+    location /private {
+        js_content js_function;
+    }
+    location @private {
+        root /var/www;
+        expires 30d;
+    }
+}
+EOF
+cat <<'EOF' > jwt_mod.js
+function js_function(r) {
+    jwt.return = r;
+    var token = r.args.token;
+    try {
+        var decoded = jwt.decode(token, "secret", false, 'HS256');
+        r.internalRedirect('@private');
+    } catch (e) {
+        r.return(200, 'ERROR');
+    }
+}
+/* module dependencies */
+var crypto = require('crypto');
+/* support algorithm mapping */
+var algorithmMap = {
+  HS256: 'sha256',
+  HS384: 'sha384',
+  HS512: 'sha512',
+  RS256: 'RSA-SHA256'
+};
+
+/**
+ * Map algorithm to hmac or sign type, to determine which crypto function to use
+ */
+var typeMap = {
+  HS256: 'hmac',
+  HS384: 'hmac',
+  HS512: 'hmac',
+  RS256: 'sign'
+};
+/* expose object */
+var jwt = {};
+/* version */
+jwt.version = '0.5.6';
+/**
+ * Decode jwt
+ *
+ * @param {Object} token
+ * @param {String} key
+ * @param {Boolean} [noVerify]
+ * @param {String} [algorithm]
+ * @return {Object} payload
+ * @api public
+ */
+jwt.decode = function jwt_decode(token, key, noVerify, algorithm) {
+  // check token
+  if (!token) {
+    throw new Error('No token supplied');
+  }
+  // check segments
+  var segments = token.split('.');
+  if (segments.length !== 3) {
+    throw new Error('Not enough or too many segments');
+  }
+  // All segment should be base64
+  var headerSeg = segments[0];
+  var payloadSeg = segments[1];
+  var signatureSeg = segments[2];
+  // base64 decode and parse JSON
+    //var header = '';
+    //var payload = '';
+    //jwt.return.return(200, payloadSeg);
+    var h = base64urlDecode(headerSeg);
+    var p = base64urlDecode(payloadSeg);
+    while (h.charCodeAt((h.length-1)) === 0) {
+        h = h.substring(0, h.length - 1);
+    }
+    while (p.charCodeAt((p.length-1)) === 0) {
+        p = p.substring(0, p.length - 1);
+    }
+  var header = JSON.parse(h);
+  var payload = JSON.parse(p);
+  if (!noVerify) {
+    if (!algorithm && /BEGIN( RSA)? PUBLIC KEY/.test(key.toString())) {
+      algorithm = 'RS256';
+    }
+    var signingMethod = algorithmMap[algorithm || header.alg];
+    var signingType = typeMap[algorithm || header.alg];
+    if (!signingMethod || !signingType) {
+      throw new Error('Algorithm not supported');
+    }
+    // verify signature. `sign` will return base64 string.
+    var signingInput = [headerSeg, payloadSeg].join('.');
+    if (!verify(signingInput, key, signingMethod, signingType, signatureSeg)) {
+      throw new Error('Signature verification failed');
+    }
+    // Support for nbf and exp claims.
+    // According to the RFC, they should be in seconds.
+    if (payload.nbf && Date.now() < payload.nbf*1000) {
+      throw new Error('Token not yet active');
+    }
+    if (payload.exp && Date.now() > payload.exp*1000) {
+      throw new Error('Token expired');
+    }
+  }
+  return payload;
+};
+
+/**
+ * Encode jwt
+ *
+ * @param {Object} payload
+ * @param {String} key
+ * @param {String} algorithm
+ * @param {Object} options
+ * @return {String} token
+ * @api public
+ */
+jwt.encode = function jwt_encode(payload, key, algorithm, options) {
+  // Check key
+  if (!key) {
+    throw new Error('Require key');
+  }
+  // Check algorithm, default is HS256
+  if (!algorithm) {
+    algorithm = 'HS256';
+  }
+  var signingMethod = algorithmMap[algorithm];
+  var signingType = typeMap[algorithm];
+  if (!signingMethod || !signingType) {
+    throw new Error('Algorithm not supported');
+  }
+  // header, typ is fixed value.
+  var header = { typ: 'JWT', alg: algorithm };
+  if (options && options.header) {
+    assignProperties(header, options.header);
+  }
+  // create segments, all segments should be base64 string
+  var segments = [];
+  segments.push(base64urlEncode(JSON.stringify(header)));
+  segments.push(base64urlEncode(JSON.stringify(payload)));
+  segments.push(sign(segments.join('.'), key, signingMethod, signingType));
+  return segments.join('.');
+};
+/* private util functions */
+function assignProperties(dest, source) {
+  for (var attr in source) {
+    if (source.hasOwnProperty(attr)) {
+      dest[attr] = source[attr];
+    }
+  }
+}
+function verify(input, key, method, type, signature) {
+  if(type === "hmac") {
+    return (signature === sign(input, key, method, type));
+  }
+  else if(type == "sign") {
+    return crypto.createVerify(method)
+                 .update(input)
+                 .verify(key, base64urlUnescape(signature), 'base64');
+  }
+  else {
+    throw new Error('Algorithm type not recognized');
+  }
+}
+function sign(input, key, method, type) {
+  var base64str;
+  if(type === "hmac") {
+    base64str = crypto.createHmac(method, key).update(input).digest('base64');
+  }
+  else if(type == "sign") {
+    base64str = crypto.createSign(method).update(input).sign(key, 'base64');
+  }
+  else {
+    throw new Error('Algorithm type not recognized');
+  }
+  var ret = base64urlEscape(base64str);
+  //jwt.return.return(200, ret);
+  return ret;
+}
+function base64urlDecode(str) {
+    return String.bytesFrom(str, 'base64');
+}
+function base64urlUnescape(str) {
+  str += new Array(5 - str.length % 4).join('=');
+  return str.replace(/\-/g, '+').replace(/_/g, '/');
+}
+function base64urlEncode(str) {
+    return str.toString('base64');
+}
+function base64urlEscape(str) {
+    str = str.replace(/\+/g, '-');
+    str = str.replace(/\//g, '_');
+    str = str.replace(/\=/g, '');
+  return str;
+}
 EOF
