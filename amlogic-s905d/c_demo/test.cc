1d5a4d6990bd904b7e515a6f7ff004cbc1b44c9f
johnyin <johnyin.news@163.com>
2026-04-01 13:45:02 +0800

    fix

--- amlogic-s905d/c_demo/test.cc
+++ amlogic-s905d/c_demo/test.cc
@@ -1,6 +1,4 @@
-/*
-gcc test.cc -L./ -lneusoft -Wl,-rpath,./
-*/
+/* gcc -x c test.cc -L./ -lneusoft -Wl,-rpath,./ -o test.dll */
 #include "demo.h"
 int main(int argc, char *argv[]) {
     return dllmain(argc, argv);
