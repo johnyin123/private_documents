f06845eb7c6ec777e55af986c6f1f1b7eb6f26d6
johnyin <johnyin.news@163.com>
2026-04-10 07:18:01 +0800

    fix

--- amlogic-s905d/c_demo/demo.c
+++ amlogic-s905d/c_demo/demo.c
@@ -3,15 +3,27 @@
 #include "demo.h"
 struct env {
     int trace_level;
-    FILE *logfile;
 } g_env = {
     .trace_level = 0,
 };
-#define debugln(fmt,args...) { if(g_env.trace_level>5 && g_env.logfile) fprintf(g_env.logfile, "DBG: "fmt"\n", ##args); }
+#ifdef USE_SYSLOG
+#include <syslog.h>
+#define debugln(fmt,args...) do { if(g_env.trace_level>LOG_DEBUG) syslog(LOG_DEBUG, "DBG: "fmt"\n", ##args) } while(0)
+#else
+#define	LOG_EMERG	0	/* system is unusable */
+#define	LOG_ALERT	1	/* action must be taken immediately */
+#define	LOG_CRIT	2	/* critical conditions */
+#define	LOG_ERR		3	/* error conditions */
+#define	LOG_WARNING	4	/* warning conditions */
+#define	LOG_NOTICE	5	/* normal but significant condition */
+#define	LOG_INFO	6	/* informational */
+#define	LOG_DEBUG	7	/* debug-level messages */
+#define debugln(fmt,args...) do { if(g_env.trace_level>LOG_DEBUG) fprintf(stderr, "DBG: "fmt"\n", ##args); } while(0)
+#endif
+
 LIB_INIT void Initializer() {
     const char* env_val = getenv("TRACE");
     if(env_val) g_env.trace_level = atoi(env_val);
-    g_env.logfile = stderr;
     debugln("Library initialized!\n");
 }
 LIB_DEINIT void Deinitializer() {
