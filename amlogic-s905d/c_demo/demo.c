34c598d5b33e584f179a8616f8c04d77fda3906c
johnyin <johnyin.news@163.com>
2026-04-13 08:32:49 +0800

    fix

--- amlogic-s905d/c_demo/demo.c
+++ amlogic-s905d/c_demo/demo.c
@@ -6,21 +6,40 @@ struct env {
 } g_env = {
     .trace_level = 0,
 };
+#define ARRAY_LEN(a)          (sizeof(a)/sizeof((a)[0]))
+#define UNUSED(x)             ((void)(x))
 #ifdef USE_SYSLOG
 #include <syslog.h>
-#define debugln(fmt,args...) do { if(g_env.trace_level>LOG_DEBUG) syslog(LOG_DEBUG, "DBG: "fmt"\n", ##args) } while(0)
+#define log_debug(fmt,args...)  { if(g_env.trace_level>LOG_DEBUG) syslog(LOG_DEBUG, "DEBUG %s:%d "fmt"\n", __FILE__, __LINE_, _##args); }
+#define log_info(fmt,args...)   { if(g_env.trace_level>LOG_INFO)  syslog(LOG_INFO,  fmt"\n", ##args); }
+#define log_error(fmt,args...)  { if(g_env.trace_level>LOG_ERR)   syslog(LOG_ERR,   fmt"\n", ##args); }
 #else
-#define	LOG_EMERG	0	/* system is unusable */
-#define	LOG_ALERT	1	/* action must be taken immediately */
-#define	LOG_CRIT	2	/* critical conditions */
-#define	LOG_ERR		3	/* error conditions */
-#define	LOG_WARNING	4	/* warning conditions */
-#define	LOG_NOTICE	5	/* normal but significant condition */
-#define	LOG_INFO	6	/* informational */
-#define	LOG_DEBUG	7	/* debug-level messages */
-#define debugln(fmt,args...) do { if(g_env.trace_level>LOG_DEBUG) fprintf(stderr, "DBG: "fmt"\n", ##args); } while(0)
+#define LOG_EMERG     0
+#define LOG_ALERT     1
+#define LOG_CRIT      2
+#define LOG_ERR       3
+#define LOG_WARNING   4
+#define LOG_NOTICE    5
+#define LOG_INFO      6
+#define LOG_DEBUG     7
+#define log_debug(fmt,args...)  { if(g_env.trace_level>LOG_DEBUG) fprintf(stderr, "DEBUG %s:%d "fmt"\n", __FILE__, __LINE__, ##args); }
+#define log_info(fmt,args...)   { if(g_env.trace_level>LOG_INFO)  fprintf(stderr, "INFO  %s:%d "fmt"\n", __FILE__, __LINE__, ##args); }
+#define log_error(fmt,args...)  { if(g_env.trace_level>LOG_ERR)   fprintf(stderr, "ERROR %s:%d "fmt"\n", __FILE__, __LINE__, ##args); }
 #endif
-
+#ifdef DEBUG
+#include <time.h>
+#define SHOW_EXECUTION_TIME(func_call) ({ \
+    clock_t start = clock(); \
+    __auto_type _result = (func_call); \
+    clock_t end = clock(); \
+    if(__builtin_types_compatible_p(__typeof__(_result), int)) log_debug("[%s] return %d", #func_call, _result); \
+    double elapsed = (double)(end - start) / CLOCKS_PER_SEC; \
+    log_debug("[%s] Duration: %.9f seconds", #func_call, elapsed); \
+    _result; \
+})
+#else
+#define SHOW_EXECUTION_TIME(func_call) (func_call)
+#endif
 LIB_INIT static void Initializer() {
     const char* env_val = getenv("TRACE");
     if(env_val) g_env.trace_level = atoi(env_val);
