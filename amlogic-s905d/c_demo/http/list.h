32bdff54def1915d7d0840b1568bc0bf8d0264ec
johnyin <johnyin.news@163.com>
2026-04-28 08:43:02 +0800

    fix

--- amlogic-s905d/c_demo/http/list.h
+++ amlogic-s905d/c_demo/http/list.h
@@ -1,38 +1,13 @@
 #ifndef _LIST_H__
 #define _LIST_H__
-typedef struct list_t {
-    struct list_t *next, *prev;
-}list_t;
+typedef struct list_t { struct list_t *next, *prev; }list_t;
 
-static inline void list_init(list_t *list) {
-    list->next = list;
-    list->prev = list;
-}
-
-static inline int list_empty(list_t *list) {
-    return list->next == list;
-}
-
-static inline void list_insert(list_t *link, list_t *new_link) {
-    new_link->prev       = link->prev;
-    new_link->next       = link;
-    new_link->prev->next = new_link;
-    new_link->next->prev = new_link;
-}
-
-static inline void list_append(list_t *list, list_t *new_link) {
-    list_insert((list_t *)list, new_link);
-}
-
-static inline void list_prepend(list_t *list, list_t *new_link) {
-    list_insert(list->next, new_link);
-}
-
-static inline void list_remove(list_t *link) {
-    link->prev->next = link->next;
-    link->next->prev = link->prev;
-}
-
+static inline void list_init(list_t *list) { list->next = list; list->prev = list; }
+static inline int  list_empty(list_t *list) { return list->next == list; }
+static inline void list_insert(list_t *link, list_t *new_link) { new_link->prev = link->prev; new_link->next = link; new_link->prev->next = new_link; new_link->next->prev = new_link; }
+static inline void list_append(list_t *list, list_t *new_link) { list_insert((list_t *)list, new_link); }
+static inline void list_prepend(list_t *list, list_t *new_link) { list_insert(list->next, new_link); }
+static inline void list_remove(list_t *link) { link->prev->next = link->next; link->next->prev = link->prev; }
 #define list_entry(link, type, member) ((type *)((char *)(link)-(unsigned long)(&((type *)0)->member)))
 #define list_head(list, type, member)  list_entry((list)->next, type, member)
 #define list_tail(list, type, member)  list_entry((list)->prev, type, member)
@@ -53,12 +28,12 @@ void testcase() {
         int val;
         list_t link;
     };
-    struct tracker_t tracker;
-    list_init(&(tracker.link));
+    struct list_t tracker;
+    list_init(&tracker);
     struct tracker_t *ptracker = mem_alloc(sizeof(struct tracker_t));
     ptracker->val = 1;
-    list_append(&(tracker.link), &ptracker->link);
-    list_for_each_entry(ptracker, &tracker.link, link) {
+    list_append(&tracker, &ptracker->link);
+    list_for_each_entry(ptracker, &tracker, link) {
         printf("  %d\n", ptracker->val);
     }
 }
