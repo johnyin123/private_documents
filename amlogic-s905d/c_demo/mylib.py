12e1a132d15c30b3f745d506563ec5d2677e9b79
johnyin <johnyin.news@163.com>
2026-04-29 08:22:43 +0800

    demo

--- amlogic-s905d/c_demo/mylib.py
+++ amlogic-s905d/c_demo/mylib.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # ctypesgen/CoPy
+'''
+from http.server import HTTPServer, BaseHTTPRequestHandler
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello, world!")
+server = HTTPServer(('localhost', 8080), SimpleHandler)
+server.serve_forever()
+'''
 import ctypes, logging, os
 
 logging.basicConfig(encoding='utf-8', format='[%(funcName)s@%(filename)s(%(lineno)d)]%(name)s %(levelname)s: %(message)s', level=os.getenv('LOG', 'WARN').upper())
