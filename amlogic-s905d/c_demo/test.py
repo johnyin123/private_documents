28f27a418d838f9f2f2ce644750030d182ec8f52
johnyin <johnyin.news@163.com>
2026-04-29 09:17:38 +0800

    fix

--- amlogic-s905d/c_demo/test.py
+++ amlogic-s905d/c_demo/test.py
@@ -5,19 +5,40 @@ import logging, os
 from typing import Iterable, Optional, Set, Tuple, Union, Dict
 logging.basicConfig(encoding='utf-8', format='[%(funcName)s@%(filename)s(%(lineno)d)]%(name)s %(levelname)s: %(message)s', level=os.getenv('LOG', 'WARN').upper())
 logger = logging.getLogger(__name__)
-from ctypes import cdll, c_char_p, c_int, POINTER
+import ctypes
 
-def main():
+def test2():
     try:
-        my_library = cdll.LoadLibrary('./libneusoft.so')
+        ctypes.CDLL('./libdcrf32.so', mode=ctypes.RTLD_GLOBAL)
+        my_lib = ctypes.cdll.LoadLibrary('./libHeaSecReadInfo.x86_64.so')
     except OSError as e:
         logger.error(f'Error loading library: {e}')
         return 1
-    my_library.dllmain.argtypes = [c_int, POINTER(c_char_p)]    # int dllmain(int argc, char *argv[]);
+    my_lib.Init.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+    my_lib.Init.restype = ctypes.c_int
+    my_lib.ReadCardBas.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+    my_lib.ReadCardBas.restype = ctypes.c_int
+
+    src=b'''{"IP":"test.srv","PORT":7020,"TIMEOUT":6,"LOG_PATH":"/home/johnyin"}'''
+    errMsg = ctypes.create_string_buffer(4096)
+    ret = my_lib.Init(src, errMsg);
+    print(f"[Python] After:  {ret} {src.decode()}, {errMsg.value.decode()}")
+    output = ctypes.create_string_buffer(4096)
+    ret = my_lib.ReadCardBas(output, errMsg)
+    print(f"[Python] After:  {ret} {output.value.decode()}, {errMsg.value.decode()}")
+    return 0
+
+def main():
+    try:
+        my_library = ctypes.cdll.LoadLibrary('./libneusoft.so')
+    except OSError as e:
+        logger.error(f'Error loading library: {e}')
+        return 1
+    my_library.dllmain.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_char_p)]    # int dllmain(int argc, char *argv[]);
     # # Optionally define the return type if it's not the default (int)
     # my_library.dllmain.restype = c_char_p 
     argv = [b"./my_program_name", b"arg1", b"second_argument", b"last_arg"]
-    c_args = (c_char_p * len(argv))(*argv)
+    c_args = (ctypes.c_char_p * len(argv))(*argv)
     my_library.dllmain(len(argv), c_args)
     return 0
 
