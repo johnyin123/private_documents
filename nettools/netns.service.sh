3dbf89a8fe65d98c717101be195b0bad1340efbf
johnyin <johnyin.news@163.com>
2026-07-06 08:00:22 +0800

    fix netns.service.sh add ipv6

--- nettools/netns.service.sh
+++ nettools/netns.service.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-cat > ns-rank.conf <<EOF
+set -o nounset -o pipefail -o errexit
+readonly DIRNAME="$(readlink -f "$(dirname "$0")")"
+
+NS_NAME=ns-rank
+
+cat > ${NS_NAME}.conf <<EOF
 BRIDGE="br-int"
 ADDRESS="192.168.167.252/24"
 GATEWAY="192.168.167.1"
-# DNS="8.8.8.8"
+DNS="127.0.0.1"
 EOF
 cat > netns@.service <<'EOF'
 [Unit]
@@ -11,6 +16,7 @@ After=network.target
 [Service]
 Type=oneshot
 RemainAfterExit=yes
+Environment=DNS=""
 EnvironmentFile=/etc/%i.conf
 ExecStart=/sbin/create_netns.sh --ipaddr ${ADDRESS} --nsname %i --bridge ${BRIDGE} --gw ${GATEWAY} --dns "${DNS}"
 ExecStart=/sbin/ip netns exec %i /bin/bash /etc/%i/startup.sh
@@ -22,24 +28,41 @@ EOF
 cat >teardown.sh <<'EOF'
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
-kill -9 $(cat /etc/ns-rank/dns.pid)
+readonly DIRNAME="\$(readlink -f "\$(dirname "\$0")")"
+
+systemctl stop cfdns || true
+systemctl reset-failed
 wg-quick down wgrank
 EOF
-cat >startup.sh <<'EOF'
+# IFS='.' read -r a1 a2 a3 a4 <<< "${ipaddr}"
+# for i in $(seq 2 254); do printf "map ${natprefix}.%d    2001::%x%02x:%x%02x\n" $i ${a1} ${a2} ${a3} $i; done
+cat >startup.sh <<EOF
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
+readonly DIRNAME="\$(readlink -f "\$(dirname "\$0")")"
+
+## ipv6 support start
+ipaddr=\${ADDRESS%/*}
+prefix=\${ADDRESS##*/}
+echo "\${ADDRESS} \${GATEWAY}"
+IFS='.' read -r a1 a2 a3 a4 <<< "\${ipaddr}"
+ipv6=\$(printf "2001::%x%02x:%x%02x\n" \${a1} \${a2} \${a3} \${a4})
+ip -6 addr add \${ipv6}/96 dev eth0 || true
+[ -z "\${GATEWAY}" ] || {
+    IFS='.' read -r a1 a2 a3 a4 <<< "\${GATEWAY}"
+    ipv6_gw=\$(printf "2001::%x%02x:%x%02x\n" \${a1} \${a2} \${a3} \${a4})
+    ip -6 route add default via \${ipv6_gw} dev eth0 || true
+}
+sysctl -w net.ipv6.conf.all.forwarding=1
+## ipv6 support end
+
 # /usr/sbin/ip route add 192.3.164.171 via 192.168.167.1 || true # use proxy
 /usr/sbin/ip route add 10.0.0.0/8 via 192.168.167.1 || true
 /usr/sbin/ip route add 172.16.0.0/12 via 192.168.167.1 || true
 /usr/sbin/ip route add 192.168.0.0/16 via 192.168.167.1 || true
 wg-quick up wgrank
 /usr/sbin/ip route replace default via 192.168.32.1 || true
-/usr/sbin/ip route replace 192.168.31.111 via 192.168.32.1 || true
-/usr/sbin/ip route replace 192.168.2.4 via 192.168.32.1 || true
-/usr/sbin/ip route replace 192.168.169.101 via 192.168.32.1 || true
-cloudflared proxy-dns &>/dev/null &
-echo $! > /etc/ns-rank/dns.pid
-echo 'nameserver 127.0.0.1' > /etc/resolv.conf
+systemd-run --working-directory=\${DIRNAME} --unit cfdns \${NS_NAME:+-p NetworkNamespacePath=/run/netns/\${NS_NAME}} cloudflared proxy-dns
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.ip_default_ttl=128
 sysctl -w net.ipv4.ping_group_range="0 2147483647" || true
@@ -52,10 +75,10 @@ table ip nat {
         masquerade
     }
 }
-table ip filter {
+table inet filter {
     chain forward {
         type filter hook forward priority 0; policy accept;
-        meta l4proto tcp tcp flags & (syn|rst) == syn counter tcp option maxseg size set rt mtu
+        tcp flags & (syn|rst) == syn tcp option maxseg size set rt mtu
     }
 }
 EO_NAT
