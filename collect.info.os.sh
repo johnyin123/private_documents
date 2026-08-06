41361788d7c9d42430eab6221bdb8363320e8c2f
johnyin <johnyin.news@163.com>
2026-08-06 12:26:37 +0800

    fix

--- collect.info.os.sh
+++ collect.info.os.sh
@@ -5,6 +5,7 @@ mqadmin getNamesrvConfig
 mqadmin consumerProgress
 mqadmin topicList
 
+{ uname -a;getsebool;ip a;free -h;cat /proc/cpuinfo;uptime;vmstat 1 10;iostat 1 10;ps -efwww;netstat -s;netstat -tunlpa;for p in $(pidof java);do echo $p;cat /proc/$p/limits; done;;journalctl --since "7 days ago";cat /proc/cmdline;sysctl -a; } &> $(hostname).log
 
 # sysctl -a | grep net.netfilter.nf_conntrack_max
 cat /proc/sys/net/netfilter/nf_conntrack_count
