8b3b7fff1085cd20b640088090aba3f0afa0689a
johnyin <johnyin.news@163.com>
2026-08-31 15:56:48 +0800

    new impl

--- ebpf/xdp_lb/xdp_lb.bpf.c
+++ ebpf/xdp_lb/xdp_lb.bpf.c
@@ -8,7 +8,22 @@ struct {
     __type(key, struct key);
     __type(value, struct backend_config);
 } config_map SEC(".maps");
-
+static __always_inline void set_backend_mac(struct ethhdr *eth, const struct backend_config *cfg, __u32 idx) {
+    switch (idx) {
+    case 0:
+        __builtin_memcpy(eth->h_dest, cfg->backends[0].mac_addr, ETH_ALEN);
+        break;
+    case 1:
+        __builtin_memcpy(eth->h_dest, cfg->backends[1].mac_addr, ETH_ALEN);
+        break;
+    case 2:
+        __builtin_memcpy(eth->h_dest, cfg->backends[2].mac_addr, ETH_ALEN);
+        break;
+    case 3:
+        __builtin_memcpy(eth->h_dest, cfg->backends[3].mac_addr, ETH_ALEN);
+        break;
+    }
+}
 SEC("xdp") int xdp_load_balancer(struct xdp_md *ctx) {
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
@@ -27,6 +42,10 @@ SEC("xdp") int xdp_load_balancer(struct xdp_md *ctx) {
     if (num_backends == 0 || num_backends > MAX_PEERS) { return XDP_PASS; }
     /*TODO: source hash persistent*/
     __u32 hash = iphdr->daddr ^ tcphdr->dest;
+#if 1
+    __u32 idx = hash % num_backends;
+    set_backend_mac(eth, lb_cfg, idx);
+#else
     __u64 idx = hash % num_backends;
     // 1. Calculate dynamic index & Clear out any signed/unsigned tracking errors using a 64-bit mask
     //__u64 idx = (__u64)(hash % num_backends)&(MAX_PEERS - 1);
@@ -34,6 +53,7 @@ SEC("xdp") int xdp_load_balancer(struct xdp_md *ctx) {
     asm volatile("" : "+r"(idx));
     if (idx >= MAX_PEERS || idx >= num_backends) { return XDP_PASS; }
     __builtin_memcpy(eth->h_dest, lb_cfg->backends[idx].mac_addr, ETH_ALEN);
+#endif
     /*No IP or TCP checksum recalculation needed!*/
     bpf_printk("LB-DR: VIP %pI4:%d Peer Index %d/%d\n", &key.ip_addr, bpf_ntohs(key.port), idx, num_backends);
     return XDP_TX;
